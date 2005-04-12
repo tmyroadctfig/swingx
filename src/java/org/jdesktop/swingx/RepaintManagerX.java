8a32d05f055e839a8f1aaff07be12c87f455eb0a
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-04-12 18:25:46 +0000

    Added TranslucentRepaintManager interface, and RepaintManagerX, which support translucency for the JXPanel
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@11 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/java/org/jdesktop/swingx/RepaintManagerX.java
@@ -0,0 +1,50 @@
+/*
+ * RepaintManagerX.java
+ *
+ * Created on April 12, 2005, 10:13 AM
+ */
+
+package org.jdesktop.swingx;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Rectangle;
+import javax.swing.JComponent;
+import javax.swing.RepaintManager;
+
+/**
+ * This repaint manager is used by Swingx for translucency. The default implementation
+ * of JXPanel (which supports translucency) will replace the current RepaintManager
+ * with a RepaintManagerX *unless* the current RepaintManager is
+ * tagged by the "TranslucentRepaintManager" interface. 
+ * <p>TODO: Add this to the main documentation (make it visible) so that people
+ * don't bump into it accidently and spend time debugging</p>
+ *
+ * @author zixle
+ * @author rbair
+ */
+public class RepaintManagerX extends RepaintManager {
+    public void addDirtyRegion(JComponent c, int x, int y, int w, int h) {
+        Rectangle dirtyRegion = getDirtyRegion(c);
+        if (dirtyRegion.width == 0 && dirtyRegion.height == 0) {
+            int lastDeltaX = c.getX();
+            int lastDeltaY = c.getY();
+            Container parent = c.getParent();
+            while (parent instanceof JComponent) {
+                if (!parent.isVisible() || (parent.getPeer() == null)) {
+                    return;
+                }
+                if (parent instanceof JXPanel && ((JXPanel)parent).getAlpha() < 1f) {
+                    x += lastDeltaX;
+                    y += lastDeltaY;
+                    lastDeltaX = lastDeltaY = 0;
+                    c = (JComponent)parent;
+                }
+                lastDeltaX += parent.getX();
+                lastDeltaY += parent.getY();
+                parent = parent.getParent();
+            }
+        }
+        super.addDirtyRegion(c, x, y, w, h);
+    }
+}
---
+++ src/java/org/jdesktop/swingx/TranslucentRepaintManager.java
@@ -0,0 +1,15 @@
+/*
+ * TranslucentRepaintManager.java
+ *
+ * Created on April 12, 2005, 10:17 AM
+ */
+
+package org.jdesktop.swingx;
+
+/**
+ * Marker interface suggesting that the given RepaintManager supports
+ * translucency
+ * @author rbair
+ */
+public interface TranslucentRepaintManager {
+}
