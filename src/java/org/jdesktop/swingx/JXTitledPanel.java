deee2c4749ddf83ca653137f3b0715e5276859c9
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-04-13 00:25:57 +0000

    Moved painting code into UIDelegates
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@17 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/JXTitledPanel.java
+++ src/java/org/jdesktop/swingx/JXTitledPanel.java
@@ -10,26 +10,19 @@ package org.jdesktop.swingx;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
-import java.awt.Insets;
-import java.awt.Paint;
+import java.awt.Font;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.LookAndFeel;
+import javax.swing.UIDefaults;
 import javax.swing.UIManager;
 import javax.swing.plaf.ColorUIResource;
-import org.jdesktop.swingx.util.UIManagerUtils;
-
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import org.jdesktop.swingx.plaf.TitledPanelUI;
 
 /**
  * A special type of Panel that has a Title section and a
@@ -44,26 +37,119 @@ import org.jdesktop.swingx.util.UIManagerUtils;
  * @author Richard Bair
  * @author Nicola Ken Barozzi
  */
-public class JXTitledPanel extends JPanel{
-	private static final ColorUIResource BLACK = new ColorUIResource(Color.BLACK);
-	private static final ColorUIResource WHITE = new ColorUIResource(Color.WHITE);
-	/**
-	 * JLabel used for the title in the Title section of the JTitledPanel.
-	 */
-	protected JLabel caption;
+public class JXTitledPanel extends JXPanel {
+    /**
+     * @see #getUIClassID
+     * @see #readObject
+     */
+    private static final String uiClassID = "TitledPanelUI";
+    private static final PropertyChangeListener LAF_LISTENER;
+    
+    /**
+     * Initialization that would ideally be moved into various look and feel
+     * classes.
+     */
+    static {
+        loadDefaults();
+        LAF_LISTENER = new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+                if (evt.getPropertyName().equals("lookAndFeel")) {
+                    loadDefaults();
+                }
+            }
+        };
+        UIManager.addPropertyChangeListener(LAF_LISTENER);
+    }
+    
+    private static void loadDefaults() {
+        LookAndFeel laf = UIManager.getLookAndFeel();
+        if (laf.getID().equals("GTK")) {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            defaults.put("JXTitledPanel.title.foreground", UIManager.getColor("light"));
+            defaults.put("JXTitledPanel.title.darkBackground", UIManager.getColor("dark"));
+            defaults.put("JXTitledPanel.title.lightBackground", UIManager.getColor("mid"));
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        } else if (laf.getID().equals("Motif")) {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            defaults.put("JXTitledPanel.title.foreground", UIManager.getColor("activeCaptionText"));
+            defaults.put("JXTitledPanel.title.darkBackground", UIManager.getColor("inactiveCaption"));
+            defaults.put("JXTitledPanel.title.lightBackground", UIManager.getColor("activeCaption"));
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        } else if (laf.getID().equals("Mac")) {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            defaults.put("JXTitledPanel.title.foreground", new ColorUIResource(0, 0, 0));
+            defaults.put("JXTitledPanel.title.darkBackground", new ColorUIResource(140, 140, 140));
+            defaults.put("JXTitledPanel.title.lightBackground", new ColorUIResource(240, 240, 240));
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        } else if (laf.getID().equals("Windows")) {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            defaults.put("JXTitledPanel.title.foreground", new ColorUIResource(33, 93, 198));
+            defaults.put("JXTitledPanel.title.darkBackground", new ColorUIResource(198, 211, 247));
+            defaults.put("JXTitledPanel.title.lightBackground", new ColorUIResource(255, 255, 255));
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        } else if (laf.getID().equals("Plastic")) {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            defaults.put("JXTitledPanel.title.foreground", Color.WHITE);
+            defaults.put("JXTitledPanel.title.darkBackground", Color.GRAY);
+            defaults.put("JXTitledPanel.title.lightBackground", Color.LIGHT_GRAY);
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        } else if (laf.getID().equals("Metal")) {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            MetalLookAndFeel mlaf = (MetalLookAndFeel)laf;
+            defaults.put("JXTitledPanel.title.foreground", new ColorUIResource(255, 255, 255));
+            defaults.put("JXTitledPanel.title.darkBackground", mlaf.getCurrentTheme().getPrimaryControlDarkShadow());
+            defaults.put("JXTitledPanel.title.lightBackground", mlaf.getCurrentTheme().getPrimaryControl());
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        } else {
+            UIDefaults defaults = UIManager.getDefaults();
+            defaults.put(uiClassID, "org.jdesktop.swingx.plaf.metal.MetalTitledPanelUI");
+            //adds some keys to the UIDefaults for the JXTitledPanel component
+            defaults.put("JXTitledPanel.title.foreground", Color.WHITE);
+            defaults.put("JXTitledPanel.title.darkBackground", Color.GRAY);
+            defaults.put("JXTitledPanel.title.lightBackground", Color.LIGHT_GRAY);
+            defaults.put("JXTitledPanel.title.font", UIManager.getFont("Button.font"));
+        }
+    }
+    
 	/**
 	 * The text to use for the title
 	 */
 	private String title = "";
+    /**
+     * The Font to use for the Title
+     */
+    private Font titleFont;
+    /**
+     * For the gradient, this is the background color to use for the
+     * dark part of the gradient
+     */
+    private Color titleDarkBackground;
+    /**
+     * For the gradient, this is the light color to use for the light part
+     * of the gradient
+     */
+    private Color titleLightBackground;
+    /**
+     * The forground color to use for the Title (particularly for the text)
+     */
+    private Color titleForeground;
 	/**
 	 * The ContentPanel.  Whatever this container is will be displayed in the
 	 * Content section
 	 */
 	private Container contentPanel;
-	/**
-	 * The Title section panel.
-	 */
-	protected JPanel topPanel;
 
 	/**
 	 * Create a new JTitledPanel with an empty string for the title.
@@ -78,22 +164,6 @@ public class JXTitledPanel extends JPanel{
 	 */
 	public JXTitledPanel(String title) {
 		this.title = (title == null ? "" : title);
-		
-		//set the UIManager colors for this component to some defaults if they are not already set
-		UIManagerUtils.initDefault("JTitledPanel.title.foreground", "black", BLACK);
-		Color c = UIManager.getColor("ComboBox.selectionBackground");
-		//TOTAL HACK. TODO What should I do about default colors?
-		if (c == null) {
-		    c = UIManager.getColor("ProgressBar.background");
-		    if (c == null) {
-		        c = Color.decode("0x80b2ea");
-		    }
-		}
-		UIManagerUtils.initDefault("JTitledPanel.title.darkBackground", "primary2", c);
-		UIManagerUtils.initDefault("JTitledPanel.title.lightBackground", "white", WHITE);
-		UIManagerUtils.initDefault("JTitledPanel.title.font", UIManager.getFont("Button.font"));
-		
-		initGui();
 	}
 
 	/**
@@ -106,6 +176,64 @@ public class JXTitledPanel extends JPanel{
 		this(title);
 		setContentContainer(content);
 	}
+
+    /**
+     * Resets the UI property with a value from the current look and feel.
+     *
+     * @see JComponent#updateUI
+     */
+    public void updateUI() {
+        //this check was added because apparently loadDefaults is not called
+        //in netbeans?? Maybe I just needed to close & restart netbeans???
+        Object ui = UIManager.getUI(this);
+        if (!(ui instanceof TitledPanelUI)) {
+            loadDefaults();
+            ui = UIManager.getUI(this);
+        }
+        setUI((TitledPanelUI)ui);
+    }
+
+    /**
+     * Returns the look and feel (L&F) object that renders this component.
+     *
+     * @return the TitledPanelUI object that renders this component
+     * @since 1.4
+     */
+    public TitledPanelUI getUI() {
+        return (TitledPanelUI)ui;
+    }
+
+
+    /**
+     * Sets the look and feel (L&F) object that renders this component.
+     *
+     * @param ui  the TitledPanelUI L&F object
+     * @see UIDefaults#getUI
+     * @since 1.4
+     * @beaninfo
+     *        bound: true
+     *       hidden: true
+     *    attribute: visualUpdate true
+     *  description: The UI object that implements the Component's LookAndFeel. 
+     */
+    public void setUI(TitledPanelUI ui) {
+        super.setUI(ui);
+    }
+
+    /**
+     * Returns a string that specifies the name of the L&F class
+     * that renders this component.
+     *
+     * @return "TitledPanelUI"
+     * @see JComponent#getUIClassID
+     * @see UIDefaults#getUI
+     * @beaninfo
+     *        expert: true
+     *   description: A string that specifies the name of the L&F class.
+     */
+    public String getUIClassID() {
+        return uiClassID;
+    }
 
 	/**
 	 * @return
@@ -120,86 +248,11 @@ public class JXTitledPanel extends JPanel{
 	public void setTitle(String title) {
 		String oldTitle = this.title;
 		this.title = (title == null ? "" : title);
-		caption.setText(title);
 		PropertyChangeEvent event = new PropertyChangeEvent(this, "title", oldTitle, title);
 		PropertyChangeListener[] listeners = this.getPropertyChangeListeners("title");
 		for (int i = 0; i < listeners.length; i++) {
 			listeners[i].propertyChange(event);
 		}
-	}
-	
-	/**
-	 * Utility method for initializing the gui
-	 */
-	private void initGui() {
-		//draw my beautiful self.
-		//the widget has a title bar, and a content area.  
-		//The content area can in theory contain any component.
-
-		//in reality, I'd like to have a peer that does all the drawing
-		//(as in swing).
-		Color titleColor = UIManager.getColor("JTitledPanel.title.foreground");
-		
-		
-		this.setLayout(new BorderLayout());
-		
-		contentPanel = new JPanel();
-		((JPanel)contentPanel).setBorder(BorderFactory.createEmptyBorder());
-		this.add(contentPanel, BorderLayout.CENTER);
-
-		caption = new JLabel(title);
-		caption.setFont(UIManager.getFont("JTitledPanel.title.font"));
-		topPanel = new JGradientPanel();
-		topPanel.setBorder(BorderFactory.createEmptyBorder());
-		topPanel.setLayout(new GridBagLayout());
-		caption.setForeground(titleColor);
-		topPanel.add(caption, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(2, 12, 2, 4), 0, 0));
-		this.add(topPanel, BorderLayout.NORTH);
-		
-		this.setBorder(BorderFactory.createRaisedBevelBorder());
-		
-		setOpaque(false);
-	}
-
-	/**
-	 * A special inner class who's background is painted as a gradient.  This is used
-	 * as the Title section of the JTitledPanel
-	 * @author Richard Bair
-	 * date: Jan 13, 2004
-	 */
-	private static final class JGradientPanel extends JPanel {
-		private GradientPaint gp;
-		private double oldWidth = -1;
-		private double oldHeight = -1;
-		private ImageIcon helper = new ImageIcon();
-		public JGradientPanel() {
-		}
-		
-		//override the background color to provide for a gradient
-		
-		/* (non-Javadoc)
-		 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
-		 * 
-		 * There is some special optimization code in here that is kind of...er..shall we say, tricky.
-		 * First off, the gradient panels are taking forever to draw. Therefore, I have resorted to caching the
-		 * gradient'ed paint job so that I don't have to repaint it all the time.
-		 */
-		protected void paintComponent(Graphics g) {
-			//draw the gradient background
-			if (oldWidth != getWidth() || oldHeight != getHeight()) {
-				gp = new GradientPaint(0, 0, UIManager.getColor("JTitledPanel.title.darkBackground"), getWidth(), 0, UIManager.getColor("JTitledPanel.title.lightBackground"));
-				Image savedImg = createImage(getWidth(), getHeight());
-				Graphics2D imgg = (Graphics2D)savedImg.getGraphics();
-				imgg.setPaint(gp);
-				imgg.fillRect(0, 0, getWidth(), getHeight());
-				oldWidth = getWidth();
-				oldHeight = getHeight();
-				helper.setImage(savedImg);
-			}
-			//draw the image
-			g.drawImage(helper.getImage(), 0, 0, getWidth(), getHeight(), helper.getImageObserver());
-		}
-
 	}
 	
 	/**
@@ -225,22 +278,47 @@ public class JXTitledPanel extends JPanel{
 	 * @param decoration
 	 */
 	public void addRightDecoration(JComponent decoration) {
-		topPanel.add(decoration, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHEAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        getUI().addRightDecoration(decoration);
 	}
 
-	/**
-	 * @return Returns the topPanel.
-	 */
-	protected JPanel getTopPanel() {
-		return topPanel;
-	}
-	
 	/**
 	 * Adds the given JComponent as a decoration on the left of the title
 	 * @param decoration
 	 */
 	public void addLeftDecoration(JComponent decoration) {
-		topPanel.add(decoration, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        getUI().addLeftDecoration(decoration);
 	}
+
+    public Font getTitleFont() {
+        return titleFont;
+    }
+
+    public void setTitleFont(Font titleFont) {
+        this.titleFont = titleFont;
+    }
+
+    public Color getTitleDarkBackground() {
+        return titleDarkBackground;
+    }
+
+    public void setTitleDarkBackground(Color titleDarkBackground) {
+        this.titleDarkBackground = titleDarkBackground;
+    }
+
+    public Color getTitleLightBackground() {
+        return titleLightBackground;
+    }
+
+    public void setTitleLightBackground(Color titleLightBackground) {
+        this.titleLightBackground = titleLightBackground;
+    }
+
+    public Color getTitleForeground() {
+        return titleForeground;
+    }
+
+    public void setTitleForeground(Color titleForeground) {
+        this.titleForeground = titleForeground;
+    }
 
 }
