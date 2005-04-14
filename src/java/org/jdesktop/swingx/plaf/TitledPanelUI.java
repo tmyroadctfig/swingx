ee537558a634c186edf02e058a870d79dfe7c636
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-04-14 17:31:31 +0000

    added proper copyright notice
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@23 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/plaf/TitledPanelUI.java
+++ src/java/org/jdesktop/swingx/plaf/TitledPanelUI.java
@@ -1,7 +1,8 @@
 /*
- * TitledPanelUI.java
+ * $Id$
  *
- * Created on April 12, 2005, 11:25 AM
+ * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
  */
 
 package org.jdesktop.swingx.plaf;
