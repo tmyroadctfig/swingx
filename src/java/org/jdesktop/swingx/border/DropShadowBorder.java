737b1dd7fd6e15154c488696ccebb01e211d7271
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-04-29 21:25:04 +0000

    The border insets were incorrect on the left side -- originally I wanted a drop shadow on the left as well as the right, and space was left over for that even though it was no longer required.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@48 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/border/DropShadowBorder.java
+++ src/java/org/jdesktop/swingx/border/DropShadowBorder.java
@@ -19,6 +19,7 @@ import javax.swing.border.Border;
 /**
  * Implements a DropShadow for any component. Any drop shadow can be quickly
  * drawn
+ *
  * @author rbair
  */
 public class DropShadowBorder implements Border {
@@ -30,71 +31,71 @@ public class DropShadowBorder implements Border {
     private ImageIcon bottomRight;
     private ImageIcon bottom;
     private ImageIcon bottomLeft;
-
+    
     private Color borderColor;
-	private int lineWidth;
-	
-	public DropShadowBorder() {
-		borderColor = Color.BLACK;
-		lineWidth = 1;
-		initImages();
-	}
-	
-	public DropShadowBorder(Color c, int width) {
-		borderColor = c == null ? Color.BLACK : c;
-		lineWidth = width;
-		initImages();
-	}
-	
-	private void initImages() {
-		topRight = new ImageIcon(getClass().getResource("top-right.png"));
-		right = new ImageIcon(getClass().getResource("right.png"));
-		bottomRight = new ImageIcon(getClass().getResource("bottom-right.png"));
-		bottom = new ImageIcon(getClass().getResource("bottom.png"));
-		bottomLeft = new ImageIcon(getClass().getResource("bottom-left.png"));
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public void paintBorder(Component c, Graphics graphics, int x, int y, int width, int height) {
-		Graphics2D g = (Graphics2D)graphics;
-		//save the old color and the old paint
-		Color oldColor = g.getColor();
+    private int lineWidth;
+    
+    public DropShadowBorder() {
+        borderColor = Color.BLACK;
+        lineWidth = 1;
+        initImages();
+    }
+    
+    public DropShadowBorder(Color c, int width) {
+        borderColor = c == null ? Color.BLACK : c;
+        lineWidth = width;
+        initImages();
+    }
+    
+    private void initImages() {
+        topRight = new ImageIcon(getClass().getResource("top-right.png"));
+        right = new ImageIcon(getClass().getResource("right.png"));
+        bottomRight = new ImageIcon(getClass().getResource("bottom-right.png"));
+        bottom = new ImageIcon(getClass().getResource("bottom.png"));
+        bottomLeft = new ImageIcon(getClass().getResource("bottom-left.png"));
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public void paintBorder(Component c, Graphics graphics, int x, int y, int width, int height) {
+        Graphics2D g = (Graphics2D)graphics;
+        //save the old color and the old paint
+        Color oldColor = g.getColor();
         
         //compute the edges of the component -- not including the border
-        int leftEdge = x + SHADOW_WIDTH;
+        int leftEdge = x;
         int rightEdge = x + width - SHADOW_WIDTH;
         int topEdge = y;
         int bottomEdge = y + height - SHADOW_HEIGHT;
         
-		//draw a rectangular border.
-		g.setColor(borderColor);
-		g.drawRect(leftEdge, topEdge, rightEdge - leftEdge, bottomEdge - topEdge);
-
-		//draw the tiles
-		g.drawImage(topRight.getImage(), rightEdge, 0, topRight.getImageObserver());
-		g.drawImage(right.getImage(), rightEdge, topRight.getIconHeight(), right.getIconWidth(), bottomEdge - topRight.getIconHeight(), right.getImageObserver());
-		g.drawImage(bottomRight.getImage(), rightEdge, bottomEdge, bottomRight.getImageObserver());
-		g.drawImage(bottom.getImage(), leftEdge + bottomLeft.getIconWidth(), bottomEdge, rightEdge - leftEdge - bottomLeft.getIconWidth(), bottom.getIconHeight(), bottom.getImageObserver());
-		g.drawImage(bottomLeft.getImage(), leftEdge, bottomEdge, bottomLeft.getImageObserver());
+        //draw a rectangular border.
+        g.setColor(borderColor);
+        g.drawRect(leftEdge, topEdge, rightEdge - leftEdge, bottomEdge - topEdge);
         
-		g.setColor(oldColor);
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public Insets getBorderInsets(Component c) {
+        //draw the tiles
+        g.drawImage(topRight.getImage(), rightEdge, 0, topRight.getImageObserver());
+        g.drawImage(right.getImage(), rightEdge, topRight.getIconHeight(), right.getIconWidth(), bottomEdge - topRight.getIconHeight(), right.getImageObserver());
+        g.drawImage(bottomRight.getImage(), rightEdge, bottomEdge, bottomRight.getImageObserver());
+        g.drawImage(bottom.getImage(), leftEdge + bottomLeft.getIconWidth(), bottomEdge, rightEdge - leftEdge - bottomLeft.getIconWidth(), bottom.getIconHeight(), bottom.getImageObserver());
+        g.drawImage(bottomLeft.getImage(), leftEdge, bottomEdge, bottomLeft.getImageObserver());
+        
+        g.setColor(oldColor);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public Insets getBorderInsets(Component c) {
         int width = lineWidth + SHADOW_WIDTH;
         int height = lineWidth + SHADOW_HEIGHT;
-        return new Insets(lineWidth, width, height, width);
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public boolean isBorderOpaque() {
-		return true;
-	}	
+        return new Insets(lineWidth, lineWidth, height, width);
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public boolean isBorderOpaque() {
+        return true;
+    }
 }
