b4ffe0012e1b0e2d658e46c0eabd6597878a56fa
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-05-17 15:01:20 +0000

    Issue number: loosely related to #9
    added methods to expose other notification quirks
    
    
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@79 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXTitledPanelTest.java
+++ src/test/org/jdesktop/swingx/JXTitledPanelTest.java
@@ -6,12 +6,17 @@
  */
 package org.jdesktop.swingx;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import org.jdesktop.swingx.util.PropertyChangeReport;
 
@@ -25,8 +30,21 @@ public class JXTitledPanelTest extends InteractiveTestCase {
     }
 
     /**
-     * SwingX Issue #9: missing notification on title change.
+     * Issue ??: notifications missing on all "title"XX properties.
      *
+     */
+    public void testTitlePropertiesNotify() {
+        String title = "starting title";
+        final JXTitledPanel panel = new JXTitledPanel(title);
+        PropertyChangeReport report = new PropertyChangeReport();
+        panel.addPropertyChangeListener(report);
+        assertTrue("panel must notify on titleFont change", report.hasEvents("titleFont"));
+    }
+    
+    /**
+     * SwingX Issue #9: missing notification on title change.
+     * happens if a generic property change listener (== one who 
+     * wants to get all property changes) is registered.
      */
     public void testTitleNotify() {
         String title = "starting title";
@@ -37,10 +55,72 @@ public class JXTitledPanelTest extends InteractiveTestCase {
         assertTrue("panel must have fired propertyChange", report.hasEvents());
         
     }
+    /**
+     * SwingX Issue #9: missing notification on title change.
+     * Notification is correct, if a named propertyChangeListener is
+     * registered.
+     */
+    public void testTitleNotifyNamed() {
+        String title = "starting title";
+        final JXTitledPanel panel = new JXTitledPanel(title);
+        PropertyChangeReport report = new PropertyChangeReport();
+        panel.addPropertyChangeListener( "title", report);
+        panel.setTitle("new title");
+        assertTrue("panel must have fired propertyChange", report.hasEvents());
+        
+    }
+    
+    /**
+     * incorrect propertyChangeEvent on setTitle(null).
+     *
+     */
+    public void testTitleNotifyPropertyValue() {
+        String title = "starting title";
+        final JXTitledPanel panel = new JXTitledPanel(title);
+        PropertyChangeReport report = new PropertyChangeReport();
+        panel.addPropertyChangeListener( "title", report);
+        panel.setTitle(null);
+        assertTrue("panel must have fired propertyChange", report.hasEvents());
+        assertEquals("new property value must be equal to getTitle", panel.getTitle(),
+                report.getLastNewValue("title"));
+        
+    }
+
+//--------------------- interactive tests
+    
+    public  void interactiveIconAndHtmlTest() {
+        String title = "<html><u>starting title </u></html>";
+        final JXTitledPanel panel = new JXTitledPanel(title);
+        Icon icon = new ImageIcon(getClass().getResource("resources/images/wellBottom.gif"));
+        panel.addLeftDecoration(new JLabel(icon));
+        panel.getContentContainer().setLayout(new BoxLayout(panel.getContentContainer(), BoxLayout.Y_AXIS));
+        panel.getContentContainer().add(new JLabel(title));
+        JFrame frame = wrapInFrame(panel, "toggle Title");
+        frame.setVisible(true);
+
+    }
+    
+    /**
+     * trying to set divers TitledPanel properties interactively.
+     * can't set titlefont.
+     */
     public void interactiveTitleTest() {
         String title = "starting title";
         final JXTitledPanel panel = new JXTitledPanel(title);
-        Action action = new AbstractAction("toggle title") {
+        panel.getContentContainer().setLayout(new BoxLayout(panel.getContentContainer(), BoxLayout.Y_AXIS));
+        Action toggleFont = new AbstractAction("toggle Font") {
+            public void actionPerformed(ActionEvent e) {
+                Font oldFont = panel.getTitleFont();
+                System.out.println("oldfont size: " + oldFont.getSize());
+                panel.setTitleFont(oldFont.deriveFont(oldFont.getSize()*2.f));
+                panel.revalidate();
+                
+            }
+            
+        };
+
+        panel.getContentContainer().add(new JButton(toggleFont));
+        Action toggleTitle = new AbstractAction("toggle title") {
             int count = 0;
             public void actionPerformed(ActionEvent e) {
                 panel.setTitle(" * " + count++ + " title");
@@ -49,7 +129,7 @@ public class JXTitledPanelTest extends InteractiveTestCase {
             
         };
 
-        panel.add(new JButton(action));
+        panel.getContentContainer().add(new JButton(toggleTitle));
         JFrame frame = wrapInFrame(panel, "toggle Title");
         frame.setVisible(true);
     }
