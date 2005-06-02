5bea1aa89bee77d3eb0893ab573e594e4d53bdf9
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-06-02 10:07:35 +0000

    added (failure) comments
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@120 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/table/ColumnHeaderRenderer.java
+++ src/java/org/jdesktop/swingx/table/ColumnHeaderRenderer.java
@@ -23,7 +23,6 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 
@@ -34,58 +33,86 @@ import org.jdesktop.swingx.icon.SortArrowIcon;
 
 /**
  * Header renderer class which renders column sort feedback (arrows).
- *
+ * 
+ * PENDING: #25, #169 - Header doesn't look right in winXP/mac
+ * 
+ * 
  * @author Amy Fowler
  * @author Ramesh Gupta
+ * @author Jeanette Winzenburg
  */
 public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
+    private static TableCellRenderer sharedInstance = null;
 
-    private static TableCellRenderer	sharedInstance = null;
-    private static Icon					defaultDownIcon = new SortArrowIcon(false);
-    private static Icon					defaultUpIcon = new SortArrowIcon(true);
-//    private static Border               defaultMarginBorder = BorderFactory.createEmptyBorder(2,2,2,2);
-    private static Border defaultArrowBorder = BorderFactory.createEmptyBorder(0, 2, 0, 4);
-    private Icon    downIcon = defaultDownIcon;
-    private Icon    upIcon = defaultUpIcon;
-    private JLabel	arrow = new JLabel((Icon) null, JLabel.CENTER);
+    private static Icon defaultDownIcon = new SortArrowIcon(false);
+
+    private static Icon defaultUpIcon = new SortArrowIcon(true);
+
+    // private static Border defaultMarginBorder =
+    // BorderFactory.createEmptyBorder(2,2,2,2);
+    private static Border defaultArrowBorder = BorderFactory.createEmptyBorder(
+            0, 2, 0, 4);
+
+    private Icon downIcon = defaultDownIcon;
+
+    private Icon upIcon = defaultUpIcon;
+
+    private JLabel arrow = new JLabel((Icon) null, JLabel.CENTER);
+
     private boolean antiAliasedText = false;
 
     private TableCellRenderer delegateRenderer;
+
     private Component delegateRendererComponent;
-    
-//    private boolean backgroundSet = false;
-//    private boolean foregroundSet = false;
-//    private boolean fontSet = false;
 
     private LabelProperties label;
-    
+
     public static TableCellRenderer getSharedInstance() {
-         if (sharedInstance == null) {
-             sharedInstance = new ColumnHeaderRenderer();
-         }
-         return sharedInstance;
-     }
+        if (sharedInstance == null) {
+            sharedInstance = new ColumnHeaderRenderer();
+        }
+        return sharedInstance;
+    }
 
     public static ColumnHeaderRenderer createColumnHeaderRenderer() {
         return new ColumnHeaderRenderer();
     }
-    
+
+    /*
+     * JW: a story ...
+     * 
+     * Original used a Label to show the typical text/icon part and another
+     * Label to show the up/down arrows, added both to this and configured both
+     * directly in getTableCellRendererComponent.
+     * 
+     * My first shot to solve the issues was to delegate the text/icon part to
+     * the defaultRenderer as returned by the JTableHeader: replace the first
+     * label with the rendererComponent of the renderer. In
+     * getTableCellRendererComponent let the renderer configure the comp and
+     * "move" the border from the delegateComp to this - so it's bordering both
+     * the comp and the arrow.
+     * 
+     * Besides not working (WinXP style headers are still not shown :-( it has
+     * issues with opaqueness: different combinations of this.opaque and
+     * delegate.opaque all have issues 
+     *  1. if the delegate is not explicitly set to false the border looks wrong 
+     *  2. if this is set to true we can have custom background 
+     *     per cell but no setting the header background has no
+     *     effect - and changing LF doesn't take up the LF default background ...
+     *  3. if this is set to false we can't have custom cell background
+     * 
+     * Any ideas?
+     * 
+     * 
+     */
+
     private ColumnHeaderRenderer() {
         setLayout(new BorderLayout());
         label = new LabelProperties();
         initDelegate();
         add(arrow, BorderLayout.EAST);
-        // different combinations of this.opaque and delegate.opaque
-        // all have issues
-        // 1. if the delegate is not explicitly set to false the border looks wrong
-        // 2. if this is set to true we can have custom background per cell but no
-        //  setting the header background has no effect - and changing LF doesn't
-        //  take up the LF default background ... hmmm
-        // 3. if this is set to false we can't have custom cell background
-        // any ideas?
-        setOpaque(true);
+        setOpaque(false);
     }
-
 
     private void initDelegateComponent(Component comp) {
         delegateRendererComponent = comp;
@@ -93,37 +120,41 @@ public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
         if (comp instanceof JComponent) {
             ((JComponent) comp).setOpaque(false);
         }
-        
+
     }
 
     private void initDelegate() {
-        delegateRendererComponent = null;
+        if (delegateRendererComponent != null) {
+            remove(delegateRendererComponent);
+            delegateRendererComponent = null;
+        }
         JTableHeader header = new JTableHeader();
         delegateRenderer = header.getDefaultRenderer();
         // some renderers can't cope with null table
         try {
-            Component comp = delegateRenderer.getTableCellRendererComponent(null,
-                    null, false, false, -1, -1);
+            Component comp = delegateRenderer.getTableCellRendererComponent(
+                    null, null, false, false, -1, -1);
             initDelegateComponent(comp);
         } catch (Exception e) {
-            // can't do anything for now, try later in a normal 
+            // can't do anything for now, try later in a normal
             // getTableCellRendererComponent
         }
-       
+
     }
 
     public Component getTableCellRendererComponent(JTable table, Object value,
-        boolean isSelected, boolean hasFocus, int rowIndex, int columnIndex) {
-        configureDelegate(table, value, isSelected, hasFocus, rowIndex, columnIndex);
+            boolean isSelected, boolean hasFocus, int rowIndex, int columnIndex) {
+        configureDelegate(table, value, isSelected, hasFocus, rowIndex,
+                columnIndex);
         if (table instanceof JXTable) {
-            // We no longer limit ourselves to a single "currently sorted column"
-            Sorter	sorter = ((JXTable) table).getSorter(columnIndex);
+            // We no longer limit ourselves to a single "currently sorted
+            // column"
+            Sorter sorter = ((JXTable) table).getSorter(columnIndex);
 
             if (sorter == null) {
                 arrow.setIcon(null);
                 arrow.setBorder(null);
-            }
-            else {
+            } else {
                 arrow.setIcon(sorter.isAscending() ? upIcon : downIcon);
                 arrow.setBorder(defaultArrowBorder);
             }
@@ -131,14 +162,14 @@ public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
         return this;
     }
 
-    private void configureDelegate(JTable table, Object value, 
+    private void configureDelegate(JTable table, Object value,
             boolean isSelected, boolean hasFocus, int rowIndex, int columnIndex) {
-        Component comp = delegateRenderer.getTableCellRendererComponent(table, value, 
-                isSelected, hasFocus, rowIndex, columnIndex);
+        Component comp = delegateRenderer.getTableCellRendererComponent(table,
+                value, isSelected, hasFocus, rowIndex, columnIndex);
         if (delegateRendererComponent == null) {
             initDelegateComponent(comp);
         }
-        
+
         Border border = UIManager.getBorder("TableHeader.cellBorder");
         if (comp instanceof JComponent) {
             JComponent jComp = (JComponent) comp;
@@ -147,7 +178,7 @@ public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
         }
         applyLabelProperties();
         setBorder(border);
-        
+
     }
 
     private void applyLabelProperties() {
@@ -157,7 +188,6 @@ public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
             label.applyPropertiesTo(delegateRenderer);
         }
     }
-
 
     public void setAntiAliasedText(boolean antiAlias) {
         this.antiAliasedText = antiAlias;
@@ -258,10 +288,10 @@ public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
     public void paint(Graphics g) {
         if (antiAliasedText) {
             Graphics2D g2 = (Graphics2D) g;
-            Object save = g2.getRenderingHint(RenderingHints.
-                                              KEY_TEXT_ANTIALIASING);
+            Object save = g2
+                    .getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING);
             g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
             super.paint(g2);
 
@@ -270,13 +300,12 @@ public class ColumnHeaderRenderer extends JPanel implements TableCellRenderer {
             super.paint(g);
         }
     }
-    
+
     public void updateUI() {
         super.updateUI();
         if (arrow != null) {
             arrow.updateUI();
         }
-        
         initDelegate();
         if (delegateRendererComponent instanceof JComponent) {
             ((JComponent) delegateRendererComponent).updateUI();
