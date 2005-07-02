aadf8f335ae555a2b288dd2823f7634d64e0a14f
l2fprod <l2fprod@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-07-02 13:22:35 +0000

    initial checkin
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@282 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/java/org/jdesktop/swingx/plaf/motif/MotifLookAndFeelAddons.java
@@ -0,0 +1,13 @@
+/*
+ * $Id$
+ *
+ * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ */
+package org.jdesktop.swingx.plaf.motif;
+
+import org.jdesktop.swingx.plaf.basic.BasicLookAndFeelAddons;
+
+public class MotifLookAndFeelAddons extends BasicLookAndFeelAddons {
+
+}
