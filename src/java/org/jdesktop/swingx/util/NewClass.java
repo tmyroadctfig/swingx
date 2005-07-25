f3fe0d656d30ddcbf5423744201bc1ce14bcf64f
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-07-25 17:20:01 +0000

    Test to see whether I am on the binding branch in SwingX
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@352 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/java/org/jdesktop/swingx/util/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * NewClass.java
+ *
+ * Created on July 25, 2005, 10:16 AM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package org.jdesktop.swingx.util;
+
+/**
+ *
+ * @author rb156199
+ */
+public class NewClass {
+    
+    /** Creates a new instance of NewClass */
+    public NewClass() {
+    }
+    
+}
