b7080b46d519d45e4c2e33c7a5ec8058676b02b9
dmouse <dmouse@1312f61e-266d-0410-97fa-c3b71232c9ac>
2005-11-21 05:39:29 +0000

    First revision of the JXDatePickerAddon
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@630 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/java/org/jdesktop/swingx/plaf/JXDatePickerAddon.java
@@ -0,0 +1,34 @@
+package org.jdesktop.swingx.plaf;
+
+import org.jdesktop.swingx.JXDatePicker;
+
+import javax.swing.*;
+import javax.swing.plaf.BorderUIResource;
+import javax.swing.border.LineBorder;
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * @author Joshua Outwater
+ */
+public class JXDatePickerAddon extends AbstractComponentAddon {
+    public JXDatePickerAddon() {
+        super("JXDatePicker");
+    }
+
+    @Override
+    protected void addBasicDefaults(LookAndFeelAddons addon, List<Object> defaults) {
+        defaults.addAll(Arrays.asList(new Object[] {
+                defaults.add(JXDatePicker.uiClassID),
+                defaults.add("org.jdesktop.swingx.plaf.basic.BasicDatePickerUI"),
+                "JXDatePicker.linkFormat",
+                "Today is {0,date, dd MMMM yyyy}",
+                "JXDatePicker.border",
+                new BorderUIResource(BorderFactory.createCompoundBorder(
+                    LineBorder.createGrayLineBorder(),
+                    BorderFactory.createEmptyBorder(3, 3, 3, 3))),
+                "JXDatePicker.numColumns",
+                10
+        }));
+    }
+}
