69da323b90a4cf1a005f4813c8540940d05ae583
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-01-11 15:15:50 +0000

    Issue number:  #233-swingx
    first go on making tree editing bidi-compliant:
    subclassed DefaultTreeCellEditor to take RToL into account
    modified visual test to use new editor
    
    
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@691 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXTreeVisualCheck.java
+++ src/test/org/jdesktop/swingx/JXTreeVisualCheck.java
@@ -1,6 +1,22 @@
 /*
- * Created on 10.01.2006
+ * $Id$
  *
+ * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package org.jdesktop.swingx;
 
@@ -22,8 +38,7 @@ import org.jdesktop.swingx.decorator.Highlighter;
 import org.jdesktop.swingx.decorator.HighlighterPipeline;
 import org.jdesktop.swingx.decorator.PatternHighlighter;
 import org.jdesktop.swingx.decorator.RolloverHighlighter;
-import org.jdesktop.swingx.treetable.TreeTableModel;
-import org.jdesktop.swingx.util.ComponentTreeTableModel;
+import org.jdesktop.swingx.tree.DefaultXTreeCellEditor;
 
 public class JXTreeVisualCheck extends JXTreeUnitTest {
     /**
@@ -34,6 +49,9 @@ public class JXTreeVisualCheck extends JXTreeUnitTest {
     public void interactiveTreeEditingRToL() {
         JTree tree =  new JTree(); 
         tree.setEditable(true);
+        DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
+        tree.setCellRenderer(renderer);
+        tree.setCellEditor(new DefaultXTreeCellEditor(tree, renderer));
         JXTree xTree = new JXTree();
         xTree.setEditable(true);
         final JXFrame frame = wrapWithScrollingInFrame(tree, xTree, "Editing: compare tree and xtree");
