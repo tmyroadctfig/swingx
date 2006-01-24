0231c903b79e8da7e11da8507a3fec63c59d0b8a
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-01-24 21:11:11 +0000

    Fixed package import
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@718 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/border/DropShadowBorder.java
+++ src/java/org/jdesktop/swingx/border/DropShadowBorder.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package com.sun.javachannel.client.ui;
+package org.jdesktop.swingx.border;
 
 import java.awt.Color;
 import java.awt.Component;
