73a238ed5787f1d4ca0ee769ed8288ef698f3925
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-02-21 18:35:45 +0000

    removed commented out code
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@790 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/plaf/basic/BasicLoginPanelUI.java
+++ src/java/org/jdesktop/swingx/plaf/basic/BasicLoginPanelUI.java
@@ -96,4 +96,41 @@ public class BasicLoginPanelUI extends LoginPanelUI {
         originalGraphics.drawString(dlg.getBannerText(), loginStringX, loginStringY);
         return img;
     }
+    //Original dialog painting code
+//    public Image getBanner() {
+//        int w = 400;
+//        int h = 60;
+//
+//        BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+//        Graphics2D g2 = img.createGraphics();
+//
+//        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+//        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//        g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+//
+//        //draw a big square
+//        g2.setColor(UIManager.getColor("JXLoginPanel.banner.darkBackground"));
+//        g2.fillRect(0, 0, w, h);
+//
+//        //create the curve shape
+//        GeneralPath curveShape = new GeneralPath(GeneralPath.WIND_NON_ZERO);
+//        curveShape.moveTo(0, h * .6f);
+//        curveShape.curveTo(w * .167f, h * 1.2f, w * .667f, h * -.5f, w, h * .75f);
+//        curveShape.lineTo(w, h);
+//        curveShape.lineTo(0, h);
+//        curveShape.lineTo(0, h * .8f);
+//        curveShape.closePath();
+//
+//        //draw into the buffer a gradient (bottom to top), and the text "Login"
+//        GradientPaint gp = new GradientPaint(0, h, UIManager.getColor("JXLoginPanel.banner.darkBackground"),
+//                0, 0, UIManager.getColor("JXLoginPanel.banner.lightBackground"));
+//        g2.setPaint(gp);
+//        g2.fill(curveShape);
+//
+//        g2.setFont(UIManager.getFont("JXLoginPanel.banner.font"));
+//        g2.setColor(UIManager.getColor("JXLoginPanel.banner.foreground"));
+//        g2.drawString(dlg.getBannerText(), w * .05f, h * .75f);
+//        return img;
+//    }
 }
