75ab36f5752eba8f02e5a983a2ee71252b21862b
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-03-24 16:01:30 +0000

    forgot to commit when tackling JXTable Sorter api
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@929 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/decorator/SortOrder.java
+++ src/java/org/jdesktop/swingx/decorator/SortOrder.java
@@ -5,7 +5,12 @@
 package org.jdesktop.swingx.decorator;
 
 /**
- * Encasulates sort state.
+ * Encasulates sort state. 
+ * There are several conenience methods to simplify usage of the three possible states 
+ *  (unsorted, ascending sorted, descending sorted).
+ *  PENDING: incomplete.
+ * 
+ * 
  * @author <a href="mailto:jesse@swank.ca">Jesse Wilson</a>
  */
 public final class SortOrder {
@@ -18,10 +23,24 @@ public final class SortOrder {
         this.name = name;
     }
     
+    /**
+     * Convenience to check if the order is sorted.
+     * @return false if unsorted, true for ascending/descending.
+     */
     public boolean isSorted() {
         return this != UNSORTED;
     }
     
+    public boolean isSorted(boolean ascending) {
+        return isSorted() && (ascending == isAscending());
+    }
+    
+    /**
+     * Convenience to check for ascending sort order.
+     * PENDING: is this helpful at all?
+     * 
+     * @return true if ascendingly sorted, false for unsorted/descending.
+     */
     public boolean isAscending() {
         return this == ASCENDING;
     }
