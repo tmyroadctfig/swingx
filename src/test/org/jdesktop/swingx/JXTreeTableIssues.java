207fa119b7e43eda6c57d92d9d5ef33af5f5bfef
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-07-07 14:41:41 +0000

    added test methods to expose missing lead/anchor synch in treeTable
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1213 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXTreeTableIssues.java
+++ src/test/org/jdesktop/swingx/JXTreeTableIssues.java
@@ -10,6 +10,31 @@ package org.jdesktop.swingx;
 
 public class JXTreeTableIssues extends JXTreeTableUnitTest {
 
+    /**
+     * test lead after setting selection via table.
+     *
+     */
+    public void testLeadSelectionFromTable() {
+        JXTreeTable treeTable = prepareTreeTable(false);
+        assertEquals(-1, treeTable.getSelectionModel().getLeadSelectionIndex());
+        assertEquals(-1, treeTable.getTreeSelectionModel().getLeadSelectionRow());
+        treeTable.setRowSelectionInterval(0, 0);
+        assertEquals(treeTable.getSelectionModel().getLeadSelectionIndex(), 
+                treeTable.getTreeSelectionModel().getLeadSelectionRow());
+    }
+    
+    /**
+     * test lead after setting selection via treeSelection.
+     *
+     */
+    public void testLeadSelectionFromTree() {
+        JXTreeTable treeTable = prepareTreeTable(false);
+        assertEquals(-1, treeTable.getSelectionModel().getLeadSelectionIndex());
+        assertEquals(-1, treeTable.getTreeSelectionModel().getLeadSelectionRow());
+        treeTable.getTreeSelectionModel().setSelectionPath(treeTable.getPathForRow(0));
+        assertEquals(treeTable.getSelectionModel().getLeadSelectionIndex(), 
+                treeTable.getTreeSelectionModel().getLeadSelectionRow());
+    }
     
     public void testDummy() {
         
