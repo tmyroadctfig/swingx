af9577b6030ee9fd92e1f35a92254f1af9aa55ed
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-08-23 15:30:33 +0000

    started to prepareTableColumnModelExt api doc for review
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1354 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/table/TableColumnModelExt.java
+++ src/java/org/jdesktop/swingx/table/TableColumnModelExt.java
@@ -28,83 +28,194 @@ import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
 /**
- * An extension to the TableColumnModel that adds logic dealing with the
- * "visibilty" property of the TableColumnExt. If a column is made invisible,
- * then the ColumnModel must not return that column in subsequent calls to
- * <code>getColumnXXX</code>. In other words, it is "silently" removed from
- * the column model until it is again made visible. However, a change in the
- * visibility status of a column will trigger an event so that the underlying
- * JTable can repaint/relayout itself. <p>
- *
- * The semantics behind removing a column and making it invisible are two
- * different things. When a column is removed, it is no longer associated
- * with the model in any way, whereas an invisible column is simply not
- * displayed. This is somewhat similar to the idea of column ordering being
- * different in the model from the view. <p>
+ * An extension of <code>TableColumnModel</code> suitable for use with
+ * <code>JXTable</code>. It extends the notion of columns considered as part
+ * of the view realm to include invisible columns. Conceptually, there are
+ * several sets of "columns":
  * 
- * Note that this is meant to be used with columns of type TableColumnExt. While
- * it is allowed to add columns of type TableColumn it doesn't make much sense to 
- * do so - they will always be visible. <p>
+ * <ol>
+ * <li> model columns: all columns of a <code>TableModel</code>. They are but
+ * a virtual concept, characterizable f.i. by (model) column index, (model)
+ * column name.
+ * <li> view columns: all <code>TableColumnExt</code> objects added to the
+ * <code>TableColumnModelExt</code>, each typically created and configured in
+ * relation to a model column. These can be regarded as a kind of subset of the
+ * model columns (not literally, obviously). Each view column belongs to exactly
+ * one of the following (real) subsets:
+ * <ul>
+ * <li> visible columns: all view columns with the visiblility property enabled
+ * <li> hidden columns: all view columns with the visibilitity property disabled
+ * </ul>
+ * </ol>
  * 
- * This class guarantees to notify registered TableColumnModelExt listener of 
- * propertyChanges fired by contained columns. 
- *
+ * This class manages the view columns and automatically takes care of keeping
+ * track of their location in the visible/hidden subset, triggering the
+ * corresponding changes in interested views. Typically, a view column's 
+ * visibility can be toggled by user interaction, f.i. via a <code>ColumnControlButton</code>.
+ * <p>
+ * An example to programatically hide
+ * the first visible column in the column model:
+ * 
+ * <pre><code>
+ * TableColumnExt columnExt = columnModel.getColumnExt(0);
+ * if (columnExt != null) {
+ *     columnExt.setVisible(false);
+ * }
+ * </code></pre>
+ * 
+ * Note that it is principally allowed to add standard <code>TableColumn</code>s.
+ * Practically, it doesn't make much sense to do so - they will always be
+ * visible.
+ * <p>
+ * 
+ * While individual visible columns can be requested by both column identifier
+ * and column index, the latter is not available for hidden columns. An example
+ * to programmatically guarantee that the view column which corresponds to the
+ * first column in the associated <code>TableModel</code>.
+ * 
+ * <pre><code>
+ * List&lt;TableColumn&gt; columns = colModel.getColumns(true);
+ * for (TableColumn column : columns) {
+ *     if (column.getModelIndex() == 0) {
+ *         if (column instanceof TableColumnExt) {
+ *             ((TableColumnExt) column).setVisible(false);
+ *         }
+ *         return;
+ *     }
+ * }
+ * </code></pre>
+ * 
+ * Alternatively, the column could be requested directly by identifier. By
+ * default the column's headerValue is returned as identifier, if none is set.
+ * 
+ * <pre><code>
+ * Object identifier = tableModel.getColumnName(0);
+ * TableColumnExt columnExt = columnModel.getColumnExt(identifier);
+ * if (columnExt != null) {
+ *     columnExt.setVisible(false);
+ * }
+ * </code></pre>
+ * 
+ * Relying on default identifiers is inherently brittle (headerValues might
+ * change f.i. with <code>Locale</code>s), so explicit configuration of columns with
+ * identifiers is strongly recommended. A custom <code>ColumnFactory</code>
+ * helps to automate column configuration.
+ * <p>
+ * 
+ * 
+ * This class guarantees to notify registered
+ * <code>TableColumnModelListener</code> which are of type
+ * <code>TableColumnModelListener</code> about propertyChanges fired by
+ * contained columns. An example of a client which adjusts itself based on
+ * titles of visible columns: 
+ * <pre><code>
+ * TableColumnModelExtListener l = new TableColumnModelExtListener() {
+ * 
+ *     public void columnPropertyChange(PropertyChangeEvent event) {
+ *         if (&quot;title&quot;.equals(event.getPropertyName())) {
+ *             TableColumn column = (TableColumn) event.getSource();
+ *             if ((column instanceof TableColumnExt)
+ *                     &amp;&amp; !((TableColumnExt) column).isVisible()) {
+ *                 return;
+ *             }
+ *             resizeAndRepaint();
+ *         }
+ *     }
+ * 
+ *     public void columnAdded(TableColumnModelEvent e) {
+ *     }
+ * 
+ *     public void columnMarginChanged(ChangeEvent e) {
+ *     }
+ * 
+ *     public void columnMoved(TableColumnModelEvent e) {
+ *     }
+ * 
+ *     public void columnRemoved(TableColumnModelEvent e) {
+ *     }
+ * 
+ *     public void columnSelectionChanged(ListSelectionEvent e) {
+ *     }
+ * 
+ * };
+ * columnModel.addColumnModelListener(l);
+ * </code></pre>
+ * 
+ * 
  * @author Richard Bair
  * @author Jeanette Winzenburg
+ * 
+ * @see DefaultTableColumnModelExt
+ * @see TableColumnExt
+ * @see ColumnControlButton
+ * @see JXTable#setColumnControlEnabled
+ * @see ColumnFactory
  * 
  */
 public interface TableColumnModelExt extends TableColumnModel {
     /**
-     * Returns a Set containing all of the columns that are invisible. If
-     * no columns in this model are invisible, then this will be an empty
-     * set.
+     * Returns a Set containing all of the columns that are invisible. If no
+     * columns in this model are invisible, then this will be an empty set.
+     * 
      * @return the set of invisible columns.
      */
     public Set<TableColumnExt> getInvisibleColumns();
-    
-    
+
     /**
-     * Returns all of the columns in the TableColumnModel, including invisible
-     * ones if the parameter is true. <p>
+     * Returns a <code>List</code> of contained <code>TableColumn</code>s.
+     * Includes or excludes invisible columns, depending on whether the
+     * <code>includeHidden</code> is true or false, respectively. If false, an
+     * <code>Iterator</code> over the List is equivalent to the
+     * <code>Enumeration</code> returned by <code>getColumns()</code>.
+     * <p>
      * 
-     * NOTE: the order of columns in the List depends on whether 
-     * or not the invisible columns are included, in the former case
-     * it's the insertion order in the latter it's the current order
-     * of the visible columns (same as in #getColumns()). 
+     * NOTE: the order of columns in the List depends on whether or not the
+     * invisible columns are included, in the former case it's the insertion
+     * order in the latter it's the current order of the visible columns.
      * 
-     * @param includeHidden if true the returned list contains all columns 
-     *  otherwise only the subset of visible columns.
-     * @return the list of columns in the TableColumnModel.
+     * @param includeHidden a boolean to indicate whether invisible columns
+     *        should be included
+     * @return a <code>List</code> of contained columns.
      */
     public List<TableColumn> getColumns(boolean includeHidden);
-    
+
     /**
-     * returns the first TableColumnExt with the given identifier or null
-     * if none is found. The returned column may be visible or hidden.
-     *  
-     * @param identifier
-     * @return first <code>TableColumnExt</code> with the given identifier
-     * or null if none is found
+     * Returns the first <code>TableColumnExt</code> with the given
+     * <code>identifier</code> or null if none is found. The returned column
+     * may be visible or hidden.
+     * 
+     * @param identifier the object used as column identifier
+     * @return first <code>TableColumnExt</code> with the given identifier or
+     *         null if none is found
      */
     public TableColumnExt getColumnExt(Object identifier);
-    
+
     /**
-     * Returns the TableColumnExt at index or null
-     *   the column is not of extended type. 
-     *  
-     * @param columnIndex 
-     * @return the <code>TableColumnExt</code> at columnIndex
-     *      or null if not of extended type.
+     * Returns the <code>TableColumnExt</code> in the model whose current
+     * position in the list of visible columns is equal to
+     * <code>columnIndex</code> or null if the column at that position 
+     * is not of type <code>TableColumnExt</code>. 
+     * The returned column is visible.
+     * 
+     * @param columnIndex the index of the column desired
+     * @return the <code>TableColumnExt</code> object that matches the column
+     *         index
+     * @throws ArrayIndexOutOfBoundsException if columnIndex out of allowed
+     *         range, that is if
+     *         <code> (columnIndex < 0) || (columnIndex >= getColumnCount())</code>.
      */
     public TableColumnExt getColumnExt(int columnIndex);
-    
+
     /**
-     * returns the number of contained columns including invisible 
-     *  if the parameter is true.
+     * Returns the number of contained columns including or excluding invisible
+     * columns, depending on whether the <code>includeHidden</code> is true or
+     * false, respectively. If false, this method returns the same count as
+     * <code>getColumnCount()</code>.
      * 
-     * @param includeHidden 
-     * @return the number of contained columns including the invisible
-     * ones if specified
+     * @param includeHidden a boolean to indicate whether invisible columns
+     *        should be included
+     * @return the number of contained columns, including or excluding the
+     *         invisible as specified.
      */
     public int getColumnCount(boolean includeHidden);
 }
