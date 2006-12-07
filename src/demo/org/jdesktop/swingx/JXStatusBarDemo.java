7161c3ec78a5987c36d657eba8adb5d9fd6ee97c
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-12-07 22:19:07 +0000

    removed unused imports
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1620 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/demo/org/jdesktop/swingx/JXStatusBarDemo.java
+++ src/demo/org/jdesktop/swingx/JXStatusBarDemo.java
@@ -9,10 +9,7 @@
 
 package org.jdesktop.swingx;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
