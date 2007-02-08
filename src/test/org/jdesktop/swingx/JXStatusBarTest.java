0a8a01d4d0f2fb8412d8249bd440c55875934195
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-02-08 05:12:43 +0000

    Added a test case for JXStatusBar
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1749 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/test/org/jdesktop/swingx/JXStatusBarTest.java
@@ -0,0 +1,47 @@
+/*
+ * $Id$
+ *
+ * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ */
+
+package org.jdesktop.swingx;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author rbair
+ */
+public class JXStatusBarTest extends junit.framework.TestCase {
+    
+    /**
+     * This test ensures that after the LAF is switched, the Border and
+     * LayoutManager are still in valid states.
+     */
+    public void testLAFSwitch() throws Exception {
+        //create a status bar with a single child, a label.
+        //ensure that after switching look and feels, the layout manager
+        //on the status bar still references the label
+        JXStatusBar bar = new JXStatusBar();
+        JLabel label = new JLabel("Hello");
+        bar.add(label);
+        Dimension dim = bar.getLayout().preferredLayoutSize(bar);
+        toggleLAF();
+        toggleLAF();
+        assertEquals(dim, bar.getLayout().preferredLayoutSize(bar));
+    }
+
+    private void toggleLAF() throws Exception {
+        LookAndFeel laf = UIManager.getLookAndFeel();
+        if (laf == null || laf.getName().equals(UIManager.getSystemLookAndFeelClassName())) {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } else {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+    }
+}
