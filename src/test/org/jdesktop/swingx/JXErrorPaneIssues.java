3e2a78adac54bc94188eb77cf6118cc25cd23bd7
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-02-22 13:16:08 +0000

    added JXErrorPaneIssues to expose several quirks
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1784 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/test/org/jdesktop/swingx/JXErrorPaneIssues.java
@@ -0,0 +1,68 @@
+/*
+ * $Id$
+ *
+ * Copyright 2006 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdesktop.swingx;
+
+import org.jdesktop.swingx.error.ErrorInfo;
+
+/**
+ * Test to expose known issues around <code>JXError*</code>.
+ * 
+ * Ideally, there would be at least one failing test method per open
+ * Issue in the issue tracker. Plus additional failing test methods for
+ * not fully specified or not yet decided upon features/behaviour.
+ * 
+ * 
+ * @author Jeanette Winzenburg
+ */
+public class JXErrorPaneIssues extends InteractiveTestCase {
+    public static void main(String[] args) {
+//      setSystemLF(true);
+      JXErrorPaneIssues test = new JXErrorPaneIssues();
+      try {
+        test.runInteractiveTests();
+      } catch (Exception e) {
+          System.err.println("exception when executing interactive tests:");
+          e.printStackTrace();
+      }
+
+    }
+
+    /**
+     * Issue #??-swingx: JXErrorPane can't cope with null errorInfo.
+     *
+     */
+    public void interactiveNPEWithNullErrorInfo() {
+        JXErrorPane errorPane = new JXErrorPane();
+        JXErrorPane.showDialog(null, errorPane);
+    }
+    
+    /**
+     * Issue #??-swingx: calling updateUI throws error.
+     *
+     */
+    public void interactiveUpdateUI() {
+        final JXErrorPane errorPane = new JXErrorPane();
+        // work around issue #??-swingx: errorPane must cope with null errorInfo.
+        errorPane.setErrorInfo(new ErrorInfo("title", "xxxx-yyy", null, null, null, null, null));
+        errorPane.updateUI();
+    }
+}
