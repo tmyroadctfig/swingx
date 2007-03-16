5e46e720c2a88db588ccf930a1a1927eb9b5a1f3
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-03-16 22:13:04 +0000

    Fixed setAlpha to fire property change events, added a little doc
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1855 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/painter/AlphaPainter.java
+++ src/java/org/jdesktop/swingx/painter/AlphaPainter.java
@@ -28,13 +28,17 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * Applies an alpha value to an entire stack of painters
+ * Applies an alpha value to an entire stack of painters.
+ *
  * @author joshy
  */
 public class AlphaPainter<T> extends CompoundPainter<T> {
     private float alpha = 1.0f;
-    
-    
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public void doPaint(Graphics2D g, T component, int width, int height) {
         Graphics2D g2 = (Graphics2D) g.create();
         if(getTransform() != null) {
@@ -54,29 +58,13 @@ public class AlphaPainter<T> extends CompoundPainter<T> {
         g2.dispose();
     }
     
-    public static void main(String ... args) {
-        JXPanel panel = new JXPanel();
-        AlphaPainter alpha = new AlphaPainter();
-        alpha.setAlpha(1f);
-        alpha.setPainters(new PinstripePainter(new Color(255,255,255,125),45,20,20));
-        
-        panel.setBackgroundPainter(new CompoundPainter(
-                new MattePainter(Color.RED),
-                alpha
-                ));
-        
-        JFrame frame = new JFrame();
-        frame.add(panel);
-        frame.pack();
-        frame.setSize(200,200);
-        frame.setVisible(true);
-    }
-
     public float getAlpha() {
         return alpha;
     }
 
     public void setAlpha(float alpha) {
+        float old = getAlpha();
         this.alpha = alpha;
+        firePropertyChange("alpha", old, getAlpha());
     }
 }
