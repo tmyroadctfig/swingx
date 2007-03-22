796fa605032bf3b04fab9246e2294dbab649c7b0
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-03-22 15:48:45 +0000

    Updated documentation
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@1919 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/painter/Painter.java
+++ src/java/org/jdesktop/swingx/painter/Painter.java
@@ -24,60 +24,77 @@ package org.jdesktop.swingx.painter;
 import java.awt.Graphics2D;
 
 /**
- * <p>Simple API for delegating painting. The JXPanel supports using this class
- * as a delegate for painting the background of the panel. This allows developers
- * to be able to customize the background painting of a JXPanel without having
- * to subclass it. Since many components within SwingX extend JXPanel, the
- * developer can implement custom painting on many parts of SwingX.</p>
- * 
- * <p>Painters can be combined together by using the CompoundPainter. CompoundPainter
- * uses an array to store several painters, and the order in which they should be
- * painted.</p>
- * 
- * <p>For example, if I want to create a CompoundPainter that started with a blue
- * background, had pinstripes on it running at a 45 degree angle, and those
- * pinstripes appeared to "fade in" from left to right, I would write the following:
- * <pre><code>
- *  Color blue = new Color(0x417DDD);
- *  Color translucent = new Color(blue.getRed(), blue.getGreen(), blue.getBlue(), 0);
- *  panel.setForeground(Color.LIGHT_GRAY);
- *  LinearGradientPaint blueToTranslucent = new LinearGradientPaint(
- *    new Point(0,0), new Point(10,0),
- *    new float[]{.4f, 1f},
- *    new Color[] { blue, translucent }
- *    );
- *  MattePainter veil =  new MattePainter(blueToTranslucent);
- *  veil.setPaintStretched(true);
- *  Painter pinstripes = new PinstripePainter(45);
- *  Painter backgroundPainter = new MattePainter(blue);
- *  Painter p = new CompoundPainter(backgroundPainter, pinstripes, veil);
- *  panel.setBackgroundPainter(p);
- * </code></pre></p>
- * 
- * <p>For convenience, AbstractPainter handles some basic painting chores and
- * should be extended for most concrete Painter implementations</p>
+ * <p>A painting delegate. The Painter interface defines exactly one method,
+ * <code>paint</code>. It is used in situations where the developer can change
+ * the painting routine of a component without having to resort to subclassing
+ * the component.</p>
  *
- * <p>TODO: I know I wrote this documentation, but it REALLY stinks. Needs to be rewritten.</p>
- * 
+ * <p><code>Painter</code>s are simply encapsulations of Java2D code and make
+ * it fairly trivial to reuse existing <code>Painter</code>s or to combine
+ * them together. Implementations of this interface are also trivial to write,
+ * such that if you can't find a <code>Painter</code> that does what you need,
+ * you can write one with minimal effort. Writing a <code>Painter</code> requires
+ * knowledge of Java2D.</p>
+ *
+ * <p>A <code>Painter</code> may be created with a type parameter. This type will be
+ * expected in the <code>paint</code> method. For example, you may wish to write a
+ * <code>Painter</code> that only works with subclasses of {@link java.awt.Component}.
+ * In that case, when the <code>Painter</code> is declared, you may declare that
+ * it requires a <code>Component</code>, allowing the paint method to be type safe. Ex:
+ * <pre><code>
+ *     Painter<Component> p = new Painter<Component>() {
+ *         public void paint(Graphics2D g, Component c, int width, int height) {
+ *             g.setColor(c.getBackground());
+ *             //and so forth
+ *         }
+ *     }
+ * </code></pre></p>
+ *
  * @author rbair
  * @see AbstractPainter
+ * @see CompoundPainter
+ * @see org.jdesktop.swingx.JXPanel
+ * @see org.jdesktop.swingx.JXLabel
+ * @see org.jdesktop.swingx.JXButton
  */
 public interface Painter<T> {
     /**
-     * <p>Paints on the given Graphics2D object to fill a portion
-     * of the area defined by the width and height. Painters do not
-     * have to fill the entire area and may also paint outside that area.
-     * 
-     * <p>The object parameter may be null. If the painter uses the object then
-     * it should check for null first.
-     * 
-     * <p>The Graphics2D object does not need to be returned to the same state it started
-     * at by the end of the method.</p>
-     * 
-     * @param g The Graphics2D object in which to paint. This must not be null.
-     * @param object The object that the Painter should paint. This may be null.
-     * @param width of the area to paint. Must be greater than zero.
-     * @param height of the area to paint. Must be greater than zero.
+     * <p>Renders to the given {@link java.awt.Graphics2D} object. Implementations
+     * of this method may modify state on the <code>Graphics2D</code>, and are not
+     * required to restore that state upon completion. In most cases, it is recommended
+     * that the caller pass in a scratch graphics object. The <code>Graphics2D</code>
+     * must never be null.</p>
+     *
+     * <p>The supplied object parameter acts as an optional configuration argument.
+     * For example, it could be of type <code>Component</code>. A <code>Painter</code>
+     * that expected it could then read state from that <code>Component</code> and
+     * use the state for painting. For example, an implementation may read the
+     * backgroundColor and use that.</p>
+     *
+     * <p>Generally, to enhance reusability, most standard <code>Painter</code>s ignore
+     * this parameter. They can thus be reused in any context. The <code>object</code>
+     * may be null. Implementations must not throw a NullPointerException if the object
+     * parameter is null.</p>
+     *
+     * <p>Finally, the <code>width</code> and <code>height</code> arguments specify the
+     * width and height that the <code>Painter</code> should paint into. More
+     * specifically, the specified width and height instruct the painter that it should
+     * paint fully within this width and height. Any specified clip on the
+     * <code>g</code> param will further constrain the region.</p>
+     *
+     * <p>For example, suppose I have a <code>Painter</code> implementation that draws
+     * a gradient. The gradient goes from white to black. It "stretches" to fill the
+     * painted region. Thus, if I use this <code>Painter</code> to paint a 500 x 500
+     * region, the far left would be black, the far right would be white, and a smooth
+     * gradient would be painted between. I could then, without modification, reuse the
+     * <code>Painter</code> to paint a region that is 20x20 in size. This region would
+     * also be black on the left, white on the right, and a smooth gradient painted
+     * between.</p>
+     *
+     * @param g The Graphics2D to render to. This must not be null.
+     * @param object an optional configuration parameter. This may be null.
+     * @param width width of the area to paint.
+     * @param height height of the area to paint.
      */
     public void paint(Graphics2D g, T object, int width, int height);
 }
