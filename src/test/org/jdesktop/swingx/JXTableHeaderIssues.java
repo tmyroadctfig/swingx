eec3e29b72d6fa9d6b87868e1194155ad8706101
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-06-27 12:28:48 +0000

    added quick example how to prevent reordering if "docked" column
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2077 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXTableHeaderIssues.java
+++ src/test/org/jdesktop/swingx/JXTableHeaderIssues.java
@@ -5,27 +5,136 @@
 package org.jdesktop.swingx;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.util.List;
+import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 import org.jdesktop.swingx.decorator.SortOrder;
+import org.jdesktop.swingx.table.DefaultTableColumnModelExt;
+import org.jdesktop.swingx.table.TableColumnExt;
 
 public class JXTableHeaderIssues extends JXTableHeaderTest {
+    @SuppressWarnings("all")
+    private static final Logger LOG = Logger
+            .getLogger(JXTableHeaderIssues.class.getName());
     public static void main(String args[]) {
         JXTableHeaderIssues test = new JXTableHeaderIssues();
+        setSystemLF(true);
         try {
-          test.runInteractiveTests();
+//          test.runInteractiveTests();
          //   test.runInteractiveTests("interactive.*Siz.*");
          //   test.runInteractiveTests("interactive.*Render.*");
-         //   test.runInteractiveTests("interactive.*Toggle.*");
+            test.runInteractiveTests("interactive.*Dock.*");
         } catch (Exception e) {
             System.err.println("exception when executing interactive tests:");
             e.printStackTrace();
         } 
     }
 
+    /**
+     * Quick proof-of-concept: JXTableHeader can go dirty and
+     * suppress moving of "docked" column.
+     *
+     */
+    public void interactiveDockColumn() {
+        final JXTableHeader header = new JXTableHeader() {
+
+            private int oldDistance;
+
+
+            @Override
+            public void setDraggedDistance(int distance) {
+                oldDistance = getDraggedDistance();
+                if (isDocked(getDraggedColumn())) {
+                    distance = 0;
+                } 
+                if (isDraggedOverDocked(distance)) {
+                    distance = 0;
+                    setDraggedColumn(null);
+                }
+                super.setDraggedDistance(distance);
+            }
+
+            private boolean isDraggedOverDocked(int distance) {
+                if (getDraggedColumn() == null) return false;
+                int dragPosition = getDragX(distance);
+                int columnUnder = columnAtPoint(new Point(dragPosition, 0));
+                if (columnUnder >= 0) {
+                    return  isDocked(getColumnModel().getColumn(columnUnder));
+                }
+                return false;
+            }
+            
+            private int getDragX(int distance) {
+                DefaultTableColumnModelExt columnModel = (DefaultTableColumnModelExt) getColumnModel();
+                List<TableColumn> columns = columnModel.getColumns(false);
+                int startX = 0;
+                for (int i = 0; i < columns.size(); i++) {
+                    if (columns.get(i) == getDraggedColumn()) {
+                        // at the wrong column the very moment 
+                        // before the dock neighbour is 
+                        // actually replaced with the second next dock neighbour
+                        // need to add the stop-column width as well
+                        if (distance * oldDistance < 0) {
+                            startX += columns.get(i).getWidth();
+                        }
+                         break;
+                    }
+                    startX += columns.get(i).getWidth();
+                }
+                return startX + distance;
+            }
+
+            
+            private boolean isDocked(TableColumn column) {
+                if (column instanceof TableColumnExt) {
+                    return Boolean.TRUE.equals(((TableColumnExt) column).getClientProperty("docked"));
+                }
+                return false;
+            }
+        };
+        final TableColumnModel cModel = new DefaultTableColumnModelExt() {
+
+            @Override
+            public void moveColumn(int columnIndex, int newIndex) {
+                if (isDocked(columnIndex) || isDocked(newIndex)) return;
+                super.moveColumn(columnIndex, newIndex);
+            }
+
+            private boolean isDocked(int columnIndex) {
+                TableColumnExt column = getColumnExt(columnIndex);
+                if (column != null) {
+                    return Boolean.TRUE.equals(column.getClientProperty("docked"));
+                }
+                return false;
+            }
+            
+        };
+        JXTable table = new JXTable(10, 5) {
+
+            @Override
+            protected JTableHeader createDefaultTableHeader() {
+                header.setColumnModel(columnModel);
+                return header;
+            }
+
+            @Override
+            protected TableColumnModel createDefaultColumnModel() {
+                return cModel;
+            }
+            
+            
+        };
+        table.getColumnExt(0).putClientProperty("docked", Boolean.TRUE);
+        showWithScrollingInFrame(table, "block dragging");
+    }
 
     /**
      * Issue #281-swingx, Issue #334-swing: 
