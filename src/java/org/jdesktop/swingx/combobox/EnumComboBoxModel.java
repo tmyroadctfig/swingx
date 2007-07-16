25324fc2460812d5e98a86378ca0f87534a86401
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-07-16 13:42:08 +0000

    Issue number:  303 Reworked the internals of the EnumComboBoxModel to allow it to convert Strings (like user input) into enums.  Original plan was to use Enum.valueOf(enumClass, string), but that does not work with custom toString implementations, where the name of the enum should not work.  I added a map for translate the string to an enum. Replaced the Issues class with a UnitTest and VisualCheck, since there are currently no unresolved issues.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2122 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/combobox/EnumComboBoxModel.java
+++ src/java/org/jdesktop/swingx/combobox/EnumComboBoxModel.java
@@ -22,94 +22,162 @@ package org.jdesktop.swingx.combobox;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
 import javax.swing.AbstractListModel;
-import javax.swing.BoxLayout;
 import javax.swing.ComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
 
 /**
- * <p>A ComboBoxModel implementation that safely wraps an Enum. It allows the
+ * <p>
+ * A ComboBoxModel implementation that safely wraps an Enum. It allows the
  * developer to directly use an enum as their model for a combobox without any
- * extra work, though the display can can be further customized. </p>
- *
+ * extra work, though the display can can be further customized.
+ * </p>
+ * 
  * <h4>Simple Usage</h4>
  * 
- * <p>The simplest usage is to wrap an <code>enum</code> inside the 
- * <code>EnumComboBoxModel</code> and then set it as the model on the
- * combo box. The combo box will then appear on screen with each value 
- * in the <code>enum</code> as a value in the combobox.
+ * <p>
+ * The simplest usage is to wrap an <code>enum</code> inside the
+ * <code>EnumComboBoxModel</code> and then set it as the model on the combo
+ * box. The combo box will then appear on screen with each value in the
+ * <code>enum</code> as a value in the combobox.
  * </p>
- * <p>ex:</p>
+ * <p>
+ * ex:
+ * </p>
+ * 
  * <pre><code>
  *  enum MyEnum { GoodStuff, BadStuff };
  *  ...
  *  JComboBox combo = new JComboBox();
  *  combo.setModel(new EnumComboBoxModel(MyEnum.class));
  * </code></pre>
- *
+ * 
  * <h4>Type safe access</h4>
- * <p>By using generics and co-variant types you can make accessing elements from the model
- * be completely typesafe. ex:
- *</p>
- *
- *<pre><code>
- *  EnumComboBoxModel<MyEnum> enumModel = new EnumComboBoxModel<MyEnum1>(MyEnum1.class);
- *  MyEnum first = enumModel.getElement(0);
- *  MyEnum selected = enumModel.getSelectedItem();
- *</code></pre>
- *
+ * <p>
+ * By using generics and co-variant types you can make accessing elements from
+ * the model be completely typesafe. ex:
+ * </p>
+ * 
+ * <pre><code>
+ * EnumComboBoxModel&lt;MyEnum&gt; enumModel = new EnumComboBoxModel&lt;MyEnum1&gt;(
+ *         MyEnum1.class);
+ * 
+ * MyEnum first = enumModel.getElement(0);
+ * 
+ * MyEnum selected = enumModel.getSelectedItem();
+ * </code></pre>
+ * 
  * <h4>Advanced Usage</h4>
- * <p>Since the exact <code>toString()</code> value of each enum constant 
- * may not be exactly what you want on screen (the values 
- * won't have spaces, for example) you can override to 
- * toString() method on the values when you declare your 
- * enum. Thus the display value is localized to the enum 
- * and not in your GUI code. ex:
+ * <p>
+ * Since the exact <code>toString()</code> value of each enum constant may not
+ * be exactly what you want on screen (the values won't have spaces, for
+ * example) you can override to toString() method on the values when you declare
+ * your enum. Thus the display value is localized to the enum and not in your
+ * GUI code. ex:
+ * 
  * <pre><code>
  *    private enum MyEnum {GoodStuff, BadStuff;
  *        public String toString() {
  *           switch(this) {
- *               case GoodStuff: return "Some Good Stuff";
- *               case BadStuff: return "Some Bad Stuff";
+ *               case GoodStuff: return &quot;Some Good Stuff&quot;;
+ *               case BadStuff: return &quot;Some Bad Stuff&quot;;
  *           }
- *           return "ERROR";
+ *           return &quot;ERROR&quot;;
  *        }
  *    };
  * </code></pre>
  * 
- *
+ * Note: if more than one enum constant returns the same {@code String} via
+ * {@code toString()}, this model will throw an exception on creation.
+ * 
  * @author joshy
+ * @author Karl Schaefer
  */
 public class EnumComboBoxModel<E extends Enum<E>> 
         extends AbstractListModel implements ComboBoxModel {
+    private static final long serialVersionUID = 2176566393195371004L;
+    
+    private final Map<String, E> valueMap;
+    private final Class<E> enumClass;
+    private final List<E> quickList;
     private E selected = null;
-    private List<E> list;
 
+    /**
+     * Creates an {@code EnumComboBoxModel} for the enum represent by the
+     * {@code Class} {@code en}.
+     * 
+     * @param en
+     *            the enum class type
+     * @throws IllegalArgumentException
+     *             if the {@code Enum.toString} returns the same value for more
+     *             than one constant
+     */
     public EnumComboBoxModel(Class<E> en) {
+        //we could size these, probably not worth it; enums are usually small 
+        valueMap = new HashMap<String, E>();
+        quickList = new ArrayList<E>();
+        enumClass = en;
+        
         EnumSet<E> ens = EnumSet.allOf(en);
-        list = new ArrayList<E>(ens);
-        selected = list.get(0);
-    }
-
-    public int getSize() {
-        return list.size();
-    }
-
-    public E getElementAt(int index) {
-        return list.get(index);
-    }
-
-    public void setSelectedItem(Object anItem) {
-        try {
-            selected = (E)anItem;
-        } catch (ClassCastException e) {
-            throw new IllegalArgumentException(anItem + " not of the expected type", e);
+        Iterator<E> iter = ens.iterator();
+        
+        while (iter.hasNext()) {
+            E element = iter.next();
+            String s = element.toString();
+            
+            if (valueMap.containsKey(s)) {
+                throw new IllegalArgumentException(
+                        "multiple constants map to one string value");
+            }
+            
+            valueMap.put(s, element);
+            quickList.add(element);
         }
-        this.fireContentsChanged(this,0,getSize());
+        
+        selected = quickList.get(0);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getSize() {
+        return quickList.size();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public E getElementAt(int index) {
+        return quickList.get(index);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public void setSelectedItem(Object anItem) {
+        E input = null;
+        
+        if (enumClass.isInstance(anItem)) {
+            input = (E) anItem;
+        } else {
+            input = valueMap.get(anItem);
+        }
+        
+        if (input != null) {
+            selected = input;
+        }
+        
+        this.fireContentsChanged(this, 0, getSize());
     }
     
+    /**
+     * {@inheritDoc}
+     */
     public E getSelectedItem() {
 	return selected;
     }
