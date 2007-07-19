f5766130afefc31fcd6025f3539edb0a5680c4f3
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-07-19 13:21:50 +0000

    cont'd date picker cell editor - added tests to JXDatePickerIssues (characterize behaviour, #235-swingx)
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2135 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXDatePickerIssues.java
+++ src/test/org/jdesktop/swingx/JXDatePickerIssues.java
@@ -21,10 +21,22 @@
  */
 package org.jdesktop.swingx;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.logging.Logger;
+
 import javax.swing.ComboBoxEditor;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.plaf.basic.BasicComboBoxEditor;
+
+import org.jdesktop.swingx.calendar.JXMonthView;
+import org.jdesktop.test.ActionReport;
 
 /**
  * Known issues of <code>JXDatePicker</code>.
@@ -32,7 +44,106 @@ import javax.swing.plaf.basic.BasicComboBoxEditor;
  * @author Jeanette Winzenburg
  */
 public class JXDatePickerIssues extends InteractiveTestCase {
+    private static final Logger LOG = Logger.getLogger(JXDatePickerIssues.class
+            .getName());
+    public static void main(String[] args) {
+        setSystemLF(true);
+        JXDatePickerIssues  test = new JXDatePickerIssues();
+        try {
+            test.runInteractiveTests();
+//          test.runInteractiveTests(".*Text.*");
+        } catch (Exception e) {
+            System.err.println("exception when executing interactive tests:");
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * Issue #235-swingx: action events
+     * 
+     * Compare textfield, formatted and picker.
+     * - simple field fires on enter always
+     * - formatted (and picker) fire on enter if value had been edited
+     *
+     * Picker
+     * - fires on click into monthView only if clicked date different
+     * - doesn't close monthview on enter/escape if unchanged
+     * 
+     * MonthView
+     * - fires on click always
+     * - fires on esc/enter only if selection changed
+     * 
+     */
+    public void interactiveActionEvent() {
+        JXDatePicker picker = new JXDatePicker();
+        JTextField simpleField = new JTextField("simple field");
+        JFormattedTextField textField = new JFormattedTextField(DateFormat.getDateInstance());
+        textField.setValue(new Date());
+        ActionListener l = new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                LOG.info("got action from: " + e.getSource().getClass().getName());
+            }
+            
+        };
+        simpleField.addActionListener(l);
+        textField.addActionListener(l);
+        picker.addActionListener(l);
+        picker.getMonthView().addActionListener(l);
+        JPanel panel = new JPanel();
+        panel.add(simpleField);
+        panel.add(textField);
+        panel.add(picker);
+        JXFrame frame = showInFrame(panel, "trace action events");
+        // JXRootPane eats esc 
+        frame.getRootPaneExt().getActionMap().remove("esc-action");
+    }
+
+    /**
+     * Issue #99-swingx: null date and opening popup forces selection.
+     * Status? Looks fixed..
+     *
+     */
+    public void interactiveNullDate() {
+        JXDatePicker picker = new JXDatePicker();
+        picker.setDate(null);
+        showInFrame(picker, "null date");
+    }
+ 
+//-------------------- unit tests
+    
+    /**
+     * sanity: when does the picker fire an action event?
+     * @throws ParseException
+     */
+    public void testDatePickerFire() throws ParseException {
+        JXDatePicker picker = new JXDatePicker();
+        ActionReport report = new ActionReport();
+        picker.addActionListener(report);
+        // fires on setDate
+        picker.setDate(null);
+        assertEquals(1, report.getEventCount());
+    }
+
+
+
+    /**
+     * Who rules? the picker or the month view?
+     * Neither - the month view's dateSelectionModel.
+     * 
+     * Setting the monthview resets the 
+     * picker's date to the view's selected - should be documented?
+     *
+     */
+    public void testDatePickerSetMonthView() {
+        JXDatePicker picker = new JXDatePicker();
+        Date date = picker.getDate();
+        // why? null selection is perfectly valid?
+        assertNotNull(date);
+        picker.setMonthView(new JXMonthView());
+        assertEquals(date, picker.getDate());
+    }
+    
     /**
      * Allowed to set a null editor? No (which is reasonable) 
      * - should be documented as of SwingX doc convention.
