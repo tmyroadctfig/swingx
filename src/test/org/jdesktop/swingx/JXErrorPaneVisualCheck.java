12db4a8da282489358aaca6f8e88398c6327ee41
rah003 <rah003@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-08-14 07:24:23 +0000

    Issue number:  #45-swinglabs Added visual check to see whether text is no longer painted over action buttons.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2239 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/test/org/jdesktop/swingx/JXErrorPaneVisualCheck.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ *
+ * Copyright 2004 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ */
+
+package org.jdesktop.swingx;
+
+import java.util.logging.Level;
+
+import org.jdesktop.swingx.error.ErrorInfo;
+
+/**
+ * A unit test for the JXErrorPane
+ *
+ * @author rah003
+ */
+public class JXErrorPaneVisualCheck extends JXEditorPaneTest {
+
+    public static void main(String[] args) throws Exception {
+      JXErrorPaneVisualCheck test = new JXErrorPaneVisualCheck();
+      try {
+          test.runInteractiveTests();
+        } catch (Exception e) {
+            System.err.println("exception when executing interactive tests:");
+            e.printStackTrace();
+        } 
+  }
+    /**
+     * Issue #45-swinglabs: JXErrorPane paints message text over action buttons 
+     *
+     */
+    public void interactiveLongMessageText() {
+        ErrorInfo errorInfo = new ErrorInfo("Server Error",
+                "The request cannot be carried out\n1\n2\n3\n4\n5\n6\n7" +
+                "\n8\n9\n0\n1\n2\n3\n4\nThis text should be shown in scroll pane.", "Server Error",
+                null, new Exception(), Level.SEVERE, null);
+        JXErrorPane.showDialog(null,errorInfo );
+    }
+}
