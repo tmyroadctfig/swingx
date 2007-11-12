cd51e378a58e15d1c01c015f0de2294da15b2e04
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-11-12 00:56:55 +0000

    New interactive check for AutoComplete.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2416 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/test/org/jdesktop/swingx/autocomplete/AutoCompleteDecoratorVisualCheck.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.jdesktop.swingx.autocomplete;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+import org.jdesktop.swingx.InteractiveTestCase;
+
+/**
+ * @author Karl George Schaefer
+ */
+public class AutoCompleteDecoratorVisualCheck extends InteractiveTestCase {
+    public static void main(String[] args) throws Exception {
+        AutoCompleteDecoratorVisualCheck test = new AutoCompleteDecoratorVisualCheck();
+        try {
+            test.runInteractiveTests();
+        } catch (Exception e) {
+            System.err.println("exception when executing interactive tests:");
+            e.printStackTrace();
+        }
+    }
+
+
+    public void interactiveCompletionAtTopTest() {
+        JComboBox combo = new JComboBox(new String[] {
+                "A1", "A2", "A3", "A4", "A5",
+                "B1", "B2", "B3", "B4", "B5",
+                "C1", "C2", "C3", "C4", "C5",
+                "D1", "D2", "D3", "D4", "D5",
+        });
+        
+        AutoCompleteDecorator.decorate(combo);
+        
+        combo.addItemListener(new ItemListener() {
+
+            public void itemStateChanged(ItemEvent e) {
+                System.err.println(e);
+                
+                JComboBox box = (JComboBox) e.getSource();
+                for (ItemListener l : box.getItemListeners()) {
+                    System.err.println(l);
+                }
+            }
+            
+        });
+        
+        JFrame frame = wrapInFrame(combo, "show combo ");
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
