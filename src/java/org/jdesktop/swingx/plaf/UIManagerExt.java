e6f2f6a43510d9dc74cbd9bd1854c242bff88780
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-11-21 05:10:26 +0000

    Added more safe methods.  Increased the documentation, especially relating to the resource bundles.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2466 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/plaf/UIManagerExt.java
+++ src/java/org/jdesktop/swingx/plaf/UIManagerExt.java
@@ -1,10 +1,29 @@
-/**
+/*
+ * $Id$
+ *
+ * Copyright 2007 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package org.jdesktop.swingx.plaf;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Insets;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Locale;
@@ -13,8 +32,10 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.Vector;
 
+import javax.swing.Icon;
 import javax.swing.UIDefaults;
 import javax.swing.UIManager;
+import javax.swing.border.Border;
 
 import org.jdesktop.swingx.painter.Painter;
 import org.jdesktop.swingx.util.Contract;
@@ -23,6 +44,16 @@ import org.jdesktop.swingx.util.Contract;
  * A utility class for obtaining configuration properties from the
  * {@code UIDefaults}. This class handles SwingX-specific L&F needs, such as
  * the installation of painters.
+ * <p>
+ * SwingX supports dynamic localization updates and the {@code UIManagerExt}
+ * class enables this. The {@linkplain UIDefaults#addResourceBundle(String)}
+ * allows resource bundles to be added to the {@code UIDefaults}. There is a
+ * bug with the class loader that prevents user added bundles from working
+ * correctly when used via Web Start. Therefore, {@code UIManagerExt} defines
+ * methods to add and remove resource bundles. These are the only methods that
+ * SwingX classes should use when adding resource bundles to the defaults. Since
+ * {@code UIManagerExt} is maintaining the bundles, any localized {@code String}s
+ * <b>must</b> be retrieved from the {@code getString} methods in this class.
  * <p>
  * The {@code getSafeXXX} methods are designed for use with
  * {@code LookAndFeelAddon}s. Any addon that attempts to obtain a property
@@ -36,6 +67,10 @@ import org.jdesktop.swingx.util.Contract;
  * @see UIDefaults
  */
 public class UIManagerExt {
+    /**
+     * Used to replicate the resource bundle behavior from the
+     * {@code UIDefaults}.
+     */
     private static class UIDefaultsExt {
         //use vector; we want synchronization
         private Vector<String> resourceBundles;
@@ -131,18 +166,62 @@ public class UIManagerExt {
         //does nothing
     }
     
+    /**
+     * Adds a resource bundle to the list of resource bundles that are searched
+     * for localized values. Resource bundles are searched in the reverse order
+     * they were added. In other words, the most recently added bundle is
+     * searched first.
+     * 
+     * @param bundleName
+     *                the base name of the resource bundle to be added
+     * @see java.util.ResourceBundle
+     * @see #removeResourceBundle
+     */
     public static void addResourceBundle(String bundleName) {
         uiDefaultsExt.addResourceBundle(bundleName);
     }
     
+    /**
+     * Removes a resource bundle from the list of resource bundles that are
+     * searched for localized defaults.
+     * 
+     * @param bundleName
+     *                the base name of the resource bundle to be removed
+     * @see java.util.ResourceBundle
+     * @see #addResourceBundle
+     */
     public static void removeResourceBundle(String bundleName) {
         uiDefaultsExt.removeResourceBundle(bundleName);
     }
     
+    /**
+     * Returns a string from the defaults. If the value for {@code key} is not a
+     * {@code String}, {@code null} is returned.
+     * 
+     * @param key
+     *                an {@code Object} specifying the string
+     * @return the {@code String} object
+     * @throws NullPointerException
+     *                 if {@code key} is {@code null}
+     */
     public static String getString(Object key) {
         return getString(key, null);
     }
     
+    /**
+     * Returns a string from the defaults. If the value for {@code key} is not a
+     * {@code String}, {@code null} is returned.
+     * 
+     * @param key
+     *                an {@code Object} specifying the string
+     * @param l
+     *                the {@code Locale} for which the painter is desired; refer
+     *                to {@code UIDefaults} for details on how a {@code null}
+     *                {@code Locale} is handled
+     * @return the {@code String} object
+     * @throws NullPointerException
+     *                 if {@code key} is {@code null}
+     */
     public static String getString(Object key, Locale l) {
         String value = UIManager.getString(key, l);
         
@@ -189,6 +268,31 @@ public class UIManagerExt {
     }
     
     /**
+     * Returns a border from the defaults. If the value for {@code key} is not a
+     * {@code Border}, {@code defaultBorder} is returned.
+     * 
+     * @param key
+     *                an {@code Object} specifying the border
+     * @param defaultBorder
+     *                the border to return if the border specified by
+     *                {@code key} does not exist
+     * @return the {@code Border} object
+     * @throws NullPointerException
+     *                 if {@code key} or {@code defaultBorder} is {@code null}
+     */
+    public static Border getSafeBorder(Object key, Border defaultBorder) {
+        Contract.asNotNull(defaultBorder, "defaultBorder cannot be null");
+        
+        Border safeBorder = UIManager.getBorder(key);
+        
+        if (safeBorder == null) {
+            safeBorder = defaultBorder;
+        }
+        
+        return safeBorder;
+    }
+    
+    /**
      * Returns a color from the defaults. If the value for {@code key} is not a
      * {@code Color}, {@code defaultColor} is returned.
      * 
@@ -214,6 +318,31 @@ public class UIManagerExt {
     }
     
     /**
+     * Returns a dimension from the defaults. If the value for {@code key} is
+     * not a {@code Dimension}, {@code defaultDimension} is returned.
+     * 
+     * @param key
+     *                an {@code Object} specifying the dimension
+     * @param defaultDimension
+     *                the dimension to return if the dimension specified by
+     *                {@code key} does not exist
+     * @return the {@code Dimension} object
+     * @throws NullPointerException
+     *                 if {@code key} or {@code defaultColor} is {@code null}
+     */
+    public static Dimension getSafeDimension(Object key, Dimension defaultDimension) {
+        Contract.asNotNull(defaultDimension, "defaultDimension cannot be null");
+        
+        Dimension safeDimension = UIManager.getDimension(key);
+        
+        if (safeDimension == null) {
+            safeDimension = defaultDimension;
+        }
+        
+        return safeDimension;
+    }
+    
+    /**
      * Returns a font from the defaults. If the value for {@code key} is not a
      * {@code Font}, {@code defaultFont} is returned.
      * 
@@ -236,5 +365,55 @@ public class UIManagerExt {
         }
         
         return safeFont;
+    }
+    
+    /**
+     * Returns an icon from the defaults. If the value for {@code key} is not a
+     * {@code Icon}, {@code defaultIcon} is returned.
+     * 
+     * @param key
+     *                an {@code Object} specifying the icon
+     * @param defaultIcon
+     *                the icon to return if the icon specified by {@code key}
+     *                does not exist
+     * @return the {@code Icon} object
+     * @throws NullPointerException
+     *                 if {@code key} or {@code defaultIcon} is {@code null}
+     */
+    public static Icon getSafeIcon(Object key, Icon defaultIcon) {
+        Contract.asNotNull(defaultIcon, "defaultIcon cannot be null");
+        
+        Icon safeIcon = UIManager.getIcon(key);
+        
+        if (safeIcon == null) {
+            safeIcon = defaultIcon;
+        }
+        
+        return safeIcon;
+    }
+    
+    /**
+     * Returns an insets from the defaults. If the value for {@code key} is not
+     * a {@code Insets}, {@code defaultInsets} is returned.
+     * 
+     * @param key
+     *                an {@code Object} specifying the insets
+     * @param defaultInsets
+     *                the insets to return if the insets specified by
+     *                {@code key} does not exist
+     * @return the {@code Insets} object
+     * @throws NullPointerException
+     *                 if {@code key} or {@code defaultInsets} is {@code null}
+     */
+    public static Insets getSafeInsets(Object key, Insets defaultInsets) {
+        Contract.asNotNull(defaultInsets, "defaultInsets cannot be null");
+        
+        Insets safeInsets = UIManager.getInsets(key);
+        
+        if (safeInsets == null) {
+            safeInsets = defaultInsets;
+        }
+        
+        return safeInsets;
     }
 }
