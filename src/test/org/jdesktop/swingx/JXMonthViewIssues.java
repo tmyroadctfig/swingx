b49c32463a59100b08214fae4045c1098cf39199
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-01-11 17:50:23 +0000

    JXMonthView: moved visual tests for fixed issues
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2587 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXMonthViewIssues.java
+++ src/test/org/jdesktop/swingx/JXMonthViewIssues.java
@@ -21,38 +21,25 @@
  */
 package org.jdesktop.swingx;
 
-import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.SortedSet;
 import java.util.TimeZone;
 import java.util.logging.Logger;
 
 import javax.swing.Action;
-import javax.swing.Box;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.tree.DefaultMutableTreeNode;
 
 import org.jdesktop.swingx.JXMonthView.SelectionMode;
 import org.jdesktop.swingx.action.AbstractActionExt;
 import org.jdesktop.swingx.calendar.CalendarUtils;
-import org.jdesktop.swingx.combobox.ListComboBoxModel;
 import org.jdesktop.swingx.event.DateSelectionEvent.EventType;
 import org.jdesktop.swingx.test.DateSelectionReport;
-import org.jdesktop.swingx.test.XTestUtils;
 import org.jdesktop.test.PropertyChangeReport;
 import org.jdesktop.test.TestUtils;
 
@@ -80,10 +67,10 @@ public class JXMonthViewIssues extends InteractiveTestCase {
       setSystemLF(true);
       JXMonthViewIssues  test = new JXMonthViewIssues();
       try {
-//          test.runInteractiveTests();
+          test.runInteractiveTests();
 //        test.runInteractiveTests("interactive.*Locale.*");
 //          test.runInteractiveTests("interactive.*AutoScroll.*");
-        test.runInteractiveTests("interactive.*UpdateUI.*");
+//        test.runInteractiveTests("interactive.*UpdateUI.*");
       } catch (Exception e) {
           System.err.println("exception when executing interactive tests:");
           e.printStackTrace();
@@ -113,28 +100,6 @@ public class JXMonthViewIssues extends InteractiveTestCase {
         };
         addAction(frame, action);
         frame.pack();
-    };
-
-    /**
-     * #702-swingx: no days shown?
-     * 
-     * Not reproducible.
-     */
-    public void interactiveBlankMonthViewOnAdd() {
-       final JComponent comp = Box.createHorizontalBox();
-       comp.add(new JXMonthView());
-       final JXFrame frame = wrapInFrame(comp, "blank view on add");
-       Action next = new AbstractActionExt("new monthView") {
-
-           public void actionPerformed(ActionEvent e) {
-               comp.add(new JXMonthView());
-               frame.pack();
-           }
-           
-       };
-       addAction(frame, next);
-       frame.pack();
-       frame.setVisible(true);
     };
 
     
@@ -213,382 +178,6 @@ public class JXMonthViewIssues extends InteractiveTestCase {
         frame.setVisible(true);
     }
     
-    /**
-     * #703-swingx: set date to first of next doesn't update the view.
-     * 
-     * Behaviour is consistent with core components. Except that it is doing 
-     * too much: revalidate most probably shouldn't change the scrolling state?
-     * 
-     */
-    public void interactiveAutoScrollOnSelectionMonthView() {
-        final JXMonthView us = new JXMonthView();
-        us.setSelectionMode(JXMonthView.SelectionMode.SINGLE_INTERVAL_SELECTION);
-        final Calendar today = Calendar.getInstance();
-        CalendarUtils.endOfMonth(today);
-        today.add(Calendar.DAY_OF_MONTH, 1);
-        us.setSelectedDate(today.getTime());
-        JXFrame frame = wrapInFrame(us, "first day of next month");
-        Action nextMonthInterval = new AbstractActionExt("next month interval") {
-
-            public void actionPerformed(ActionEvent e) {
-                if (us.isSelectionEmpty()) return;
-                today.setTime(us.getSelectedDate());
-                today.add(Calendar.DAY_OF_MONTH, -20);
-                Date start = today.getTime();
-                today.add(Calendar.DAY_OF_MONTH, +40);
-                us.setSelectionInterval(start, today.getTime());
-                // shouldn't effect scrolling state
-                us.revalidate();
-                // client code must trigger 
-//                us.ensureDateVisible(start.getTime());
-            }
-            
-        };
-        addAction(frame, nextMonthInterval);
-        Action next = new AbstractActionExt("next month") {
-
-            public void actionPerformed(ActionEvent e) {
-                if (us.isSelectionEmpty()) return;
-                if (!CalendarUtils.isEndOfMonth(today)) {
-                    CalendarUtils.endOfMonth(today);
-                    
-                }
-                today.add(Calendar.DAY_OF_MONTH, 1);
-                us.setSelectedDate(today.getTime());
-                LOG.info("firstDisplayed before: " + new Date(us.getFirstDisplayedDate()));
-                
-                // shouldn't effect scrolling state
-                us.revalidate();
-                LOG.info("firstDisplayed: " + new Date(us.getFirstDisplayedDate()));
-                // client code must trigger 
-//                us.ensureDateVisible(today.getTimeInMillis());
-            }
-            
-        };
-        addAction(frame, next);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    /**
-     * #703-swingx: select date doesn't ensure visibility of selected.
-     * 
-     * compare with core list: doesn't scroll as well.
-     * 
-     */
-    public void interactiveAutoScrollOnSelectionList() {
-        // add hoc model
-        SortedSet<Date> dates = getDates();
-        
-        final JXList us = new JXList(new ListComboBoxModel<Date>(new ArrayList<Date>(dates)));
-        us.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        JXFrame frame = wrapWithScrollingInFrame(us, "list - autoscroll on selection");
-        Action next = new AbstractActionExt("select last + 1") {
-
-            public void actionPerformed(ActionEvent e) {
-                int last = us.getLastVisibleIndex();
-                us.setSelectedIndex(last + 1);
-                // shouldn't effect scrolling state
-                us.revalidate();
-                // client code must trigger 
-//                us.ensureIndexIsVisible(last+1);
-            }
-            
-        };
-        addAction(frame, next);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    /**
-     * #703-swingx: set date to first of next doesn't "scroll".
-     * 
-     * compare with core tree: doesn't scroll as well.
-     * 
-     */
-    public void interactiveAutoScrollOnSelectionTree() {
-        // add hoc model
-        SortedSet<Date> dates = getDates();
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("dates");
-        for (Date date : dates) {
-            root.add(new DefaultMutableTreeNode(date));
-        }
-        
-        final JXTree us = new JXTree(root);
-        JXFrame frame = wrapWithScrollingInFrame(us, "tree - autoscroll on selection");
-        Action next = new AbstractActionExt("select last + 1") {
-
-            public void actionPerformed(ActionEvent e) {
-                int last = us.getLeadSelectionRow();
-                us.setSelectionRow(last + 1);
-                // shouldn't effect scrolling state
-                us.revalidate();
-                // client code must trigger 
-//                us.scrollRowToVisible(last + 1);
-            }
-            
-        };
-        addAction(frame, next);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    /**
-     * #703-swingx: set date to first of next doesn't "scroll".
-     * 
-     * compare with core tree: doesn't scroll as well.
-     * 
-     */
-    public void interactiveAutoScrollOnSelectionTable() {
-        // add hoc model
-        SortedSet<Date> dates = getDates();
-        DefaultTableModel model = new DefaultTableModel(0, 1);
-        for (Date date : dates) {
-            model.addRow(new Object[] {date});
-        }
-        
-        final JXTable us = new JXTable(model);
-        JXFrame frame = wrapWithScrollingInFrame(us, "table - autoscroll on selection");
-        Action next = new AbstractActionExt("select last + 1") {
-
-            public void actionPerformed(ActionEvent e) {
-                int last = us.getSelectedRow();
-                us.setRowSelectionInterval(last + 1, last + 1);
-                // shouldn't effect scrolling state
-                us.revalidate();
-                // client code must trigger 
-//                us.scrollRowToVisible(last + 1);
-            }
-            
-        };
-        addAction(frame, next);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    /**
-     * Convenience to get a bunch of dates.
-     * 
-     * @return
-     */
-    private SortedSet<Date> getDates() {
-        JXMonthView source = new JXMonthView();
-        source.setSelectionMode(JXMonthView.SelectionMode.SINGLE_INTERVAL_SELECTION);
-        final Calendar today = Calendar.getInstance();
-        Date start = today.getTime();
-        today.add(Calendar.DAY_OF_MONTH, +40);
-        source.setSelectionInterval(start, today.getTime());
-        SortedSet<Date> dates = source.getSelection();
-        return dates;
-    }
-    
-    
-    /**
-     * #681-swingx: first row overlaps days.
-     * 
-     * Looks like a problem with the constructor taking a locale? 
-     * Default is okay (even if German), US is okay, explicit german is wrong.
-     */
-    public void interactiveFirstRowOfMonthSetLocale() {
-        JPanel p = new JPanel();
-        // default constructor
-        p.add(new JXMonthView());
-        // explicit us locale
-        JXMonthView us = new JXMonthView();
-        us.setLocale(Locale.US);
-        p.add(us);
-        // explicit german locale
-        JXMonthView german = new JXMonthView();
-        german.setLocale(Locale.GERMAN);
-        p.add(german);
-        showInFrame(p, "first row overlapping - setLocale");
-    }
-
-   
-    /**
-     * #681-swingx: first row overlaps days.
-     * 
-     * Looks like a problem with the constructor taking a locale? 
-     * Default is okay (even if German), US is okay, explicit german is wrong.
-     */
-    public void interactiveFirstRowOfMonthLocaleConstructor() {
-        JPanel p = new JPanel();
-        // default constructor
-        p.add(new JXMonthView());
-        // explicit us locale
-        p.add(new JXMonthView(Locale.US));
-//         explicit german locale
-        p.add(new JXMonthView(Locale.GERMAN));
-        showInFrame(p, "first row overlapping - constructor");
-    }
-    /**
-     * #681-swingx: first row overlaps days.
-     * Here everything looks okay.
-     * 
-     * @see #interactiveFirstRowOfMonthLocaleDependent()
-     */
-    public void interactiveFirstRowOfMonth() {
-        JXMonthView monthView = new JXMonthView();
-        calendar.set(2008, 1, 1);
-        monthView.setSelectedDate(calendar.getTime());
-        showInFrame(monthView, "first row");
-    }
-
-    /**
-     * Issue #618-swingx: JXMonthView displays problems with non-default
-     * timezones.
-     * 
-     */
-    public void interactiveUpdateOnTimeZone() {
-        JPanel panel = new JPanel();
-
-        final JComboBox zoneSelector = new JComboBox(TimeZone.getAvailableIDs());
-        final JXDatePicker picker = new JXDatePicker();
-        final JXMonthView monthView = new JXMonthView();
-        monthView.setSelectedDate(picker.getDate());
-        monthView.setTraversable(true);
-        // Synchronize the picker and selector's zones.
-        zoneSelector.setSelectedItem(picker.getTimeZone().getID());
-
-        // Set the picker's time zone based on the selected time zone.
-        zoneSelector.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String zone = (String) zoneSelector.getSelectedItem();
-                TimeZone tz = TimeZone.getTimeZone(zone);
-                picker.setTimeZone(tz);
-                monthView.setTimeZone(tz);
-              
-                assertEquals(tz, monthView.getCalendar().getTimeZone());
-            }
-        });
-
-        panel.add(zoneSelector);
-        panel.add(picker);
-        panel.add(monthView);
-        JXFrame frame = showInFrame(panel, "display problems with non-default timezones");
-        Action assertAction = new AbstractActionExt("assert dates") {
-
-            public void actionPerformed(ActionEvent e) {
-                Calendar cal = monthView.getCalendar();
-                LOG.info("cal/firstDisplayed" + 
-                        cal.getTime() +"/" + new Date(monthView.getFirstDisplayedDate()));
-            }
-            
-        };
-        addAction(frame, assertAction);
-        frame.pack();
-    }
-    
-    /**
-     * Issue #618-swingx: JXMonthView displays problems with non-default
-     * timezones.
-     * 
-     */
-    public void interactiveUpdateOnTimeZoneJP() {
-        JComponent panel = Box.createVerticalBox();
-
-        final JComboBox zoneSelector = new JComboBox(TimeZone.getAvailableIDs());
-        final JXMonthView monthView = new JXMonthView();
-        monthView.setTraversable(true);
-        // Synchronize the picker and selector's zones.
-        zoneSelector.setSelectedItem(monthView.getTimeZone().getID());
-
-        // Set the picker's time zone based on the selected time zone.
-        zoneSelector.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String zone = (String) zoneSelector.getSelectedItem();
-                TimeZone tz = TimeZone.getTimeZone(zone);
-                monthView.setTimeZone(tz);
-              
-                assertEquals(tz, monthView.getCalendar().getTimeZone());
-            }
-        });
-
-        panel.add(monthView);
-//        JPanel bar = new JPanel();
-        JLabel label = new JLabel("Select TimeZone:");
-        label.setHorizontalAlignment(JLabel.CENTER);
-//        panel.add(label);
-        panel.add(zoneSelector);
-        JXFrame frame = wrapInFrame(panel, "TimeZone");
-        frame.pack();
-        frame.setVisible(true);
-    }
-    /**
-     * Issue #618-swingx: JXMonthView displays problems with non-default
-     * timezones.
-     * 
-     */
-    public void interactiveTimeZoneClearDateState() {
-        JPanel panel = new JPanel();
-
-        final JComboBox zoneSelector = new JComboBox(TimeZone.getAvailableIDs());
-        final JXDatePicker picker = new JXDatePicker();
-        final JXMonthView monthView = new JXMonthView();
-        monthView.setSelectedDate(picker.getDate());
-        monthView.setLowerBound(XTestUtils.getStartOfToday(-10));
-        monthView.setUpperBound(XTestUtils.getStartOfToday(10));
-        monthView.setUnselectableDates(XTestUtils.getStartOfToday(2));
-        monthView.setFlaggedDates(new long[] {XTestUtils.getStartOfToday(4).getTime()});
-        monthView.setTraversable(true);
-        // Synchronize the picker and selector's zones.
-        zoneSelector.setSelectedItem(picker.getTimeZone().getID());
-
-        // Set the picker's time zone based on the selected time zone.
-        zoneSelector.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String zone = (String) zoneSelector.getSelectedItem();
-                TimeZone tz = TimeZone.getTimeZone(zone);
-                picker.setTimeZone(tz);
-                monthView.setTimeZone(tz);
-              
-                assertEquals(tz, monthView.getCalendar().getTimeZone());
-            }
-        });
-
-        panel.add(zoneSelector);
-        panel.add(picker);
-        panel.add(monthView);
-        JXFrame frame = showInFrame(panel, "clear internal date-related state");
-        Action assertAction = new AbstractActionExt("assert dates") {
-
-            public void actionPerformed(ActionEvent e) {
-                Calendar cal = monthView.getCalendar();
-                LOG.info("cal/firstDisplayed" + 
-                        cal.getTime() +"/" + new Date(monthView.getFirstDisplayedDate()));
-            }
-            
-        };
-        addAction(frame, assertAction);
-        frame.pack();
-    }
-    
-    /**
-     * Issue #659-swingx: lastDisplayedDate must be synched.
-     * 
-     */
-    public void interactiveLastDisplayed() {
-        final JXMonthView month = new JXMonthView();
-        month.setSelectionMode(SelectionMode.SINGLE_INTERVAL_SELECTION);
-        month.setTraversable(true);
-        Action action = new AbstractActionExt("check lastDisplayed") {
-
-            public void actionPerformed(ActionEvent e) {
-                Calendar cal = Calendar.getInstance();
-                cal.setTimeInMillis(month.getLastDisplayedDate());
-                Date viewLast = cal.getTime();
-                cal.setTimeInMillis(month.getUI().getLastDisplayedDate());
-                Date uiLast = cal.getTime();
-                LOG.info("last(view/ui): " + viewLast + "/" + uiLast);
-                
-            }
-            
-        };
-        JXFrame frame = wrapInFrame(month, "default - for debugging only");
-        addAction(frame, action);
-        frame.setVisible(true);
-    }
-
 //----------------------
     
     /**
--- src/test/org/jdesktop/swingx/JXMonthViewVisualCheck.java
+++ src/test/org/jdesktop/swingx/JXMonthViewVisualCheck.java
@@ -25,16 +25,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
+import javax.swing.Action;
+import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.jdesktop.swingx.JXMonthView.SelectionMode;
+import org.jdesktop.swingx.action.AbstractActionExt;
 import org.jdesktop.swingx.event.DateSelectionEvent;
 import org.jdesktop.swingx.event.DateSelectionListener;
+import org.jdesktop.swingx.test.XTestUtils;
 
 /**
  * Test to expose known issues with JXMonthView.
@@ -44,6 +53,10 @@ import org.jdesktop.swingx.event.DateSelectionListener;
 public class JXMonthViewVisualCheck extends InteractiveTestCase {
     private static final Logger LOG = Logger.getLogger(JXMonthViewVisualCheck.class
             .getName());
+
+    @SuppressWarnings("unused")
+    private Calendar calendar;
+
     public static void main(String[] args) {
 //      setSystemLF(true);
       JXMonthViewVisualCheck  test = new JXMonthViewVisualCheck();
@@ -55,6 +68,213 @@ public class JXMonthViewVisualCheck extends InteractiveTestCase {
           e.printStackTrace();
       }
   }
+
+    /**
+     * #681-swingx: first row overlaps days.
+     * 
+     * Looks like a problem with the constructor taking a locale? 
+     * Default is okay (even if German), US is okay, explicit german is wrong.
+     */
+    public void interactiveFirstRowOfMonthSetLocale() {
+        JPanel p = new JPanel();
+        // default constructor
+        p.add(new JXMonthView());
+        // explicit us locale
+        JXMonthView us = new JXMonthView();
+        us.setLocale(Locale.US);
+        p.add(us);
+        // explicit german locale
+        JXMonthView german = new JXMonthView();
+        german.setLocale(Locale.GERMAN);
+        p.add(german);
+        showInFrame(p, "first row overlapping - setLocale");
+    }
+
+   
+    /**
+     * #681-swingx: first row overlaps days.
+     * 
+     * Looks like a problem with the constructor taking a locale? 
+     * Default is okay (even if German), US is okay, explicit german is wrong.
+     */
+    public void interactiveFirstRowOfMonthLocaleConstructor() {
+        JPanel p = new JPanel();
+        // default constructor
+        p.add(new JXMonthView());
+        // explicit us locale
+        p.add(new JXMonthView(Locale.US));
+//         explicit german locale
+        p.add(new JXMonthView(Locale.GERMAN));
+        showInFrame(p, "first row overlapping - constructor");
+    }
+    /**
+     * #681-swingx: first row overlaps days.
+     * Here everything looks okay.
+     * 
+     * @see #interactiveFirstRowOfMonthLocaleDependent()
+     */
+    public void interactiveFirstRowOfMonth() {
+        JXMonthView monthView = new JXMonthView();
+        calendar.set(2008, 1, 1);
+        monthView.setSelectedDate(calendar.getTime());
+        showInFrame(monthView, "first row");
+    }
+
+    /**
+     * Issue #618-swingx: JXMonthView displays problems with non-default
+     * timezones.
+     * 
+     */
+    public void interactiveUpdateOnTimeZone() {
+        JPanel panel = new JPanel();
+
+        final JComboBox zoneSelector = new JComboBox(TimeZone.getAvailableIDs());
+        final JXDatePicker picker = new JXDatePicker();
+        final JXMonthView monthView = new JXMonthView();
+        monthView.setSelectedDate(picker.getDate());
+        monthView.setTraversable(true);
+        // Synchronize the picker and selector's zones.
+        zoneSelector.setSelectedItem(picker.getTimeZone().getID());
+
+        // Set the picker's time zone based on the selected time zone.
+        zoneSelector.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                String zone = (String) zoneSelector.getSelectedItem();
+                TimeZone tz = TimeZone.getTimeZone(zone);
+                picker.setTimeZone(tz);
+                monthView.setTimeZone(tz);
+              
+                assertEquals(tz, monthView.getCalendar().getTimeZone());
+            }
+        });
+
+        panel.add(zoneSelector);
+        panel.add(picker);
+        panel.add(monthView);
+        JXFrame frame = showInFrame(panel, "display problems with non-default timezones");
+        Action assertAction = new AbstractActionExt("assert dates") {
+
+            public void actionPerformed(ActionEvent e) {
+                Calendar cal = monthView.getCalendar();
+                LOG.info("cal/firstDisplayed" + 
+                        cal.getTime() +"/" + new Date(monthView.getFirstDisplayedDate()));
+            }
+            
+        };
+        addAction(frame, assertAction);
+        frame.pack();
+    }
+    
+    /**
+     * Issue #618-swingx: JXMonthView displays problems with non-default
+     * timezones.
+     * 
+     */
+    public void interactiveUpdateOnTimeZoneJP() {
+        JComponent panel = Box.createVerticalBox();
+
+        final JComboBox zoneSelector = new JComboBox(TimeZone.getAvailableIDs());
+        final JXMonthView monthView = new JXMonthView();
+        monthView.setTraversable(true);
+        // Synchronize the picker and selector's zones.
+        zoneSelector.setSelectedItem(monthView.getTimeZone().getID());
+
+        // Set the picker's time zone based on the selected time zone.
+        zoneSelector.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                String zone = (String) zoneSelector.getSelectedItem();
+                TimeZone tz = TimeZone.getTimeZone(zone);
+                monthView.setTimeZone(tz);
+              
+                assertEquals(tz, monthView.getCalendar().getTimeZone());
+            }
+        });
+
+        panel.add(monthView);
+//        JPanel bar = new JPanel();
+        JLabel label = new JLabel("Select TimeZone:");
+        label.setHorizontalAlignment(JLabel.CENTER);
+//        panel.add(label);
+        panel.add(zoneSelector);
+        JXFrame frame = wrapInFrame(panel, "TimeZone");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    /**
+     * Issue #618-swingx: JXMonthView displays problems with non-default
+     * timezones.
+     * 
+     */
+    public void interactiveTimeZoneClearDateState() {
+        JPanel panel = new JPanel();
+
+        final JComboBox zoneSelector = new JComboBox(TimeZone.getAvailableIDs());
+        final JXDatePicker picker = new JXDatePicker();
+        final JXMonthView monthView = new JXMonthView();
+        monthView.setSelectedDate(picker.getDate());
+        monthView.setLowerBound(XTestUtils.getStartOfToday(-10));
+        monthView.setUpperBound(XTestUtils.getStartOfToday(10));
+        monthView.setUnselectableDates(XTestUtils.getStartOfToday(2));
+        monthView.setFlaggedDates(new long[] {XTestUtils.getStartOfToday(4).getTime()});
+        monthView.setTraversable(true);
+        // Synchronize the picker and selector's zones.
+        zoneSelector.setSelectedItem(picker.getTimeZone().getID());
+
+        // Set the picker's time zone based on the selected time zone.
+        zoneSelector.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                String zone = (String) zoneSelector.getSelectedItem();
+                TimeZone tz = TimeZone.getTimeZone(zone);
+                picker.setTimeZone(tz);
+                monthView.setTimeZone(tz);
+              
+                assertEquals(tz, monthView.getCalendar().getTimeZone());
+            }
+        });
+
+        panel.add(zoneSelector);
+        panel.add(picker);
+        panel.add(monthView);
+        JXFrame frame = showInFrame(panel, "clear internal date-related state");
+        Action assertAction = new AbstractActionExt("assert dates") {
+
+            public void actionPerformed(ActionEvent e) {
+                Calendar cal = monthView.getCalendar();
+                LOG.info("cal/firstDisplayed" + 
+                        cal.getTime() +"/" + new Date(monthView.getFirstDisplayedDate()));
+            }
+            
+        };
+        addAction(frame, assertAction);
+        frame.pack();
+    }
+    
+    /**
+     * Issue #659-swingx: lastDisplayedDate must be synched.
+     * 
+     */
+    public void interactiveLastDisplayed() {
+        final JXMonthView month = new JXMonthView();
+        month.setSelectionMode(SelectionMode.SINGLE_INTERVAL_SELECTION);
+        month.setTraversable(true);
+        Action action = new AbstractActionExt("check lastDisplayed") {
+
+            public void actionPerformed(ActionEvent e) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(month.getLastDisplayedDate());
+                Date viewLast = cal.getTime();
+                cal.setTimeInMillis(month.getUI().getLastDisplayedDate());
+                Date uiLast = cal.getTime();
+                LOG.info("last(view/ui): " + viewLast + "/" + uiLast);
+                
+            }
+            
+        };
+        JXFrame frame = wrapInFrame(month, "default - for debugging only");
+        addAction(frame, action);
+        frame.setVisible(true);
+    }
+
 
     /**
      * Issue #637-swingx: make JXMonthView Locale-aware.
@@ -212,6 +432,11 @@ public class JXMonthViewVisualCheck extends InteractiveTestCase {
     }
     
 //----------------------
+    @Override
+    protected void setUp() throws Exception {
+        calendar = Calendar.getInstance();
+    }
+
     
     /**
      * do nothing test - keep the testrunner happy.
