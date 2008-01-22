74c316614cf9f32fd608fa1e521589bf875a7dd6
rah003 <rah003@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-01-22 21:09:20 +0000

    Fixed failing test.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2618 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXLabel2VisualTest.java
+++ src/test/org/jdesktop/swingx/JXLabel2VisualTest.java
@@ -4,16 +4,25 @@ package org.jdesktop.swingx;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-public class JXLabel2VisualTest implements Runnable{
+public class JXLabel2VisualTest extends InteractiveTestCase {
 
-    public static void main(String[] args) {
-    SwingUtilities.invokeLater(new JXLabel2VisualTest());
+    public static void main(String[] args) throws Exception {
+        // setSystemLF(true);
+        JXLabel2VisualTest test = new JXLabel2VisualTest();
+        
+        try {
+            test.runInteractiveTests();
+        } catch (Exception e) {
+            System.err.println("exception when executing interactive tests:");
+            e.printStackTrace();
+        }
     }
 
     /**
      * #swingx-680 Preferred size is not set when label is rotated.
      */
-    public void run() {
+    public static void interactiveJXLabel() {
+
     JFrame testFrame = new JFrame("JXLabel Test");
     testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
