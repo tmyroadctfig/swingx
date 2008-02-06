493aa5fb9bfeb783d04ff72809f96987c4fa9290
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-02-06 02:59:32 +0000

    Issue number:  744 Updated UIHighlighterAddon to be more consistent with other addons.  Also made use of the new DefaultsList enhancements.  Checked visual checks before submitting.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2666 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/plaf/UIColorHighlighterAddon.java
+++ src/java/org/jdesktop/swingx/plaf/UIColorHighlighterAddon.java
@@ -21,16 +21,10 @@
  */
 package org.jdesktop.swingx.plaf;
 
-import java.awt.Color;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.swing.plaf.ColorUIResource;
 import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
 
-import org.jdesktop.swingx.plaf.windows.WindowsClassicLookAndFeelAddons;
 import org.jdesktop.swingx.plaf.windows.WindowsLookAndFeelAddons;
 import org.jdesktop.swingx.util.OS;
 
@@ -42,129 +36,71 @@ import org.jdesktop.swingx.util.OS;
  * 
  * 
  * @author Jeanette Winzenburg
+ * @author Karl Schaefer
  */
 public class UIColorHighlighterAddon extends AbstractComponentAddon {
 
-    /**
-     * @param name
-     */
     public UIColorHighlighterAddon() {
         super("UIColorHighlighter");
     }
 
     /**
-     * Loads the LF-specific striping background color for
-     * usage in UIColorHighlighter.
-     * 
+     * {@inheritDoc}
      */
     @Override
-    public void initialize(LookAndFeelAddons addon) {
-        Color striping = null;
-        if (isWindows(addon)) {
-            striping = getWindowStripingColor(addon);
-        } else if (isMetal(addon)) {
-            striping = getMetalStripingColor(addon);
-        } else if (isMac(addon)) {
-            striping = getMacStripingColor(addon);
-        } else if (isMotif(addon)) {
-            striping = getMotifStripingColor(addon);
-        }
-//        // PENDING: JW - fall-back here or in UIColorHighlighter?
-//        if (striping == null) {
-//            striping = getGenericStriping();
-//        }
-        List<Object> defaults = new ArrayList<Object>();
-        defaults.addAll(Arrays.asList(new Object[] {
-                "UIColorHighlighter.stripingBackground", 
-                striping,
-//                "UIColorHighlighter.genericStripingBackground",
-//                getGenericStriping()
-                }));
-        addon.loadDefaults(defaults.toArray());
+    protected void addBasicDefaults(LookAndFeelAddons addon, DefaultsList defaults) {
+        super.addBasicDefaults(addon, defaults);
+        
+        defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(229, 229, 229));
     }
 
     /**
-     * Creates and returns a generic striping color.
-     * Internally used if no LF specific color is found.
-     * 
-     * @return a generic striping color
+     * {@inheritDoc}
      */
-    protected ColorUIResource getGenericStriping() {
-        return new ColorUIResource(229, 229, 229);
+    @Override
+    protected void addMacDefaults(LookAndFeelAddons addon, DefaultsList defaults) {
+        super.addMacDefaults(addon, defaults);
+        
+        defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(237, 243, 254));
     }
 
     /**
-     * Creates and returns a the MacX specific striping color, may be null.
-     * @param addon the AddOn to create the color for
-     * @return the Mac specific striping color.
+     * {@inheritDoc}
      */
-    protected ColorUIResource getMacStripingColor(LookAndFeelAddons addon) {
-        return new ColorUIResource(237, 243, 254);
-    }
-
-    /**
-     * Creates and returns a the Metal specific striping color, may be null.
-     * Checks for Ocean only.
-     * 
-     * @param addon the AddOn to create the color for
-     * @return the Metal specific striping color.
-     */
-    protected ColorUIResource getMetalStripingColor(LookAndFeelAddons addon) {
-        ColorUIResource striping = null;
-        try {
-            Method method = MetalLookAndFeel.class.getMethod("getCurrentTheme");
-            Object currentTheme = method.invoke(null);
-            if (Class.forName("javax.swing.plaf.metal.OceanTheme").isInstance(
-                    currentTheme)) {
-                striping = new ColorUIResource(230, 238, 246);
-            }
-        } catch (Exception e) {
-        }
-        if (striping == null) {
-            striping = new ColorUIResource(235, 235, 255);
-        }
-        return striping;
-    }
-
-    /**
-     * Creates and returns a the Windows specific striping color, may be null.
-     * Can handle classic and XP (normal blue, homestead, metalic).<p>
-     * 
-     * PENDING: Vista?
-     * 
-     * @param addon the AddOn to create the color for
-     * @return the Windows specific striping color.
-     */
-    protected ColorUIResource getWindowStripingColor(LookAndFeelAddons addon) {
-        if (addon instanceof WindowsClassicLookAndFeelAddons) {
-            return new ColorUIResource(218, 222, 233);
-        }
-        ColorUIResource striping = null;
-        String xpStyle = OS.getWindowsVisualStyle();
-        if (WindowsLookAndFeelAddons.HOMESTEAD_VISUAL_STYLE
-                .equalsIgnoreCase(xpStyle)) {
-            striping = new ColorUIResource(228, 231, 219);
-        } else if (WindowsLookAndFeelAddons.SILVER_VISUAL_STYLE
-                .equalsIgnoreCase(xpStyle)) {
-            striping = new ColorUIResource(235, 235, 236);
+    @Override
+    protected void addMetalDefaults(LookAndFeelAddons addon, DefaultsList defaults) {
+        super.addMetalDefaults(addon, defaults);
+        
+        if (MetalLookAndFeel.getCurrentTheme() instanceof OceanTheme) {
+            defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(230, 238, 246));
         } else {
-            // default blue
-            striping = new ColorUIResource(224, 233, 246);
+            defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(235, 235, 255));
         }
-        return striping;
     }
 
     /**
-     * Creates and returns a the motif specific striping color, may be null.
-     * 
-     * PENDING: no Motif included in color design map, returns
-     *   generic.
-     *   
-     * @param addon the AddOn to create the color for
-     * @return the Motif specific striping color, here: generic
+     * {@inheritDoc}
      */
-    protected ColorUIResource getMotifStripingColor(LookAndFeelAddons addon) {
-        return getGenericStriping();
+    @Override
+    protected void addWindowsDefaults(LookAndFeelAddons addon, DefaultsList defaults) {
+        super.addWindowsDefaults(addon, defaults);
+        
+        if (OS.isUsingWindowsVisualStyles()) {
+            String xpStyle = OS.getWindowsVisualStyle();
+            
+            if (WindowsLookAndFeelAddons.HOMESTEAD_VISUAL_STYLE
+                    .equalsIgnoreCase(xpStyle)) {
+                defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(228, 231, 219));
+            } else if (WindowsLookAndFeelAddons.SILVER_VISUAL_STYLE
+                    .equalsIgnoreCase(xpStyle)) {
+                defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(235, 235, 236));
+            } else {
+                // default blue
+                defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(224, 233, 246));
+            }
+            
+        } else {
+            defaults.add("UIColorHighlighter.stripingBackground", new ColorUIResource(218, 222, 233));
+        }
     }
-
 }
