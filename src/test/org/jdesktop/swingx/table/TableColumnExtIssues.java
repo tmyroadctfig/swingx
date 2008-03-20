31d394c2309cc6cb446b40accbd76ababb30dc5f
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-03-20 11:57:16 +0000

    Issue #815-swingx: TableColumnExt must not clone listeners
    - added example
    
    
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2834 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/table/TableColumnExtIssues.java
+++ src/test/org/jdesktop/swingx/table/TableColumnExtIssues.java
@@ -9,10 +9,13 @@ package org.jdesktop.swingx.table;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
 
+import javax.swing.table.TableColumn;
+
 import junit.framework.TestCase;
 
 import org.jdesktop.test.PropertyChangeReport;
 import org.jdesktop.test.SerializableSupport;
+import org.jdesktop.test.TestUtils;
 
 /**
  * Test to exposed known issues of <code>TableColumnExt</code>.
@@ -25,6 +28,56 @@ import org.jdesktop.test.SerializableSupport;
  */
 public class TableColumnExtIssues extends TestCase {
 
+    /**
+     * Issue #815-swingx: Listeners must not be cloned.
+     * Sanity: test that listeners registered with the clone are not
+     * notified when changing the original.
+     */
+    public void testListenerNotificationOrigChanged() {
+        TableColumnCloneable column = new TableColumnCloneable();
+        column.setPreferredWidth(column.getMinWidth());
+        TableColumnCloneable clone = (TableColumnCloneable) column.clone();
+        PropertyChangeReport report = new PropertyChangeReport();
+        clone.addPropertyChangeListener(report);
+        column.setPreferredWidth(column.getPreferredWidth() + 10);
+        assertEquals(0, report.getEventCount());
+    }
+    
+    /**
+     * Issue #815-swingx: Listeners must not be cloned.
+     * test that listeners registered with the original are not notified
+     * when changing the clone.
+     */
+    public void testListenerNotificationCloneChanged() {
+        TableColumnCloneable column = new TableColumnCloneable();
+        column.setPreferredWidth(column.getMinWidth());
+        PropertyChangeReport report = new PropertyChangeReport();
+        column.addPropertyChangeListener(report);
+        TableColumnCloneable clone = (TableColumnCloneable) column.clone();
+        clone.setPreferredWidth(column.getPreferredWidth() + 10);
+        assertEquals(0, report.getEventCount());
+    }
+    
+    /**
+     * TableColumn sub with a better-behaved clone - removes the cloned listeners
+     */
+    public static class TableColumnCloneable extends TableColumn implements Cloneable {
+
+        @Override
+        public Object clone()  {
+            try {
+                TableColumn column = (TableColumn) super.clone();
+                for (int i = 0; i < getPropertyChangeListeners().length; i++) {
+                    column.removePropertyChangeListener(getPropertyChangeListeners()[i]);
+                }
+                return column;
+            } catch (CloneNotSupportedException e) { // don't expect
+            }
+            return null;
+        }
+        
+        
+    }
     /**
      * Issue #815-swingx: Listeners must not be cloned.
      */
