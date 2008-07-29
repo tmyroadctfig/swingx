979dfb22b4325fe085f7ed72fcdfd3bb9d14a40f
rah003 <rah003@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-07-29 20:54:16 +0000

    Issue number:  #784 Possible workaround and more debug info for missing ui resources.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@2994 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/plaf/LookAndFeelAddons.java
+++ src/java/org/jdesktop/swingx/plaf/LookAndFeelAddons.java
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -26,6 +26,8 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JComponent;
 import javax.swing.UIManager;
@@ -61,8 +63,8 @@ import org.jdesktop.swingx.util.OS;
  * {@link #setAddon(String)}method. For example, to install the
  * Windows addons, add the following statement
  * <code>LookAndFeelAddons.setAddon("org.jdesktop.swingx.plaf.windows.WindowsLookAndFeelAddons");</code>.
- * 
- * @author <a href="mailto:fred@L2FProd.com">Frederic Lavigne</a> 
+ *
+ * @author <a href="mailto:fred@L2FProd.com">Frederic Lavigne</a>
  * @author Karl Schaefer
  */
 public abstract class LookAndFeelAddons {
@@ -77,8 +79,8 @@ public abstract class LookAndFeelAddons {
   private static final Object APPCONTEXT_INITIALIZED = new Object();
 
   private static boolean trackingChanges = false;
-  private static PropertyChangeListener changeListener;    
-  
+  private static PropertyChangeListener changeListener;
+
   static {
     // load the default addon
     String addonClassname = getBestMatchAddonClassName();
@@ -95,7 +97,7 @@ public abstract class LookAndFeelAddons {
       // addon class or do we just fail?
       throw new ExceptionInInitializerError(e);
     }
-    
+
     setTrackingLookAndFeelChanges(true);
   }
 
@@ -119,18 +121,18 @@ public abstract class LookAndFeelAddons {
 
   /**
    * Adds the given defaults in UIManager.
-   * 
+   *
    * Note: the values are added only if they do not exist in the existing look
    * and feel defaults. This makes it possible for look and feel implementors to
    * override SwingX defaults.
-   * 
+   *
    * Note: the array is traversed in reverse order. If a key is found twice in
    * the array, the key/value with the highest position in the array gets
    * precedence over the other key in the array
-   * 
+   *
    * @param keysAndValues
    */
-  public void loadDefaults(Object[] keysAndValues) {    
+  public void loadDefaults(Object[] keysAndValues) {
     // Go in reverse order so the most recent keys get added first...
     for (int i = keysAndValues.length - 2; i >= 0; i = i - 2) {
       if (UIManager.getLookAndFeelDefaults().get(keysAndValues[i]) == null) {
@@ -140,7 +142,7 @@ public abstract class LookAndFeelAddons {
   }
 
   public void unloadDefaults(Object[] keysAndValues) {
-    // commented after Issue 446. 
+    // commented after Issue 446.
     /*
     for (int i = 0, c = keysAndValues.length; i < c; i = i + 2) {
       UIManager.getLookAndFeelDefaults().put(keysAndValues[i], null);
@@ -159,14 +161,14 @@ public abstract class LookAndFeelAddons {
     LookAndFeelAddons addon = (LookAndFeelAddons)addonClass.newInstance();
     setAddon(addon);
   }
-   
+
   public static void setAddon(LookAndFeelAddons addon) {
     if (currentAddon != null) {
       currentAddon.uninitialize();
     }
 
     addon.initialize();
-    currentAddon = addon;    
+    currentAddon = addon;
     UIManager.put(APPCONTEXT_INITIALIZED, Boolean.TRUE);
   }
 
@@ -178,7 +180,7 @@ public abstract class LookAndFeelAddons {
    * Based on the current look and feel (as returned by
    * <code>UIManager.getLookAndFeel()</code>), this method returns
    * the name of the closest <code>LookAndFeelAddons</code> to use.
-   * 
+   *
    * @return the addon matching the currently installed look and feel
    */
   public static String getBestMatchAddonClassName() {
@@ -211,7 +213,7 @@ public abstract class LookAndFeelAddons {
   /**
    * Gets the addon best suited for the operating system where the
    * virtual machine is running.
-   * 
+   *
    * @return the addon matching the native operating system platform.
    */
   public static String getSystemAddonClassName() {
@@ -237,7 +239,7 @@ public abstract class LookAndFeelAddons {
    * Each new component added by the library will contribute its
    * default UI classes, colors and fonts to the LookAndFeelAddons.
    * See {@link ComponentAddon}.
-   * 
+   *
    * @param component
    */
   public static void contribute(ComponentAddon component) {
@@ -252,12 +254,12 @@ public abstract class LookAndFeelAddons {
 
   /**
    * Removes the contribution of the given addon
-   * 
+   *
    * @param component
    */
   public static void uncontribute(ComponentAddon component) {
     contributedComponents.remove(component);
-    
+
     if (currentAddon != null) {
       component.uninitialize(currentAddon);
     }
@@ -266,25 +268,35 @@ public abstract class LookAndFeelAddons {
   /**
    * Workaround for IDE mixing up with classloaders and Applets environments.
    * Consider this method as API private. It must not be called directly.
-   * 
+   *
    * @param component
    * @param expectedUIClass
-   * @return an instance of expectedUIClass 
+   * @return an instance of expectedUIClass
    */
   public static ComponentUI getUI(JComponent component, Class<?> expectedUIClass) {
     maybeInitialize();
 
     // solve issue with ClassLoader not able to find classes
     String uiClassname = (String)UIManager.get(component.getUIClassID());
+    // possible workaround and more debug info on #784
+    if (uiClassname == null) {
+        Logger logger = Logger.getLogger("LookAndFeelAddons");
+        logger.warning("Failed to retrieve UI for " + component.getClass().getName() + " with UIClassID " + component.getUIClassID());
+        if (logger.isLoggable(Level.FINE)) {
+            logger.fine("Existing UI defaults keys: " + new ArrayList(UIManager.getDefaults().keySet()));
+        }
+        // really ugly hack. Should be removed as soon as we figure out what is causing the issue
+        uiClassname = "org.jdesktop.swingx.plaf.basic.Basic" + expectedUIClass.getSimpleName();
+    }
     try {
       Class<?> uiClass = Class.forName(uiClassname);
       UIManager.put(uiClassname, uiClass);
     } catch (ClassNotFoundException e) {
       // we ignore the ClassNotFoundException
     }
-    
+
     ComponentUI ui = UIManager.getUI(component);
-    
+
     if (expectedUIClass.isInstance(ui)) {
       return ui;
     } else {
@@ -309,7 +321,7 @@ public abstract class LookAndFeelAddons {
       }
     }
   }
-  
+
   /**
    * With applets, if you reload the current applet, the UIManager will be
    * reinitialized (entries previously added by LookAndFeelAddons will be
@@ -317,7 +329,7 @@ public abstract class LookAndFeelAddons {
    * itself through the static block in components and the classes do not get
    * reloaded. This means component.updateUI will fail because it will not find
    * its UI.
-   * 
+   *
    * This method ensures LookAndFeelAddons get re-initialized if needed. It must
    * be called in every component updateUI methods.
    */
@@ -326,13 +338,13 @@ public abstract class LookAndFeelAddons {
       // this is to ensure "UIManager#maybeInitialize" gets called and the
       // LAFState initialized
       UIManager.getLookAndFeelDefaults();
-      
+
       if (!UIManager.getBoolean(APPCONTEXT_INITIALIZED)) {
         setAddon(currentAddon);
       }
     }
   }
-  
+
   //
   // TRACKING OF THE CURRENT LOOK AND FEEL
   //
@@ -346,11 +358,11 @@ public abstract class LookAndFeelAddons {
       }
     }
   }
-  
+
   /**
    * If true, everytime the Swing look and feel is changed, the addon which
    * best matches the current look and feel will be automatically selected.
-   * 
+   *
    * @param tracking
    *          true to automatically update the addon, false to not automatically
    *          track the addon. Defaults to false.
@@ -372,7 +384,7 @@ public abstract class LookAndFeelAddons {
       trackingChanges = tracking;
     }
   }
-  
+
   /**
    * @return true if the addon will be automatically change to match the current
    *         look and feel
@@ -381,12 +393,12 @@ public abstract class LookAndFeelAddons {
   public static synchronized boolean isTrackingLookAndFeelChanges() {
     return trackingChanges;
   }
-   
+
     /**
      * Convenience method for setting a component's background painter property
      * with a value from the defaults. The painter is only set if the painter is
      * {@code null} or an instance of {@code UIResource}.
-     * 
+     *
      * @param c
      *                component to set the painter on
      * @param painter
@@ -399,13 +411,13 @@ public abstract class LookAndFeelAddons {
      */
     public static void installBackgroundPainter(JComponent c, String painter) {
         Class<?> clazz = c.getClass();
-        
+
         try {
             Method getter = clazz.getMethod("getBackgroundPainter");
             Method setter = clazz.getMethod("setBackgroundPainter", Painter.class);
-            
+
             Painter<?> p = (Painter<?>) getter.invoke(c);
-            
+
             if (p == null || p instanceof UIResource) {
                 setter.invoke(c, UIManagerExt.getPainter(painter));
             }
@@ -415,11 +427,11 @@ public abstract class LookAndFeelAddons {
             throw new IllegalArgumentException("cannot set painter on " + c.getClass());
         }
     }
-    
+
     /**
      * Convenience method for uninstalling a background painter. If the painter
      * of the component is a {@code UIResource}, it is set to {@code null}.
-     * 
+     *
      * @param c
      *                component to uninstall the painter on
      * @throws NullPointerException
@@ -430,13 +442,13 @@ public abstract class LookAndFeelAddons {
      */
     public static void uninstallBackgroundPainter(JComponent c) {
         Class<?> clazz = c.getClass();
-        
+
         try {
             Method getter = clazz.getMethod("getBackgroundPainter");
             Method setter = clazz.getMethod("setBackgroundPainter", Painter.class);
-            
+
             Painter<?> p = (Painter<?>) getter.invoke(c);
-            
+
             if (p == null || p instanceof UIResource) {
                 setter.invoke(c, (Painter<?>) null);
             }
