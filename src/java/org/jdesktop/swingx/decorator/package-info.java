5bf2bb98bddb4c5c1c1d7a797a11fa94b7a6b7b9
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-08-04 11:18:52 +0000

    Issue #906-swingx: define and enforce HighlighterClient
    started:
    - minor doc update
    
    
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3027 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/java/org/jdesktop/swingx/decorator/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Contains API used to implement coordinated sorting, filtering 
+ * and highlighting of the extended Swing cell-rendering component 
+ * classes JXTable, JXTreeTable, JXTree, and JXList. <p>
+ * 
+ * For details, see
+ * <a href="http://wiki.java.net/bin/view/Javadesktop/SwingXRendererBasedHighlight">
+ * SwingX Highlighter Basics (in the wiki) </a>. <p>
+ * 
+ * PENDING JW: describe here instead of linking ;-)
+ */
+package org.jdesktop.swingx.decorator;
+
