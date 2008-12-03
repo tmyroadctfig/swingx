70bc2563985af068d776e8849766c97c8f6dff1e
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-12-03 19:24:43 +0000

    Enhanced unit test for DefaultTreeTableModel.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3124 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/treetable/DefaultTreeTableModelUnitTest.java
+++ src/test/org/jdesktop/swingx/treetable/DefaultTreeTableModelUnitTest.java
@@ -3,6 +3,7 @@ package org.jdesktop.swingx.treetable;
 import java.util.Vector;
 
 import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -27,7 +28,7 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
     private DefaultMutableTreeTableNode grandchild3;
     private DefaultMutableTreeTableNode grandchild4;
     private DefaultMutableTreeTableNode grandchild5;
-    private DefaultMutableTreeTableNode grandchild6;
+    private AbstractMutableTreeTableNode grandchild6;
     
     @Before
     public void setUpJ4() throws Exception {
@@ -56,7 +57,15 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
         child2.add(grandchild4);
         grandchild5 = new DefaultMutableTreeTableNode("grandchild5");
         child2.add(grandchild5);
-        grandchild6 = new DefaultMutableTreeTableNode("grandchild6");
+        grandchild6 = new AbstractMutableTreeTableNode("grandchild6") {
+            public int getColumnCount() {
+                return 0;
+            }
+
+            public Object getValueAt(int column) {
+                return getUserObject();
+            }
+        };
         child2.add(grandchild6);
         root.add(child2);
         
@@ -77,7 +86,7 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
     }
     
     @Test
-    public void testModelGetPathToRoot() {
+    public void testGetPathToRoot() {
         TreeNode[] testGroup1 = model.getPathToRoot(grandchild3);
         
         assertEquals(testGroup1[0], root);
@@ -99,12 +108,22 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
         } catch (NullPointerException e) {
             //success
         }
+        
+        try {
+            model.getPathToRoot(new DefaultMutableTreeTableNode("failure"));
+            fail("expected NullPointerException");
+        } catch (NullPointerException e) {
+            //success
+        }
     }
     
     @Test
-    public void testModelGetValueAt() {
+    public void testGetValueAt() {
         //Test expected cases
         assertEquals(model.getValueAt(root, 0), "root");
+        
+        //Outside TTN count
+        assertNull(model.getValueAt(grandchild6, 0));
         
         //Test boundary cases
         try {
@@ -120,11 +139,196 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
         } catch (IllegalArgumentException e) {
             //success
         }
+        
+        //Test exceptional cases
+        try {
+            model.getValueAt(null, 0);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.getValueAt(new Object(), 0);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+    }
+    
+    @Test
+    public void testSetValueAt() {
+        //Test expected cases
+        model.setValueAt("new", root, 0);
+        assertEquals(model.getValueAt(root, 0), "new");
+        
+        //Test boundary cases
+        try {
+            model.setValueAt("new", child1, model.getColumnCount());
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.setValueAt("new", grandchild4, -1);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        //Test exceptional cases
+        try {
+            model.setValueAt("new", null, 0);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.setValueAt("new", new Object(), 0);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+    }
+    
+    public void testSetValueAtIssues() {
+        //TODO what to do in this case?  exception? nothing?
+        //Outside TTN count
+        model.setValueAt("new", grandchild6, 0);
+    }
+    
+    @Test
+    public void testGetChild() {
+        assertEquals(model.getChild(root, 0), child1);
+        
+        //Test boundary cases
+        //TODO untestable: outcome depends on how node handles OOB
+        //should TTN document an IOOBException?
+        
+        //Test exceptional cases
+        try {
+            model.getChild(null, 0);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.getChild(new Object(), 0);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+    }
+    
+    @Test
+    public void testGetChildCount() {
+        assertEquals(model.getChildCount(root), 2);
+        
+        assertEquals(model.getChildCount(child1), 3);
+        
+        assertEquals(model.getChildCount(grandchild4), 0);
+        
+        //Test exceptional cases
+        try {
+            model.getChildCount(null);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.getChildCount(new Object());
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+    }
+    
+    @Test
+    public void testGetIndexOfChild() {
+        assertEquals(model.getIndexOfChild(root,child1), 0);
+        
+        assertEquals(model.getIndexOfChild(root,child2), 1);
+        
+        //Test exceptional cases
+        try {
+            model.getIndexOfChild(null, child1);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.getIndexOfChild(new Object(), child1);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        try {
+            model.getIndexOfChild(child1, null);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.getIndexOfChild(child1, new Object());
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+    }
+    
+    @Test
+    public void testIsCellEditable() {
+        //Test expected cases
+//        assertFalse(model.isLeaf(root));
+//        assertFalse(model.isLeaf(child2));
+//        assertTrue(model.isLeaf(grandchild3));
+        
+        //Test boundary cases
+        try {
+            model.isCellEditable(root, model.getColumnCount());
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        try {
+            model.isCellEditable(root, -1);
+            fail("expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+        
+        //Test exceptional cases
+        try {
+            model.isCellEditable(null, 0);
+            
+            fail("IllegalArgumentException is not thrown.");
+        } catch (IllegalArgumentException e) {
+            //test succeeded
+        }
+        
+        try {
+            model.isCellEditable(new Object(), 0);
+            
+            fail("IllegalArgumentException is not thrown.");
+        } catch (IllegalArgumentException e) {
+            //test succeeded
+        }
     }
     
     @Test
     public void testModelIsLeaf() {
-        //asksAllowsChildren == false
+        //Test expected cases
+        assertFalse(model.isLeaf(root));
+        assertFalse(model.isLeaf(child2));
+        assertTrue(model.isLeaf(grandchild3));
+        
         //Test exceptional cases
         try {
             model.isLeaf(null);
@@ -141,11 +345,6 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
         } catch (IllegalArgumentException e) {
             //test succeeded
         }
-        
-        //Test expected cases
-        assertFalse(model.isLeaf(root));
-        assertFalse(model.isLeaf(child2));
-        assertTrue(model.isLeaf(grandchild3));
     }
     
     @Test
@@ -201,4 +400,49 @@ public class DefaultTreeTableModelUnitTest extends TestCase {
     }
     
     //TODO test "fire" methods and reloads
+    
+    @Test
+    public void testValueForPathChanged() {
+        //Test expected cases
+        model.valueForPathChanged(new TreePath(root), "a new root");
+        assertEquals(root.getUserObject(), "a new root");
+        
+        //Test exceptional cases
+        try {
+            model.valueForPathChanged(null, new Object());
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            //success
+        }
+        
+        try {
+            model.valueForPathChanged(new TreePath(new Object()), new Object());
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //success
+        }
+    }
+    
+    @Test
+    public void testSetUserObject() {
+        //Test expected cases
+        model.setUserObject(grandchild2, "a new value");
+        assertEquals(grandchild2.getUserObject(), "a new value");
+        
+        //Test exceptional cases
+        try {
+            model.setUserObject(null, new Object());
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            //success
+        }
+        
+        //TODO fix model to return correct exception
+//        try {
+//            model.setUserObject(new DefaultMutableTreeTableNode("unmanaged"), new Object());
+//            fail("Expected IllegalArgumentException");
+//        } catch (IllegalArgumentException e) {
+//            //success
+//        }
+    }
 }
