5f1d77618453a4bd9f0288f21461f0e22242c91a
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2008-12-18 09:53:00 +0000

    doc cleanup - started package-info for swingx package
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3144 1312f61e-266d-0410-97fa-c3b71232c9ac

---
+++ src/java/org/jdesktop/swingx/package-info.java
@@ -0,0 +1,73 @@
+/*
+ * $Id$
+ *
+ * Copyright 2007 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+/**
+ *    Contains extensions to the Swing GUI toolkit, including new and enhanced
+      components that provide functionality commonly required by rich, 
+      data-centric client applications.  
+      Many of these features will eventually be incorporated into the Swing
+      toolkit, although API compatibility will not be guaranteed.<p>
+      
+      <h2>New or Enhanced Functionality</h2>
+      
+      <h3>Auto-completion for textfields</h3>
+      
+      <h3>Enhanced Rendering Support for Collection Components</h3>
+      
+      <h3>Built-In Search Support for Collection Components and JXEditorPane</h3>
+      
+      <h3>Filtering/Sorting for Table and List</h3>
+      
+      <b>NOTE:</b> this is incompatible with core (table) sorting/filtering in JDK 6+.
+      
+      <h3>Login/Authentication Framework</h3>
+      
+      <h2>Enhanced components</h2>
+      <ul>
+      <li> {@link JXButton}
+      <li> {@link JXDialog}
+      <li> {@link JXEditorPane}
+      <li> {@link JXFrame}
+      <li> ...
+      </ul>
+            
+      <h2>New components</h2>
+      
+      <ul>
+      <li> {@link JXBusyLabel}
+      <li> {@link JXTreeTable}
+      <li> {@link JXCollapsiblePane}
+      <li> Calendar components: {@link JXDatePicker} and {@link JXMonthView}
+      <li> {@link JXColorSelectionButton}
+      <li> {@link JXErrorPane}
+      <li> Search components: {@link JXFindBar} and {@link JXFindPanel}
+      <li> {@link JXGlassBox} (PENDING JW: remove?)
+      <li> {@link JXGradientChooser}
+      <li> {@link JXGraph}
+      <li> {@link JXHeader}
+      <li> {@link JXHyperLink}
+      <li> ....
+
+      </ul>
+ */
+package org.jdesktop.swingx;
+
