b34faf0d8aa331aa400e8c071b10b98fe103c168
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2009-07-07 12:00:37 +0000

    JTableIssue: more tests to understand core sorting
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3376 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JTableIssues.java
+++ src/test/org/jdesktop/swingx/JTableIssues.java
@@ -8,36 +8,51 @@
 package org.jdesktop.swingx;
 
 import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.DefaultCellEditor;
 import javax.swing.DefaultListSelectionModel;
+import javax.swing.DefaultRowSorter;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.RepaintManager;
+import javax.swing.RowSorter;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.event.RowSorterEvent;
 import javax.swing.event.TableModelEvent;
+import javax.swing.plaf.basic.BasicTableUI;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import org.jdesktop.swingx.action.AbstractActionExt;
 import org.jdesktop.test.AncientSwingTeam;
 import org.jdesktop.test.CellEditorReport;
 import org.jdesktop.test.ListSelectionReport;
+import org.jdesktop.test.PropertyChangeReport;
+import org.jdesktop.test.TestUtils;
+import org.junit.Test;
 
 /**
  * @author Jeanette Winzenburg
@@ -52,12 +67,367 @@ public class JTableIssues extends InteractiveTestCase {
       try {
 //        test.runInteractiveTests();
 //          test.runInteractiveTests("interactive.*ColumnControl.*");
-          test.runInteractiveTests("interactive.*Edit.*");
+          test.runInteractiveTests("interactive.*Sort.*");
       } catch (Exception e) {
           System.err.println("exception when executing interactive tests:");
           e.printStackTrace();
       }
   }
+
+  //---------------- core sorting 
+    
+  //------------------ testing core    
+      
+      @Test
+      public void testSetRowSorterChangeNotification() {
+          JTable table = new JTable(new AncientSwingTeam());
+          PropertyChangeReport report = new PropertyChangeReport();
+          table.addPropertyChangeListener(report);
+          table.setRowSorter(new TableRowSorter<TableModel>(table.getModel()));
+          TestUtils.assertPropertyChangeEvent(report, "rowSorter", null, table.getRowSorter());
+      }
+      
+      /**
+       * core issue: rowSorter replaced on setAutoCreateRowSorter even without change to flag.
+       */
+      @Test
+      public void testSetAutoCreateRowSorter() {
+          JTable table = new JTable();
+          assertEquals("sanity: core table autoCreate off initially", false, table.getAutoCreateRowSorter());
+          assertNull("sanity: core rowSorter is not created", table.getRowSorter());
+          table.setAutoCreateRowSorter(true);
+          assertNotNull("sanity: core rowSorter is created", table.getRowSorter());
+          TableModel model = new AncientSwingTeam();
+          table.setModel(model);
+          RowSorter<?> sorter = table.getRowSorter();
+          table.setAutoCreateRowSorter(true);
+          assertSame(sorter, table.getRowSorter());
+      }
+
+  //----------------------- interactive
+   
+      /**
+       * Core Issue: the calculation of the repaint region after update is completely broken.
+       * Nevertheless, the cell is updated correctly. Seems like someplace, the complete table
+       * is marked as dirty? <p>
+       * 
+       * Reason is that always the complete table is repainted if we have individual rowheights.
+       * slight dirt in the update code: even if already painted, the would be dirty-region
+       * for same rowheights is calculated and repainted (repaintManager folds them into one
+       * repaint request, though). 
+       */
+      public void interactiveRepaintIndiRowHeight() {
+          DefaultTableModel model = new DefaultTableModel(20, 3) {
+              /**
+               * Overridden to reach fire rowUpdated (instead of cellUpdated)
+               */
+              @Override
+              public void setValueAt(Object aValue, int row, int column) {
+                  Vector rowVector = (Vector)dataVector.elementAt(row);
+                  rowVector.setElementAt(aValue, column);
+                  fireTableRowsUpdated(row, row);
+              }
+              
+          };
+          final JTable table = new JTable(model) ;
+         for (int i = 4; i < table.getRowCount(); i++) {
+              table.setRowHeight(i, table.getRowHeight() + i *4);
+          }
+          JXFrame frame = showInFrame(table, "repaint after update");
+          Action action = new AbstractAction("update focused") {
+              
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  int selected = table.getRowCount() / 2;
+                  if (selected < 0) return;
+                  table.setValueAt("XX" + table.getValueAt(selected, 0), selected, 0);
+              }
+              
+          };
+          addAction(frame, action);
+      }
+      
+      
+      /**
+       * NPE on null values (if comparable)? No, not any longer.
+       */
+      public void interactiveSortWithNull() {
+          final JTable table = new JTable(new AncientSwingTeam());
+          table.setValueAt(null, 4, 3);
+          table.setAutoCreateRowSorter(true);
+          JXFrame frame = showWithScrollingInFrame(table, "NPE with null number?");
+      }
+
+      /**
+       * Core issue #6539455: table not properly repainted on update (from model).
+       * 
+       * Happens if the update is not triggered by an edit in the table itself. If
+       * so, all is well (repaint called for all of the table). If not, repaint is
+       * limited to the cell that has been updated (not even the whole row is
+       * painted) - correct would be to repaint all rows between the old and new
+       * row view index, inclusively.
+       */
+      public void interactiveSortOnUpdateNotEditing() {
+          final JTable table = new JTable(new AncientSwingTeam());
+          table.setAutoCreateRowSorter(true);
+          ((TableRowSorter) table.getRowSorter()).setSortsOnUpdates(true);
+          table.getRowSorter().toggleSortOrder(0);
+          JXFrame frame = showWithScrollingInFrame(table,
+                  "updates and repaint");
+          Action edit = new AbstractAction("update first visible") {
+
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  table.setValueAt("XXX" + table.getValueAt(0, 0), 0, 0);
+
+              }
+          };
+          addAction(frame, edit);
+      }
+      
+      /**
+       * Core issue #6539455: table not properly repainted on update (from model).
+       * 
+       * Happens if the update is not triggered by an edit in the table itself. If
+       * so, all is well (repaint called for all of the table). If not, repaint is
+       * limited to the cell that has been updated (not even the whole row is
+       * painted) - correct would be to repaint all rows between the old and new
+       * row view index, inclusively.
+       */
+      public void interactiveSortOnUpdateNotEditingHack() {
+          final JTable table = new JTableRepaintOnUpdate();
+          table.setModel(new AncientSwingTeam());
+          table.setAutoCreateRowSorter(true);
+          ((TableRowSorter) table.getRowSorter()).setSortsOnUpdates(true);
+          table.getRowSorter().toggleSortOrder(0);
+          JXFrame frame = showWithScrollingInFrame(table,
+                  "updates and repaint (hacked)");
+          Action edit = new AbstractAction("update first visible") {
+
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  table.setValueAt("XXX" + table.getValueAt(0, 0), 0, 0);
+
+              }
+          };
+          addAction(frame, edit);
+      }
+      
+      
+      public static class JTableRepaintOnUpdate extends JTable {
+
+          private UpdateHandler beforeSort;
+          
+          
+          @Override
+          public void sorterChanged(RowSorterEvent e) {
+              super.sorterChanged(e);
+              maybeRepaintOnSorterChanged(e);
+          } 
+
+          private void beforeUpdate(TableModelEvent e) {
+              if (!isSorted()) return;
+              beforeSort = new UpdateHandler(e);
+          }
+          
+          /**
+           * 
+           */
+          private void afterUpdate() {
+              beforeSort = null;
+          }
+          
+          
+          /**
+           * 
+           */
+          private void maybeRepaintOnSorterChanged(RowSorterEvent e) {
+              if (beforeSort == null) return;
+              if ((e == null) || (e.getType() != RowSorterEvent.Type.SORTED)) return;
+              UpdateHandler afterSort = new UpdateHandler(beforeSort);
+              if (afterSort.allHidden(beforeSort)) {
+                  return;
+              } else if (afterSort.complex(beforeSort)) {
+                  repaint();
+                  return;
+              }
+              int firstRow = afterSort.getFirstCombined(beforeSort);
+              int lastRow = afterSort.getLastCombined(beforeSort);
+              Rectangle first = getCellRect(firstRow, 0, false);
+              first.width = getWidth();
+              Rectangle last = getCellRect(lastRow, 0, false);
+              repaint(first.union(last));
+          }
+          
+          private class UpdateHandler {
+              private int firstModelRow;
+              private int lastModelRow;
+              private int viewRow;
+              private boolean allHidden;
+              
+              public UpdateHandler(TableModelEvent e) {
+                  firstModelRow = e.getFirstRow();
+                  lastModelRow = e.getLastRow();
+                  convert();
+              }
+              
+              public UpdateHandler(UpdateHandler e) {
+                  firstModelRow = e.firstModelRow;
+                  lastModelRow = e.lastModelRow;
+                  convert();
+              }
+              
+              public boolean allHidden(UpdateHandler e) {
+                  return this.allHidden && e.allHidden;
+              }
+              
+              public boolean complex(UpdateHandler e) {
+                  return (firstModelRow != lastModelRow);
+              }
+              
+              public int getFirstCombined(UpdateHandler e) {
+                  if (allHidden) return e.viewRow;
+                  if (e.allHidden) return viewRow;
+                  return Math.min(viewRow, e.viewRow);
+              }
+              
+              public int getLastCombined(UpdateHandler e) {
+                  if (allHidden || e.allHidden) return getRowCount() - 1;
+                  return Math.max(viewRow, e.viewRow);
+                  
+              }
+              /**
+               * @param e
+               */
+              private void convert() {
+                  // multiple updates
+                  if (firstModelRow != lastModelRow) {
+                      // don't bother too much - calculation not guaranteed to do anything good
+                      // just check if the all changed indices are hidden
+                      allHidden = true;
+                      for (int i = firstModelRow; i <= lastModelRow; i++) {
+                          if (convertRowIndexToView(i) >= 0) {
+                              allHidden = false;
+                              break;
+                          }
+                      }
+                      viewRow = -1;
+                      return;
+                  }
+                  // single update
+                  viewRow = convertRowIndexToView(firstModelRow);
+                  allHidden = viewRow < 0;
+              }
+              
+          }
+          /**
+           * @return
+           */
+          private boolean isSorted() {
+              // JW: not good enough - need a way to decide if there are any sortkeys which
+              // constitute a sort or any effective filters  
+              return getRowSorter() != null;
+          }
+
+
+
+          @Override
+          public void tableChanged(TableModelEvent e) {
+              if (isUpdate(e)) {
+                  beforeUpdate(e);
+              }
+              try {
+                  super.tableChanged(e);
+              } finally {
+                  afterUpdate();
+              }
+          }
+          
+
+          /**
+           * Convenience method to detect dataChanged table event type.
+           * 
+           * @param e the event to examine. 
+           * @return true if the event is of type dataChanged, false else.
+           */
+          protected boolean isDataChanged(TableModelEvent e) {
+              if (e == null) return false;
+              return e.getType() == TableModelEvent.UPDATE && 
+                  e.getFirstRow() == 0 &&
+                  e.getLastRow() == Integer.MAX_VALUE;
+          }
+          
+          /**
+           * Convenience method to detect update table event type.
+           * 
+           * @param e the event to examine. 
+           * @return true if the event is of type update and not dataChanged, false else.
+           */
+          protected boolean isUpdate(TableModelEvent e) {
+              if (isStructureChanged(e)) return false;
+              return e.getType() == TableModelEvent.UPDATE && 
+                  e.getLastRow() < Integer.MAX_VALUE;
+          }
+
+          /**
+           * Convenience method to detect a structureChanged table event type.
+           * @param e the event to examine.
+           * @return true if the event is of type structureChanged or null, false else.
+           */
+          protected boolean isStructureChanged(TableModelEvent e) {
+              return e == null || e.getFirstRow() == TableModelEvent.HEADER_ROW;
+          }
+
+
+      }
+      /**
+       * Core Issue ??: must not sort if mouse in resize region 
+       */
+      public void interactiveSortOnResize() {
+          JTable table = new JTable(new AncientSwingTeam());
+          table.setAutoCreateRowSorter(true);
+          MouseListener l = new MouseAdapter() {};
+          showWithScrollingInFrame(table, "must not sort in resize");
+      }
+
+      /**
+       * Core issue #6539455: table not properly repainted on update (from model).
+       * 
+       * This setup differs from the examples (assuming we would add a second table, arggh)
+       * above in that the sorter is shared as well as the model. In this case the repaint is
+       * okay, as the second table receives the event from the sorter outside of its 
+       * tableChanged, that is ignoreSort is false.
+       */
+      public void interactiveSharedRowSorter() {
+          TableModel model = new AncientSwingTeam();
+          final JTable one = new JTable();
+          one.setDragEnabled(true);
+          one.setAutoCreateRowSorter(true);
+          one.setModel(model);
+          JTable other = new JTable(model);
+          other.setRowSorter(one.getRowSorter());
+          JXFrame frame = showWithScrollingInFrame(one, other, "shared model and rowsorter");
+          Action editFirst = new AbstractAction("prefix X on first") {
+
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  Object old = one.getValueAt(0, 0);
+                  one.setValueAt("X" + old, 0, 0);
+              }
+              
+          };
+          addAction(frame, editFirst);
+          Action toggleSortOnUpdate = new AbstractAction("toggleSortsOnUpdate") {
+
+              @Override
+              public void actionPerformed(ActionEvent e) {
+                  DefaultRowSorter<?, ?> sorter = (DefaultRowSorter<?, ?>) one.getRowSorter();
+                  sorter.setSortsOnUpdates(!sorter.getSortsOnUpdates());
+              } };
+          addAction(frame, toggleSortOnUpdate);
+      }
+
+   //---------------- end core sorting   
 
     public void testFormatDefaultRenderer() {
         DefaultTableModel model = new DefaultTableModel(1, 1) {
@@ -252,10 +622,7 @@ public class JTableIssues extends InteractiveTestCase {
         JTable table = new JTable(0, 0);
         table.addColumn(new TableColumn(1));
     }
-    
-//----------------------- interactive
- 
-    
+
     public void interactiveAutoStartsEdit() {
         final String autoKey = "JTable.autoStartsEdit";
         final JTable table = new JTable(new AncientSwingTeam());
@@ -669,5 +1036,7 @@ public class JTableIssues extends InteractiveTestCase {
         assertEquals("lead must be 0", 0, table.getSelectionModel().getLeadSelectionIndex());
         
     }
+//------------- from incubator ... PENDING: cleanup/remove
+    
 
 }
