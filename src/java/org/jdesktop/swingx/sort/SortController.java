884793ece2c9f930d51b891e645eb423c8cdfbd6
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2009-07-10 13:53:01 +0000

    Issue #1122-swingx: JXTable re-enable sort api started - enhanced SortController functionality to take over most of table's sort related responsibility - implemented in TableSortController - added tests for TableSortController - prepared to use in JXTable (not yet wired to it by default)
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3389 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/sort/SortController.java
+++ src/java/org/jdesktop/swingx/sort/SortController.java
@@ -23,43 +23,126 @@ package org.jdesktop.swingx.sort;
 
 import javax.swing.SortOrder;
 
-import org.jdesktop.swingx.JXTable;
-
 /**
- * Defines the interactive sort control for a table. All sort gesture requests
- * from a {@link JXTable} will be routed through the SortController returned
- * from {@link FilterPipeline}.
+ * Defines the interactive sort control for sortable collection components (like 
+ * JXList, JXTable). All sort gesture requests from their sort api 
+ * are routed through the SortController.
  * <p>
  * 
+ * This is very-much work-in-progress: while moving from ol' SwingX sorting to 
+ * core jdk6 sorting we need a hook for sorting api on the view. So in terms of
+ * jdk6 classes, this is something like:<p>
  * 
- *  
- * <p>
- * The GlazedLists project (http://publicobject.com/glazedlists/) 
- * has an example about how to replace the SwingX'
- * internal (view based) by an external 
- * (model-decoration based) sort/filter mechanism. 
- *  
- * <p>
- * This interface is inspired by a Java 1.6 class RowSorter, extracting
- *  the sort control part - change notification and index mapping is left to the 
- *  enclosing FilterPipeline. 
+ * <code><pre>
+ * SortController == DefaultRowSorter - RowSorter + XX
+ * </pre></code>
+ * All methods which change sort state must respect per-controller and per-column 
+ * sortable property, as follows
+ * <ol>
+ * <li> if per-controller sortable is false, do nothing
+ * <li> if per-controller sortable is true, if per-column sortable is false, do nothing
+ * <li> if both are true toggle the SortOrder of the given column
+ * </ol>
+ *
+ * PENDING JW: add RowFilter support<p>
+ * 
  * 
  *  @author <a href="mailto:jesse@swank.ca">Jesse Wilson</a>
+ *  @author Jeanette Winzenburg
  * 
  */
 public interface SortController {
 
     /**
-     * Reverses the sort order of the specified column. 
-     * It is up to implementating classes to provide the exact behavior when invoked.
+     * Sets whether or not this controller is sortable.<p>
+     * 
+     * The default is true.<p>
+     * 
+     * PENDING JW: define behaviour if sortable is disabled while has sortOrders.
+     * In this case JXTable resets all sorts.
+     * 
+     * @param sortable whether or not this controller is sortable
+     * @see #isSortable()        
+     */
+    void setSortable(boolean sortable);
+    
+    /**
+     * Returns true if this controller is sortable; otherwise, false.
      *
+     * @return true if this controller is sortable
+     *         
+     * @see #isSortable()
+     */
+    boolean isSortable();
+    
+    /**
+     * Sets whether or not the specified column is sortable.<p>
+     * 
+     * The default is true.<p>
+     * 
+     * PENDING JW: define behaviour if sortable is disabled while has sortOrders.
+     * In this case JXTable removes the sort of the column.<p>
+     * 
+     * PENDING JW: decide whether or not this method should trigger a resort
+     * DefaultRowSorter explicitly doesn't, JXTable does.
+     * 
+     * @param column the column to enable or disable sorting on, in terms
+     *        of the underlying model
+     * @param sortable whether or not the specified column is sortable
+     * @throws IndexOutOfBoundsException if <code>column</code> is outside
+     *         the range of the model
+     * @see #isSortable(int)        
+     * @see #toggleSortOrder(int)
+     * @see #setSortOrder(int, SortOrder)
+     */
+    void setSortable(int column, boolean sortable);
+    
+    /**
+     * Returns true if the specified column is sortable. <p>
+     * This returns true if both the controller's sortable property and
+     * the column's sortable property is true. Returns false if any of
+     * them is false.
+     *
+     * @param column the column to check sorting for, in terms of the
+     *        underlying model
+     * @return true if the column is sortable
+     * @throws IndexOutOfBoundsException if column is outside
+     *         the range of the underlying model
+     *         
+     * @see #isSortable(int)
+     */
+    boolean isSortable(int column);
+
+    
+    /**
+     * Reverses the sort order of the specified column. The exact behaviour is
+     * up to implementations.<p>
+     * 
+     * Implementations must respect the per-controller and per-column-sortable 
+     * property.
+     * 
      * @param column the model index of the column to toggle
+     * @see #isSortable(int) 
+     * @see #isSortable()
      */
     void toggleSortOrder(int column);
 
+    /**
+     * Sets the sort order of the specified column. <p>
+     * 
+     * Implementations must respect the per-controller and per-column-sortable 
+     * property.
+     *
+     * @param column the model index of the column to set
+     * @param sortOrder the SortOrder to set for the column
+     * 
+     * @see #isSortable(int) 
+     * @see #isSortable()
+     */
+    void setSortOrder(int column, SortOrder sortOrder);
  
     /**
-     * Get the sort order of the specified column.
+     * Returns the sort order of the specified column.
      * 
      * 
      * @return one of {@link SortOrder#ASCENDING},
@@ -67,7 +150,27 @@ public interface SortController {
      */
     SortOrder getSortOrder(int column);
 
-    void setSortOrder(int column, SortOrder sortOrder);
     
-    void removeAll();
+    /**
+     * Resets all interactive sorting. <p>
+     * 
+     * Implementations must respect the per-controller and per-column-sortable 
+     * property.
+     * 
+     */
+    void resetSortOrders();
+    
+    /**
+     * Sets the cycle of sort ordes to toggle through.
+     * 
+     * @param cyle
+     */
+    void setSortOrderCycle(SortOrder... cyle);
+    
+    /**
+     * Returns the cycle of sort orders to cycle through.
+     * 
+     * @return
+     */
+    SortOrder[] getSortOrderCycle();
 }
---
+++ src/test/org/jdesktop/swingx/sort/TableSortControllerTest.java
@@ -0,0 +1,183 @@
+/*
+ * $Id$
+ *
+ * Copyright 2009 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdesktop.swingx.sort;
+
+import java.util.List;
+
+import javax.swing.SortOrder;
+import javax.swing.RowSorter.SortKey;
+import javax.swing.table.TableModel;
+
+import org.jdesktop.swingx.InteractiveTestCase;
+import org.jdesktop.test.AncientSwingTeam;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test of TableSortController.
+ * 
+ * @author Jeanette Winzenburg
+ */
+@RunWith(JUnit4.class)
+public class TableSortControllerTest extends InteractiveTestCase {
+
+    private TableModel teamModel;
+    private TableSortController<TableModel> controller;
+
+    /**
+     * Test that toggle sort order has no effect if column not sortable.
+     */
+    @Test
+    public void testToggleSortOrderIfSortableFalse() {
+        controller.setSortable(0, false);
+        controller.toggleSortOrder(0);
+        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
+    }
+    /**
+     * Test that set sort order has no effect if column not sortable.
+     * 
+     */
+    @Test
+    public void testSetSortOrderIfSortableFalse() {
+        controller.setSortable(0, false);
+        controller.setSortOrder(0, SortOrder.ASCENDING);
+        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
+    }
+
+    /**
+     * Test that resetSortOrders doesn't change sorts at all if controller is not sortable.
+     */
+    @Test
+    public void testResetSortOrdersIfSortableFalse() {
+        controller.toggleSortOrder(0);
+        controller.toggleSortOrder(1);
+        controller.setSortable(false);
+        List<? extends SortKey> keys = controller.getSortKeys();
+        assertEquals(2, keys.size());
+        controller.resetSortOrders();
+        assertEquals("resetSortOrders must not touch unsortable columns", 
+                keys, controller.getSortKeys());
+    }
+   
+    /**
+     * Test that resetSortOrders doesn't change sorts at all if controller is not sortable.
+     */
+    @Test
+    public void testResetSortOrdersIfColumnSortableFalse() {
+        controller.toggleSortOrder(0);
+        controller.toggleSortOrder(1);
+        controller.setSortable(0, false);
+        List<? extends SortKey> keys = controller.getSortKeys();
+        assertEquals(2, keys.size());
+        controller.resetSortOrders();
+        assertEquals("resetSortOrders must not touch unsortable columns", 
+                1, controller.getSortKeys().size());
+        
+    }
+    /**
+     * Test that resetSortOrders removes all sorts.
+     */
+    @Test
+    public void testResetSortOrders() {
+        controller.toggleSortOrder(0);
+        controller.toggleSortOrder(1);
+        assertEquals(2, controller.getSortKeys().size());
+        controller.resetSortOrders();
+        assertEquals(0, controller.getSortKeys().size());
+    }
+    /**
+     * Test that toggle cycles through sort order cycle.
+     */
+    @Test
+    public void testToggleSortOrder() {
+        // PENDING JW: use custom cycle to really test
+        SortOrder[] cycle = controller.getSortOrderCycle();
+        for (int i = 0; i < cycle.length; i++) {
+            controller.toggleSortOrder(0);
+            assertEquals(cycle[i], controller.getSortOrder(0));
+        }
+    }
+    
+    @Test
+    public void testGetSortOrder() {
+        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
+    }
+    
+    @Test
+    public void testSetSortOrder() {
+        SortOrder toSet = SortOrder.ASCENDING;
+        controller.setSortOrder(0, toSet);
+        assertEquals(toSet, controller.getSortOrder(0));
+    }
+
+    /**
+     * Test per-column sortable setter
+     */
+    @Test
+    public void testSortableColumn() {
+        controller.setSortable(0, false);
+        assertFalse(controller.isSortable(0));
+    }
+    /**
+     * Test that per-controller and per-column sortable getters return false if
+     * controller sortable is false.
+     */
+    @Test
+    public void testSortableFalse() {
+        controller.setSortable(false);
+        assertFalse("controller must not be sortable", controller.isSortable());
+        for (int i = 0; i < controller.getModel().getColumnCount(); i++) {
+            assertFalse("columns must not be sortable if controller isn't", controller.isSortable(i));
+        }
+    }
+    
+    /**
+     * Controller sortable is true by default.
+     */
+    @Test
+    public void testSortableDefault() {
+       assertTrue("controller must be sortable by default", controller.isSortable());
+    }
+    
+    /**
+     * per-column sortable is true by default.
+     */
+    @Test
+    public void testSortableColumnDefault() {
+        for (int i = 0; i < controller.getModel().getColumnCount(); i++) {
+            assertTrue("columns must be sortable by default", controller.isSortable(i));
+        }
+    }
+    
+    
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        teamModel = new AncientSwingTeam();
+        controller = new TableSortController<TableModel>();
+        controller.setModel(teamModel);
+    }
+
+    
+}
