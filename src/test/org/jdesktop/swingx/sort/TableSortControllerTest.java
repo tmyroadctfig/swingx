3288411343a1344f581ec984c35b8f08197a9bb2
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2009-08-19 11:54:49 +0000

    STARTED - task 1155: Move SortController impl to a common ancestor for list/table sorter https://swingx.dev.java.net/issues/show_bug.cgi?id=1155 implemented ListSortController cleanup tests: have one AbstractTestSortController, let ConcreteSortControllerTests extend it
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3450 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/sort/TableSortControllerTest.java
+++ src/test/org/jdesktop/swingx/sort/TableSortControllerTest.java
@@ -23,20 +23,13 @@ package org.jdesktop.swingx.sort;
 
 import java.awt.Color;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.swing.RowFilter;
-import javax.swing.SortOrder;
-import javax.swing.RowSorter.SortKey;
 import javax.swing.table.TableModel;
 
-import org.jdesktop.swingx.InteractiveTestCase;
-import org.jdesktop.swingx.renderer.StringValue;
-import org.jdesktop.swingx.renderer.StringValues;
 import org.jdesktop.test.AncientSwingTeam;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -47,22 +40,11 @@ import org.junit.runners.JUnit4;
  * @author Jeanette Winzenburg
  */
 @RunWith(JUnit4.class)
-public class TableSortControllerTest extends InteractiveTestCase {
+public class TableSortControllerTest extends AbstractTestSortController {
     @SuppressWarnings("unused")
     private static final Logger LOG = Logger
             .getLogger(TableSortControllerTest.class.getName());
     
-    private TableModel teamModel;
-    private TableSortController<TableModel> controller;
-    
-    /**
-     * A custom StringValue for Color. Maps to a string composed of the
-     * prefix "R/G/B: " and the Color's rgb value.
-     */
-    private StringValue sv;
-
-    /** writable version */
-    private StringValueRegistry registry;
 
 
     public static void main(String[] args) {
@@ -73,232 +55,27 @@ public class TableSortControllerTest extends InteractiveTestCase {
             e.printStackTrace();
         }
     }
+
+
     
     @Test
     public void testUseStringValueProvider() {
         registry.setStringValue(sv, Color.class);
         controller.setStringValueProvider(registry);
-        RowFilter<TableModel, Integer> filter = RowFilter.regexFilter("R/G/B: -2", 2);
+        RowFilter<Object, Object> filter = RowFilter.regexFilter("R/G/B: -2", 2);
         controller.setRowFilter(filter);
         assertTrue("view row count: " + controller.getViewRowCount(), controller.getViewRowCount() > 0);
 //        assertEquals(sv.getString(table.getValueAt(0, 2)), table.getStringAt(0, 2));
     }
-    
-    @Test
-    public void testSetStringValueProvider() {
-        registry.setStringValue(sv, Color.class);
-        controller.setStringValueProvider(registry);
-        assertEquals(registry, controller.getStringValueProvider());
-    }
-    
-    @Test
-    public void testLastColumn() {
-        controller.toggleSortOrder(teamModel.getColumnCount() - 1);
-        // was silly mistake ...
-        controller.toggleSortOrder(teamModel.getColumnCount() - 1);
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void testNPEOnNullSortOrderCycle() {
-        controller.setSortOrderCycle((SortOrder[])null);
-    }
-    
-    @Test(expected=NullPointerException.class)
-    public void testNPEOnNullSortOrderCycleElements() {
-        controller.setSortOrderCycle((SortOrder) null);
-    }
-    
-    @Test
-    public void testUseSortOrderCycle() {
-        // empty cylce - does nothing
-        controller.setSortOrderCycle(new SortOrder[0]);
-        controller.toggleSortOrder(0);
-        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
-    }
-    
-    @Test
-    public void testSortOrderCycleSet() {
-        SortOrder[] cycle = new SortOrder[] {};
-        controller.setSortOrderCycle(cycle);
-        assertEqualsArrayByElements(cycle, controller.getSortOrderCycle());
-    }
-    
-    /**
-     * Need to completely take over the toggleSortOrder to support custom cycle.
-     * So need to check if the exception is thrown as expected
-     */
-    @Test(expected=IndexOutOfBoundsException.class)
-    public void testColumnIndexCheckedOnToggle() {
-        controller.toggleSortOrder(teamModel.getColumnCount());
-    }
-    /**
-     * @param string
-     * @param first
-     * @param second
-     */
-    private void assertEqualsArrayByElements(Object[] first,
-            Object[] second) {
-        assertEquals("must have same size", first.length, second.length);
-        for (int i = 0; i < second.length; i++) {
-            assertEquals("item must be same at index:  " + i, first[i], second[i]);
-        }
-    }
 
-    /**
-     * Test that sortsOnUpdate property is true by default. 
-     * That's different from core (which is false)
-     */
-    @Test
-    public void testSortsOnUpdateDefault() {
-       assertEquals("sortsOnUpdates must be true by default", true, controller.getSortsOnUpdates()); 
-    }
-    /**
-     * Test that toggle sort order has no effect if column not sortable.
-     */
-    @Test
-    public void testToggleSortOrderIfSortableFalse() {
-        controller.setSortable(0, false);
-        controller.toggleSortOrder(0);
-        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
-    }
-    /**
-     * Test that set sort order has no effect if column not sortable.
-     * 
-     */
-    @Test
-    public void testSetSortOrderIfSortableFalse() {
-        controller.setSortable(0, false);
-        controller.setSortOrder(0, SortOrder.ASCENDING);
-        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
-    }
-
-    /**
-     * Test that resetSortOrders doesn't change sorts at all if controller is not sortable.
-     */
-    @Test
-    public void testResetSortOrdersIfSortableFalse() {
-        controller.toggleSortOrder(0);
-        controller.toggleSortOrder(1);
-        controller.setSortable(false);
-        List<? extends SortKey> keys = controller.getSortKeys();
-        assertEquals(2, keys.size());
-        controller.resetSortOrders();
-        assertEquals("resetSortOrders must not touch unsortable columns", 
-                keys, controller.getSortKeys());
-    }
-   
-    /**
-     * Test that resetSortOrders doesn't change sorts at all if controller is not sortable.
-     */
-    @Test
-    public void testResetSortOrdersIfColumnSortableFalse() {
-        controller.toggleSortOrder(0);
-        controller.toggleSortOrder(1);
-        controller.setSortable(0, false);
-        List<? extends SortKey> keys = controller.getSortKeys();
-        assertEquals(2, keys.size());
-        controller.resetSortOrders();
-        assertEquals("resetSortOrders must not touch unsortable columns", 
-                1, controller.getSortKeys().size());
-        
-    }
-    /**
-     * Test that resetSortOrders removes all sorts.
-     */
-    @Test
-    public void testResetSortOrders() {
-        controller.toggleSortOrder(0);
-        controller.toggleSortOrder(1);
-        assertEquals(2, controller.getSortKeys().size());
-        controller.resetSortOrders();
-        assertEquals(0, controller.getSortKeys().size());
-    }
-    /**
-     * Test that toggle cycles through sort order cycle.
-     */
-    @Test
-    public void testToggleSortOrder() {
-        // PENDING JW: use custom cycle to really test
-        SortOrder[] cycle = controller.getSortOrderCycle();
-        for (int i = 0; i < cycle.length; i++) {
-            controller.toggleSortOrder(0);
-            assertEquals(cycle[i], controller.getSortOrder(0));
-        }
+    @Override
+    protected int getColumnCount() {
+        return ((TableModel) controller.getModel()).getColumnCount();
     }
     
-    @Test
-    public void testGetSortOrder() {
-        assertEquals(SortOrder.UNSORTED, controller.getSortOrder(0));
-    }
-    
-    @Test
-    public void testSetSortOrder() {
-        SortOrder toSet = SortOrder.ASCENDING;
-        controller.setSortOrder(0, toSet);
-        assertEquals(toSet, controller.getSortOrder(0));
-    }
-
-    /**
-     * Test per-column sortable setter
-     */
-    @Test
-    public void testSortableColumn() {
-        controller.setSortable(0, false);
-        assertFalse(controller.isSortable(0));
-    }
-    /**
-     * Test that per-controller and per-column sortable getters return false if
-     * controller sortable is false.
-     */
-    @Test
-    public void testSortableFalse() {
-        controller.setSortable(false);
-        assertFalse("controller must not be sortable", controller.isSortable());
-        for (int i = 0; i < controller.getModel().getColumnCount(); i++) {
-            assertFalse("columns must not be sortable if controller isn't", controller.isSortable(i));
-        }
-    }
-    
-    /**
-     * Controller sortable is true by default.
-     */
-    @Test
-    public void testSortableDefault() {
-       assertTrue("controller must be sortable by default", controller.isSortable());
-    }
-    
-    /**
-     * per-column sortable is true by default.
-     */
-    @Test
-    public void testSortableColumnDefault() {
-        for (int i = 0; i < controller.getModel().getColumnCount(); i++) {
-            assertTrue("columns must be sortable by default", controller.isSortable(i));
-        }
-    }
 
 //-------------------- utility methods and setup
     
-    /**
-     * Creates and returns a StringValue which maps a Color to it's R/G/B rep, 
-     * prepending "R/G/B: "
-     * 
-     * @return the StringValue for color.
-     */
-    private StringValue createColorStringValue() {
-        StringValue sv = new StringValue() {
-
-            public String getString(Object value) {
-                if (value instanceof Color) {
-                    Color color = (Color) value;
-                    return "R/G/B: " + color.getRGB();
-                }
-                return StringValues.TO_STRING.getString(value);
-            }
-            
-        };
-        return sv;
-    }
     /**
      * @param registry2
      * @param teamModel2
@@ -327,15 +104,23 @@ public class TableSortControllerTest extends InteractiveTestCase {
         registry.setColumnClasses(classPerColumn);
     }
 
-
-    @Before
     @Override
-    public void setUp() throws Exception {
-        teamModel = new AncientSwingTeam();
-        controller = new TableSortController<TableModel>(teamModel);
-        sv = createColorStringValue();
-        registry = new StringValueRegistry();
-        initColumnClasses(registry, teamModel);
+    protected DefaultSortController<?, ?> createDefaultSortController(
+            Object model) {
+        return new TableSortController<TableModel>((TableModel) model);
+    }
+
+
+    @Override
+    protected TableModel createModel() {
+        return new AncientSwingTeam();
+    }
+
+
+    @Override
+    protected void setupModelDependentState(Object model) {
+      initColumnClasses(registry, (TableModel) model);
+
     }
 
     
