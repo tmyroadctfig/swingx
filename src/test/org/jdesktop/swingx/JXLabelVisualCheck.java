4b37acb0a9c0bbf8216d59354ce94720e0ed5bbb
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2009-09-09 09:51:19 +0000

    JXLabelVisualCheck: added example to tweak foreground painter
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3496 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXLabelVisualCheck.java
+++ src/test/org/jdesktop/swingx/JXLabelVisualCheck.java
@@ -21,17 +21,38 @@
  */
 package org.jdesktop.swingx;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.logging.Logger;
 
 import javax.swing.Action;
 import javax.swing.Box;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 import org.jdesktop.swingx.action.AbstractActionExt;
+import org.jdesktop.swingx.painter.AbstractPainter;
 import org.jdesktop.swingx.painter.AlphaPainter;
 import org.jdesktop.swingx.painter.CompoundPainter;
+import org.jdesktop.swingx.painter.MattePainter;
 import org.jdesktop.swingx.painter.ShapePainter;
+
+import com.jhlabs.image.AbstractBufferedImageOp;
+import com.jhlabs.image.CausticsFilter;
+import com.jhlabs.image.ChromeFilter;
+import com.jhlabs.image.ShadowFilter;
+import com.jhlabs.image.SparkleFilter;
 
 /**
  * Base test class for JXLabel related code and issues.
@@ -51,6 +72,117 @@ public class JXLabelVisualCheck extends InteractiveTestCase {
         }
     }
     
+    /**
+     * Example of how-to apply filters to the label's foreground.
+     */
+    @SuppressWarnings("unchecked")
+    public void interactiveFancyFilter() {
+        JXLabel label = new JXLabel("that's the real text");
+        label.setFont(new Font("SansSerif", Font.BOLD, 80));
+        AbstractPainter<?> fg = (AbstractPainter<Object>) label.getForegroundPainter();
+        fg.setFilters(new ChromeFilter(), new ShadowFilter(5, 5, 2, .7f));
+        JXPanel panel = new JXPanel();
+        MattePainter background = new MattePainter(Color.BLUE);
+        background.setFilters(new CausticsFilter(), new SparkleFilter());
+        panel.setBackgroundPainter(background);
+        panel.add(label);
+        JXFrame frame = wrapInFrame(panel, "fancy filter");
+        show(frame,400, 400);
+    }
+    
+    
+    /**
+     * Notice that this is not perfect wrt alignment to see just uncomment super.paintComponent(g) 
+     * Maybe somebody with BasicLabelUI knowledge could help?
+     * 
+     */
+    public static final class JHLabsLabel extends JLabel {
+
+     private int textX;
+
+     private int textY;
+
+     private AbstractBufferedImageOp[] filters;
+
+     public JHLabsLabel(String string, AbstractBufferedImageOp... filters) {
+      super(string);
+      this.filters = filters;
+     }
+
+     @Override
+     public void paintComponent(Graphics g) {
+
+      // super.paintComponent(g);
+
+      BufferedImage img = createTextImage(getText(), getFont());
+
+      for (AbstractBufferedImageOp f : filters) {
+       img = f.filter(img, null);
+      }
+
+      // g.drawImage(f.filter(img, null), textX, textY, null);
+      g.drawImage(img, textX, textY, null);
+     }
+
+     private BufferedImage createTextImage(String text, Font font) {
+
+      Rectangle paintIconR = new Rectangle();
+      Rectangle paintTextR = new Rectangle();
+      Rectangle paintViewR = new Rectangle();
+      Insets paintViewInsets = new Insets(0, 0, 0, 0);
+
+      paintViewInsets = getInsets(paintViewInsets);
+      paintViewR.x = paintViewInsets.left;
+      paintViewR.y = paintViewInsets.top;
+      paintViewR.width = getWidth()
+        - (paintViewInsets.left + paintViewInsets.right);
+      paintViewR.height = getHeight()
+        - (paintViewInsets.top + paintViewInsets.bottom);
+
+      String clippedText = SwingUtilities.layoutCompoundLabel(
+        (JComponent) this, getFontMetrics(getFont()), text,
+        getIcon(), getVerticalAlignment(),
+        getHorizontalAlignment(), getVerticalTextPosition(),
+        getHorizontalTextPosition(), paintViewR, paintIconR,
+        paintTextR, getIconTextGap());
+
+      boolean isAntiAliased = true;
+      boolean usesFractionalMetrics = false;
+      FontRenderContext frc = new FontRenderContext(null, isAntiAliased,
+        usesFractionalMetrics);
+      TextLayout layout = new TextLayout(clippedText, font, frc);
+      Rectangle2D bounds = layout.getBounds();
+      int w = (int) Math.ceil(bounds.getWidth());
+      int h = (int) Math.ceil(bounds.getHeight());
+      BufferedImage image = new BufferedImage(w, h,
+        BufferedImage.TYPE_INT_ARGB);
+      Graphics2D g = image.createGraphics();
+      g.setColor(new Color(0, 0, 0, 0));
+      g.fillRect(0, 0, w, h);
+      g.setColor(getForeground());
+      g.setFont(font);
+      Object antiAliased = isAntiAliased ? RenderingHints.VALUE_TEXT_ANTIALIAS_ON
+        : RenderingHints.VALUE_TEXT_ANTIALIAS_OFF;
+      g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+        antiAliased);
+      Object fractionalMetrics = usesFractionalMetrics ? RenderingHints.VALUE_FRACTIONALMETRICS_ON
+        : RenderingHints.VALUE_FRACTIONALMETRICS_OFF;
+      g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
+        fractionalMetrics);
+      g.drawString(clippedText, (float) -bounds.getX(), (float) -bounds
+        .getY());
+      // g.drawString(clippedText, (float) 0, (float) 0);
+      g.dispose();
+
+      textX = paintTextR.x;
+      textY = paintTextR.y;// + getFontMetrics(font).getAscent() / 2;
+      System.out.println(String.format("X=%d Y=%d, w=%d h=%d", textX,
+        textY, w, h));
+
+      return image;
+     }
+
+    }
 
     /**
      * Issue #??-swingx: default foreground painter not guaranteed after change.
