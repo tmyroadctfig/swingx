5db48452c108f9871544fa12a20df6c43f163eef
kleopatra <kleopatra@1312f61e-266d-0410-97fa-c3b71232c9ac>
2009-11-18 15:05:59 +0000

    Issue #1223-swingx: JXTitledPanel support rounded borders - added visual test to expose
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3557 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/JXTitledPanelTest.java
+++ src/test/org/jdesktop/swingx/JXTitledPanelTest.java
@@ -8,10 +8,15 @@ package org.jdesktop.swingx;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.ComponentOrientation;
 import java.awt.Font;
 import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.LinearGradientPaint;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.util.logging.Logger;
 
@@ -26,8 +31,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
+import org.jdesktop.swingx.color.ColorUtil;
 import org.jdesktop.swingx.painter.MattePainter;
 import org.jdesktop.test.PropertyChangeReport;
 import org.junit.Test;
@@ -317,7 +324,70 @@ public class JXTitledPanelTest extends InteractiveTestCase {
         JFrame frame = wrapInFrame(panel, "toggle Title");
         frame.setVisible(true);
     }
-    
+ 
+    /**
+     * Issue #1223-swingx: title looks ugly with rounded border.
+     * 
+     */
+    public void interactiveRoundedBorder() {
+        final JXTitledPanel titledPanel = new JXTitledPanel("Search");
+        titledPanel.setBorder(new RoundedBorder(50));
+        JXFrame frame = wrapInFrame(titledPanel, "Rounded border?");
+        show(frame, 400, 400);
+    }
+
+    /**
+     * This is the implementation from swinglabs demos (originally 
+     * swingset3): while it be extreme in declaring all the vertical
+     * extend of the complete corner as border insets, the general 
+     * problem remains in every distribution. Fitting a square
+     * titleBar into round corners isn't easily possible ;-)
+     * 
+     * see forum thread:
+     * http://forums.java.net/jive/thread.jspa?threadID=69440&tstart=0
+     */
+    public static class RoundedBorder implements Border {    
+        private int cornerRadius;
+        
+        public RoundedBorder() {
+            this(10);
+        }
+        
+        public RoundedBorder(int cornerRadius) {
+            this.cornerRadius = cornerRadius;
+        }
+
+        public Insets getBorderInsets(Component c) {
+            return getBorderInsets(c, new Insets(0,0,0,0));
+        }
+
+        public Insets getBorderInsets(Component c, Insets insets) {
+            insets.top = insets.bottom = cornerRadius/2; 
+            insets.left = insets.right = 1;
+            return insets;
+        }
+
+        public boolean isBorderOpaque() {
+            return false;
+        }
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2 = (Graphics2D)g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            
+            Color color = Color.RED;
+            g2.setColor(ColorUtil.setAlpha(color, 40));        
+            g2.drawRoundRect(x, y + 2, width - 1, height - 3, cornerRadius, cornerRadius);
+            g2.setColor(ColorUtil.setAlpha(color, 90));        
+            g2.drawRoundRect(x, y + 1, width - 1, height - 2, cornerRadius, cornerRadius); 
+            g2.setColor(ColorUtil.setAlpha(color, 255));        
+            g2.drawRoundRect(x, y, width - 1, height - 1, cornerRadius, cornerRadius);
+
+            g2.dispose();            
+        }
+    }
+
     public static void main(String args[]) {
         JXTitledPanelTest test = new JXTitledPanelTest();
         try {
