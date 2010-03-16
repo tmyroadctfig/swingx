9f0815d4b13a6e562d2ea896da02a1fcdbba2de8
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2010-03-16 13:48:12 +0000

    Issue 1287: When border delegate is null return empty insets.  This prevents the NullPointerException and is consistent with how null borders are handled by Swing.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3627 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/test/org/jdesktop/swingx/plaf/BuddyLayoutAndBorderTest.java
+++ src/test/org/jdesktop/swingx/plaf/BuddyLayoutAndBorderTest.java
@@ -1,7 +1,11 @@
 package org.jdesktop.swingx.plaf;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -9,9 +13,12 @@ import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 import org.jdesktop.swingx.prompt.BuddySupport;
+import org.jdesktop.test.EDTRunner;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(EDTRunner.class)
 public class BuddyLayoutAndBorderTest {
 	private BuddyLayoutAndBorder blab;
 	private JTextField textField;
@@ -68,5 +75,12 @@ public class BuddyLayoutAndBorderTest {
 
 		btn.setVisible(false);
 		assertSame(left, blab.getBorderInsets(textField).left);
+	}
+	
+	@Test
+	public void testGetBorderInsetsWithNullDelegate() {
+	    textField.setBorder(null);
+	    blab.install(textField);
+	    assertThat(blab.getRealBorderInsets(), is(new Insets(0, 0, 0, 0)));
 	}
 }
