af4062c1312bcb0679bc54b3adcb32210df65fc1
bierhance <bierhance@1312f61e-266d-0410-97fa-c3b71232c9ac>
2010-07-27 13:56:28 +0000

    Issue 394: rewrite of ComboBoxCellEditor to support aqua l&f
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3738 1312f61e-266d-0410-97fa-c3b71232c9ac

--- src/java/org/jdesktop/swingx/autocomplete/ComboBoxCellEditor.java
+++ src/java/org/jdesktop/swingx/autocomplete/ComboBoxCellEditor.java
@@ -21,17 +21,13 @@
 package org.jdesktop.swingx.autocomplete;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.io.Serializable;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
 
-import javax.swing.AbstractCellEditor;
-import javax.swing.ComboBoxEditor;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.table.TableCellEditor;
+import javax.swing.text.JTextComponent;
 
 /**
  * <p>This is a cell editor that can be used when a combo box (that has been set
@@ -53,98 +49,61 @@ import javax.swing.table.TableCellEditor;
  * </code></pre>
  * </p>
  */
-public class ComboBoxCellEditor extends AbstractCellEditor implements TableCellEditor, Serializable {
+public class ComboBoxCellEditor extends DefaultCellEditor {
     
-    /** the combo box */
-    private JComboBox comboBox;
-
     /**
      * Creates a new ComboBoxCellEditor.
      * @param comboBox the comboBox that should be used as the cell editor.
      */
     public ComboBoxCellEditor(final JComboBox comboBox) {
-        this.comboBox = comboBox;
-        
-        Handler handler = new Handler();
-        
-        // Don't do this:
-        // this.comboBox.putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
-        // it probably breaks various things
-        
-        // hitting enter in the combo box should stop cellediting
-        JComponent editorComponent = (JComponent) comboBox.getEditor().getEditorComponent();
-        editorComponent.addKeyListener(handler);
-        // remove the editor's border - the cell itself already has one
-        editorComponent.setBorder(null);
+        super(comboBox);
 
-        // editor component might change (e.g. a look&feel change)
-        // the new editor component needs to be modified then (keyListener, border)
-        comboBox.addPropertyChangeListener(handler);
-    }
-    
-    // ------ Implementing CellEditor ------
-    /**
-     * Returns the value contained in the combo box
-     * @return the value contained in the combo box
-     */
-    public Object getCellEditorValue() {
-        return comboBox.getSelectedItem();
-    }
-    
-    /**
-     * Tells the combo box to stop editing and accept any partially edited value as the value of the combo box.
-     * Always returns true.
-     * @return true
-     */
-    @Override
-    public boolean stopCellEditing() {
-        if (comboBox.isEditable()) {
-            // Notify the combo box that editing has stopped (e.g. User pressed F2)
-            comboBox.actionPerformed(new ActionEvent(this, 0, ""));
-        }
-        fireEditingStopped();
-        return true;
-    }
-    
-    // ------ Implementing TableCellEditor ------
-    /**
-     * Sets an initial value for the combo box.
-     * Returns the combo box that should be added to the client's Component hierarchy.
-     * Once installed in the client's hierarchy this combo box will then be able to draw and receive user input.
-     * @param table the JTable that is asking the editor to edit; can be null
-     * @param value the value of the cell to be edited; null is a valid value
-     * @param isSelected will be ignored
-     * @param row the row of the cell being edited
-     * @param column the column of the cell being edited
-     * @return the combo box for editing
-     */
-    public java.awt.Component getTableCellEditorComponent(javax.swing.JTable table, Object value, boolean isSelected, int row, int column) {
-        comboBox.setSelectedItem(value);
-        return comboBox;
-    }
-    
-    // ------ Implementing TreeCellEditor ------
-//    public java.awt.Component getTreeCellEditorComponent(javax.swing.JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row) {
-//        String stringValue = tree.convertValueToText(value, isSelected, expanded, leaf, row, false);
-//        comboBox.setSelectedItem(stringValue);
-//        return comboBox;
-//    }
-    
-    class Handler extends KeyAdapter implements PropertyChangeListener {
-        @Override
-        public void keyPressed(KeyEvent keyEvent) {
-            int keyCode = keyEvent.getKeyCode();
-            if (keyCode==KeyEvent.VK_ENTER) stopCellEditing();
-        }
-        public void propertyChange(PropertyChangeEvent e) {
-            if (e.getPropertyName().equals("editor")) {
-                ComboBoxEditor editor = comboBox.getEditor();
-                if (editor!=null && editor.getEditorComponent()!=null) {
-                    JComponent editorComponent = (JComponent) comboBox.getEditor().getEditorComponent();
-                    editorComponent.addKeyListener(this);
-                    editorComponent.setBorder(null);
+        comboBox.removeActionListener(this.delegate);
+
+        this.delegate = new EditorDelegate() {
+            @Override
+	    public void setValue(Object value) {
+		comboBox.setSelectedItem(value);
+            }
+            @Override
+	    public Object getCellEditorValue() {
+		return comboBox.getSelectedItem();
+	    }
+            @Override
+            public boolean shouldSelectCell(EventObject anEvent) {
+                if (anEvent instanceof MouseEvent) {
+                    MouseEvent e = (MouseEvent)anEvent;
+                    return e.getID() != MouseEvent.MOUSE_DRAGGED;
+                }
+                return true;
+            }
+	    @Override
+            public boolean stopCellEditing() {
+		if (comboBox.isEditable()) {
+		    // Commit edited value.
+		    comboBox.actionPerformed(new ActionEvent(
+				     ComboBoxCellEditor.this, 0, ""));
+		}
+		return super.stopCellEditing();
+	    }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JTextComponent editorComponent = (JTextComponent) comboBox.getEditor().getEditorComponent();
+
+                if (editorComponent.getDocument() instanceof AutoCompleteDocument) {
+                    AutoCompleteDocument document = (AutoCompleteDocument) editorComponent.getDocument();
+                    // if auto completion is happening right now, cell editing should not be stopped
+                    if (!document.selecting) {
+                        ComboBoxCellEditor.this.stopCellEditing();
+                    }
+
+                } else {
+                    ComboBoxCellEditor.this.stopCellEditing();
                 }
             }
-        }
+        };
+	comboBox.addActionListener(this.delegate);
     }
 }
