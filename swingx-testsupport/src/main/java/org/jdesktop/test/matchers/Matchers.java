eb30e96a22b43c0b7fafc560ba365d22e527ef41
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2011-03-14 20:54:41 +0000

    Added an equivalence matcher.  Necessary for easy testing of serialized objects.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@3951 1312f61e-266d-0410-97fa-c3b71232c9ac

--- swingx-testsupport/src/main/java/org/jdesktop/test/matchers/Matchers.java
+++ swingx-testsupport/src/main/java/org/jdesktop/test/matchers/Matchers.java
@@ -18,4 +18,22 @@ public final class Matchers {
     public static Matcher<PropertyChangeEvent> property(String propertyName, Object oldValue, Object newValue) {
         return new PropertyChangeEventMatcher(propertyName, oldValue, newValue);
     }
+
+    /**
+     * This matcher returns {@code true} when the arguments are equivalent.
+     * <p>
+     * For purposes of this matcher equivalence is as follows:
+     * <ol>
+     * <li>the objects are equal</li>
+     * <li>the objects contain the same contents, insofar as the {@link java.beans.BeanInfo class
+     * info} for each object instance returns equivalent values</li>
+     * </ol>
+     * 
+     * @param <T>
+     * @param object
+     * @return
+     */
+    public static <T> Matcher<T> equivalentTo(T object) {
+        return new EquivalentMatcher<T>(object);
+    }
 }
