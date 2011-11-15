67c549485ff10ba9fe30c43bbe6aa97e52192975
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2011-11-15 19:02:52 +0000

    Code clean up.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@4085 1312f61e-266d-0410-97fa-c3b71232c9ac

--- swingx-testsupport/src/main/java/org/jdesktop/test/matchers/Matchers.java
+++ swingx-testsupport/src/main/java/org/jdesktop/test/matchers/Matchers.java
@@ -6,15 +6,45 @@ import java.beans.PropertyChangeEvent;
 
 import org.hamcrest.Matcher;
 
+/**
+ * A collection of matchers useful for testing.
+ * 
+ * @author kschaefer
+ */
 public final class Matchers {
+    /**
+     * A matcher for any property change.
+     * 
+     * @return {@code true} if any property can be matched; {@code false} otherwise
+     */
     public static Matcher<PropertyChangeEvent> anyProperty() {
         return any(PropertyChangeEvent.class);
     }
-    
+
+    /**
+     * A matcher for a specified property.
+     * 
+     * @param propertyName
+     *            the name of the property to match
+     * @return {@code true} if the property exists and has a name that matches the specified name;
+     *         {@code false} otherwise
+     */
     public static Matcher<PropertyChangeEvent> propertyWithName(String propertyName) {
         return new PropertyChangeEventMatcher(propertyName, null, null);
     }
-    
+
+    /**
+     * A matcher for a specified property.
+     * 
+     * @param propertyName
+     *            the name of the property to match
+     * @param oldValue
+     *            the old property value
+     * @param newValue
+     *            the new property value
+     * @return {@code true} if the property exists and all parameters match the specified values;
+     *         {@code false} otherwise
+     */
     public static Matcher<PropertyChangeEvent> property(String propertyName, Object oldValue, Object newValue) {
         return new PropertyChangeEventMatcher(propertyName, oldValue, newValue);
     }
@@ -30,8 +60,10 @@ public final class Matchers {
      * </ol>
      * 
      * @param <T>
+     *            the type of the matcher
      * @param object
-     * @return
+     *            the object to test
+     * @return {@code true} if the object is equivalent; {@code false} otherwise
      */
     public static <T> Matcher<T> equivalentTo(T object) {
         return new EquivalentMatcher<T>(object);
