33a4343704e495a5b0a09a2691fccabe0627fad4
kschaefe <kschaefe@1312f61e-266d-0410-97fa-c3b71232c9ac>
2012-02-01 21:30:22 +0000

    Clean up JavaDoc issues.
    git-svn-id: https://svn.java.net/svn/swingx~svn/trunk@4152 1312f61e-266d-0410-97fa-c3b71232c9ac

--- swingx-testsupport/src/main/java/org/jdesktop/test/categories/Visual.java
+++ swingx-testsupport/src/main/java/org/jdesktop/test/categories/Visual.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 
 /**
  * A category to use on tests that require visual display. This does not mean that the tests require
- * to be seen, but that {@link GraphicsEnvironment.isHeadless()} must return {@code true}.
+ * to be seen, but that {@link java.awt.GraphicsEnvironment#isHeadless()} must return {@code true}.
  * 
  * @author kschaefer
  */
